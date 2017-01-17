c83304487e321da2f029bd92cec8652020a15f56
Mag-nus <none>
2017-01-16 19:53:34 -0600

    Ready to return results to the client on the world port.. waiting for more instructions on how to properly do so.

--- Source/ACE/Network/Fragments/FragmentOpcode.cs
+++ Source/ACE/Network/Fragments/FragmentOpcode.cs
@@ -2,14 +2,15 @@
 {
     public enum FragmentOpcode
     {
-        None            = 0x0000,
-        CharacterDelete = 0xF655,
-		CharacterCreate = 0xF656,
-		CharacterList   = 0xF658,
-        PatchStatus     = 0xF7EA,
-        ServerName      = 0xF7E1,
+        None                    = 0x0000,
+		CharacterCreateResponse = 0xF643,
+        CharacterDelete         = 0xF655,
+		CharacterCreate         = 0xF656,
+		CharacterList           = 0xF658,
+        PatchStatus             = 0xF7EA,
+        ServerName              = 0xF7E1,
 
         // to be named...
-        Unknown75E5     = 0xF7E5
+        Unknown75E5             = 0xF7E5
     }
 }
