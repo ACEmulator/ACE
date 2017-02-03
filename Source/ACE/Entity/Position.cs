ab92499654d2c94f630474948ad1b33f80ae02f6
Xen <none@none.com>
2017-02-03 14:52:50 -0600

    Added /teleloc command

--- Source/ACE/Entity/Position.cs
+++ Source/ACE/Entity/Position.cs
@@ -1,4 +1,5 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Numerics;
 
 public class Position
@@ -27,6 +28,28 @@ public class Position
         Facing  = new Quaternion(payload.ReadSingle(), payload.ReadSingle(), payload.ReadSingle(), qw);
     }
 
+    public Position(float NorthSouth, float EastWest)
+    {
+        NorthSouth -= 0.5f;
+        EastWest -= 0.5f;
+        NorthSouth *= 10.0f;
+        EastWest *= 10.0f;
+
+        uint basex = (uint)(EastWest + 0x400);
+        uint basey = (uint)(NorthSouth + 0x400);
+
+        if (basex < 0 || basex >= 0x7F8 || basey < 0 || basey >= 0x7F8)
+            throw new Exception("Bad coordinates");  // TODO: Instead of throwing exception should we set to a default location?
+
+        float xOffset = ((basex & 7) * 24.0f) + 12;
+        float yOffset = ((basey & 7) * 24.0f) + 12;
+        float zOffset = GetZFromCellXY(Cell, xOffset, yOffset);
+
+        Cell = GetCellFromBase(basex, basey);
+        Offset = new Vector3(xOffset, yOffset, zOffset);
+        Facing = new Quaternion(0.0f, 0.0f, 0.0f, 1.0f);        
+    }
+        
     public void Write(BinaryWriter payload, bool quaternion = true)
     {
         payload.Write(Cell);
@@ -41,5 +64,24 @@ public class Position
             payload.Write(Facing.Y);
             payload.Write(Facing.Z);
         }
+    }
+
+    private float GetZFromCellXY(uint cell, float xOffset, float yOffset)
+    {
+        // TODO: Load correct z from file
+        return 200.0f; 
+    }
+
+    private uint GetCellFromBase(uint basex, uint basey)
+    {
+        byte blockx = (byte)(basex >> 3);
+        byte blocky = (byte)(basey >> 3);
+        byte cellx = (byte)(basex & 7);
+        byte celly = (byte)(basey & 7);
+
+        uint block = (uint)((blockx << 8) | blocky);
+        uint cell = (uint)((cellx << 3) | celly);
+
+        return (block << 16) | (cell + 1);
     }
 }
