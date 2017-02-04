530b993a27ac42719a4b2a524e7bac50d794a9aa
Ty Conner <tyconner@itcproductions.com>
2017-02-04 03:22:28 -0500

    Fixing a bug introduced by database access changes

--- Source/ACE/Network/Handlers/AuthenticationHandler.cs
+++ Source/ACE/Network/Handlers/AuthenticationHandler.cs
@@ -19,16 +19,27 @@ namespace ACE.Network
             string account    = packet.Payload.ReadString16L();
             packet.Payload.ReadUInt32();
             string glsTicket  = packet.Payload.ReadString32L();
-          
-            var result = await DatabaseManager.Authentication.GetAccountByName(account);
-            if (result.Count == 0 & !ConfigManager.Config.Server.EnableAutoAccountCreate)
+
+            try
             {
-                string password = System.Convert.ToString(glsTicket);
-                // TODO: convert password to normal string
-                Command.AccountCommands.HandleAccountCreate(session, account, password);
-                result = await DatabaseManager.Authentication.GetAccountByName(account);
+                var result = await DatabaseManager.Authentication.GetAccountByName(account);
+                AccountSelectCallback(result, session);
             }
-            AccountSelectCallback(result, session);
+            catch (System.IndexOutOfRangeException ex)
+            {
+                if (ConfigManager.Config.Server.EnableAutoAccountCreate)
+                {
+                    string password = System.Convert.ToString(glsTicket);
+                    // TODO: convert password to normal string
+                    Command.AccountCommands.HandleAccountCreate(session, account, password);
+                    var result = await DatabaseManager.Authentication.GetAccountByName(account);
+                    AccountSelectCallback(result, session);
+                }
+                else
+                {
+                    AccountSelectCallback(null, session);
+                }
+            }
         }
 
         private static void AccountSelectCallback(Account account, Session session)
