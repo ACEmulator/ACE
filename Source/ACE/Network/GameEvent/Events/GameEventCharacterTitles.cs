1fbd7f602eb4036ca0009d7de2438a38368fee3d
LytelthorpeThistledown <LytelthorpeThistledown@gmail.com>
2017-02-11 19:26:00 -0600

    Reduce temporary list of titles

--- Source/ACE/Network/GameEvent/Events/GameEventCharacterTitles.cs
+++ Source/ACE/Network/GameEvent/Events/GameEventCharacterTitles.cs
@@ -10,8 +10,8 @@
         {
             fragment.Payload.Write(1u);
             fragment.Payload.Write(1u); // TODO: get current title from database
-            fragment.Payload.Write(1000u); // TODO: get player's title list from database
-            for (uint i = 1; i <= 1000; i++)
+            fragment.Payload.Write(10u); // TODO: get player's title list from database
+            for (uint i = 1; i <= 10; i++)
                 fragment.Payload.Write(i);
         }
     }
