f94f3adb3e8852f743641896bbd73ce023f3b379
Ty Conner <tyconner@itcproductions.com>
2017-02-17 01:24:37 -0500

    Updated the console output slightly, prepped for a quick help command to be implemented.

---
+++ Source/ACE/Command/Handlers/ConsoleCommands.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using ACE.Entity;
+using ACE.Entity.Enum;
+using ACE.Managers;
+using ACE.Network;
+
+namespace ACE.Command
+{
+    public static class ConsoleCommands
+    {
+        // help
+        [CommandHandler("acehelp", AccessLevel.Admin, CommandHandlerFlag.ConsoleInvoke, 0)]
+        public static void HandleACEHelp(Session session, params string[] parameters)
+        {
+            //TODO: ACEHELP output
+            Console.WriteLine("TODO: Sorry, still need to implement this command");
+        }
+    }
+}
