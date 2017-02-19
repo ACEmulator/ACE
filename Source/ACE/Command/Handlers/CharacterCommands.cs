3356a7c9ef719018b70168aadc27ddf71deb3343
Ty Conner <tyconner@itcproductions.com>
2017-02-19 02:22:30 -0500

    Added set-characteraccess command

---
+++ Source/ACE/Command/Handlers/CharacterCommands.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.IO;
+
+using ACE.Common.Cryptography;
+using ACE.Database;
+using ACE.Entity;
+using ACE.Entity.Enum;
+using ACE.Network;
+
+namespace ACE.Command.Handlers
+{
+    public static class CharacterCommands
+    {
+        // set-characteraccess charactername accesslevel
+        [CommandHandler("set-characteraccess", AccessLevel.Admin, CommandHandlerFlag.ConsoleInvoke, 1)]
+        public static void HandleCharacterTokenization(Session session, params string[] parameters)
+        {
+            uint characterId = 0;
+            string characterName = parameters[0];
+
+            AccessLevel accessLevel;
+            if (parameters.Length > 1)
+            {
+                switch (parameters[1].ToLower())
+                {
+                    case "1":
+                    case "advocate":
+                        accessLevel = AccessLevel.Advocate;
+                        break;
+                    case "2":
+                    case "sentinel":
+                        accessLevel = AccessLevel.Sentinel;
+                        break;
+                    case "3":
+                    case "envoy":
+                        accessLevel = AccessLevel.Envoy;
+                        break;
+                    case "4":
+                    case "developer":
+                        accessLevel = AccessLevel.Developer;
+                        break;
+                    case "5":
+                    case "admin":
+                        accessLevel = AccessLevel.Admin;
+                        break;
+                    default:
+                        accessLevel = AccessLevel.Player;
+                        break;
+                }
+            }
+            else
+                accessLevel = AccessLevel.Player;
+
+            characterId = DatabaseManager.Character.SetCharacterAccessLevelByName(characterName.ToLower(), accessLevel);
+
+            if (characterId > 0)
+            {
+                string articleAorAN = "a";
+                if (accessLevel == AccessLevel.Advocate || accessLevel == AccessLevel.Admin || accessLevel == AccessLevel.Envoy)
+                    articleAorAN = "an";
+                
+                System.Console.WriteLine("Character " + characterName + " has been made " + articleAorAN + " " + Enum.GetName(typeof(AccessLevel), accessLevel) + ".");
+            }
+            else
+                System.Console.WriteLine("There is no character by the name " + characterName + " found in the database. Has it been deleted?");
+        }
+    }
+}
