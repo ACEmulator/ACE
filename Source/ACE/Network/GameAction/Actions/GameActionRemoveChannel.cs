89b316638fd52095ba6bb13bc9c2f426f6e759e2
Ty Conner <tyconner@itcproductions.com>
2017-02-26 00:53:18 -0500

    Resurrected the various Admin global channels used by the Advocates, Sentinels, Envoys, and Admins, stubbed the @on and @off commands and added support for @index and @clist commands.

---
+++ Source/ACE/Network/GameAction/Actions/GameActionRemoveChannel.cs
@@ -0,0 +1,29 @@
+﻿
+using ACE.Common.Extensions;
+using ACE.Entity;
+using ACE.Entity.Enum;
+
+namespace ACE.Network.GameAction.Actions
+{
+    [GameAction(GameActionOpcode.RemoveChannel)]
+    public class GameActionRemoveChannel : GameActionPacket
+    {
+        private GroupChatType chatChannelID;
+
+        public GameActionRemoveChannel(Session session, ClientPacketFragment fragment) : base(session, fragment) { }
+
+        public override void Read()
+        {
+            chatChannelID = (GroupChatType)Fragment.Payload.ReadUInt32();
+        }
+
+        public override void Handle()
+        {
+            // Probably need some IsAdvocate and IsSentinel type thing going on here as well. leaving for now
+            if (!Session.Player.IsAdmin && !Session.Player.IsArch && !Session.Player.IsPsr)
+                return;
+
+            //TODO: Unsubscribe from channel (chatChannelID) and save to db. Channel subscriptions are meant to persist between sessions.
+        }
+    }
+}
