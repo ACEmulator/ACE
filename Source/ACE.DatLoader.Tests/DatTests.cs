c4793ec44fdbcbdfb15db3aa38d071c512375d3b
Mogwai-AC <mogwai@adayfam.net>
2017-03-04 07:59:37 -0500

    portal.dat and cell.dat loading (issue #3) (#104)
    * cell.dat loading
    
    * portal.dat loading
    
    * reading sector size from dat file

---
+++ Source/ACE.DatLoader.Tests/DatTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ACE.DatLoader.Tests
+{
+    [TestClass]
+    public class DatTests
+    {
+        [TestMethod]
+        public void LoadCellDat_NoExceptions()
+        {
+            string celldat = @"C:\Turbine\client_cell_1.dat";
+            DatDatabase dat = new DatDatabase(celldat);
+            int count = dat.AllFiles.Count();
+            Assert.AreEqual(805348, count);
+        }
+
+        [TestMethod]
+        public void LoadPortalDat_NoExceptions()
+        {
+            string portaldatfile = @"C:\Turbine\client_portal.dat";
+            DatDatabase dat = new DatDatabase(portaldatfile);
+            int count = dat.AllFiles.Count();
+            Assert.AreEqual(79694, count);
+        }
+    }
+}
---
+++ Source/ACE.DatLoader/DatDatabase.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ACE.DatLoader
+{
+    public class DatDatabase
+    {
+        public DatDirectory RootDirectory { get; private set; }
+
+        public List<DatFile> AllFiles { get; private set; }
+
+        public DatDatabase(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException(filePath);
+            }
+
+            using (FileStream stream = new FileStream(filePath, FileMode.Open))
+            {
+                byte[] sectorSizeBuffer = new byte[4];
+                stream.Seek(0x144u, SeekOrigin.Begin);
+                stream.Read(sectorSizeBuffer, 0, sizeof(uint));
+                uint sectorSize = BitConverter.ToUInt32(sectorSizeBuffer, 0);
+
+                stream.Seek(0x160u, SeekOrigin.Begin);
+                byte[] firstDirBuffer = new byte[4];
+                stream.Read(firstDirBuffer, 0, sizeof(uint));
+                uint firstDirectoryOffset = BitConverter.ToUInt32(firstDirBuffer, 0);
+
+                RootDirectory = new DatDirectory(firstDirectoryOffset, Convert.ToInt32(sectorSize), stream);
+            }
+
+            AllFiles = new List<DatFile>();
+            RootDirectory.AddFilesToList(AllFiles);
+        }
+    }
+}
