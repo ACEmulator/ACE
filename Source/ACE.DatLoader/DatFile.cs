d73ad958db18dd1bf490a8f6bfcde94c9b19ac56
Lurker McDoogle <lurker.mcdoogle@gmail.com>
2017-03-13 15:38:29 -0400

    * improved cell.dat and portal.dat exporting, created admin server commands for them as well as unit tests that can be uncommented. * bug fix in DatFile with the FileOffset being wrong.  started detailed documentation on dat file contents. * worked on some object parsing.  found a bunch more object types in the client - now categorizes ALL portal files.

--- Source/ACE.DatLoader/DatFile.cs
+++ Source/ACE.DatLoader/DatFile.cs
@@ -1,10 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace ACE.DatLoader
 {
     public class DatFile
     {
+        private DatFileType? fileType = null;
+
         /// <summary>
         /// private to force loading from the static method
         /// </summary>
@@ -25,22 +29,54 @@ namespace ACE.DatLoader
 
         public uint Iteration { get; private set; }
 
+        public DatDatabaseType DatType {get; private set;}
+
         /// <summary>
         /// populates a new CellFile from the specified buffer.
         /// </summary>
-        public static DatFile FromBuffer(byte[] buffer, int offset)
+        public static DatFile FromBuffer(byte[] buffer, int offset, DatDatabaseType type)
         {
             DatFile cf = new DatFile()
             {
                 BitFlags = BitConverter.ToUInt32(buffer, offset),
                 ObjectId = BitConverter.ToUInt32(buffer, offset + 4),
-                FileOffset = BitConverter.ToUInt32(buffer, offset + 8),
+                FileOffset = BitConverter.ToUInt32(buffer, offset + 8) + 4,
                 FileSize = BitConverter.ToUInt32(buffer, offset + 12),
                 Date = BitConverter.ToUInt32(buffer, offset + 16),
-                Iteration = BitConverter.ToUInt32(buffer, offset + 20)
+                Iteration = BitConverter.ToUInt32(buffer, offset + 20),
+                DatType = type
             };
 
             return cf;
+        }
+
+        public DatFileType? GetFileType()
+        {
+            if (fileType != null)
+                return fileType.Value;
+
+            var type = typeof(DatFileType);
+            var enumTypes = Enum.GetValues(typeof(DatFileType)).Cast<DatFileType>().ToList();
+
+            foreach(var fileType in enumTypes)
+            {
+                var memInfo = type.GetMember(fileType.ToString());
+                var datType = memInfo[0].GetCustomAttributes(typeof(DatDatabaseTypeAttribute), false).Cast<DatDatabaseTypeAttribute>().ToList();
+
+                if (datType?.Count > 0 && datType[0].Type == this.DatType)
+                {
+                    // file type matches, now check id range
+                    var idRange = memInfo[0].GetCustomAttributes(typeof(DatFileTypeIdRangeAttribute), false).Cast<DatFileTypeIdRangeAttribute>().ToList();
+                    if (idRange?.Count > 0 && idRange[0].BeginRange <= this.ObjectId && idRange[0].EndRange >= this.ObjectId)
+                    {
+                        // id range matches
+                        this.fileType = fileType;
+                        break;
+                    }
+                }
+            }
+
+            return fileType;
         }
 
         /// <summary>
---
+++ Source/ACE.DatLoader/PortalDatDatabase.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACE.DatLoader
+{
+    public class PortalDatDatabase : DatDatabase
+    {
+        public PortalDatDatabase(string filename) : base(filename, DatDatabaseType.Portal)
+        {
+
+        }
+
+        public void ExtractCategorizedContents(string path)
+        {
+            string thisFolder = null;
+
+            using (FileStream stream = new FileStream(this.FilePath, FileMode.Open))
+            {
+                foreach (var file in this.AllFiles)
+                {
+                    if (file.GetFileType() != null)
+                        thisFolder = Path.Combine(path, file.GetFileType().ToString());
+                    else
+                        thisFolder = "UnknownType";
+
+                    if (!Directory.Exists(thisFolder))
+                    {
+                        Directory.CreateDirectory(thisFolder);
+                    }
+
+                    byte[] buffer = new byte[file.FileSize];
+                    stream.Seek(file.FileOffset, SeekOrigin.Begin);
+                    stream.Read(buffer, 0, buffer.Length);
+
+                    string hex = file.ObjectId.ToString("X8");
+                    string thisFile = Path.Combine(thisFolder, hex + ".bin");
+                    File.WriteAllBytes(thisFile, buffer);
+                }
+            }
+        }
+    }
+}
