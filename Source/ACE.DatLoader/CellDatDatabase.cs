1faa88b3b6c1b3061c56176e7b5408feefafe555
lurker-mcdoogle <lurker.mcdoogle@gmail.com>
2017-03-15 08:12:21 -0400

    * improved cell.dat and portal.dat exporting, created admin server commands for them as well as unit tests that can be uncommented. (#127)
    * bug fix in DatFile with the FileOffset being wrong.  started detailed documentation on dat file contents.
    * worked on some object parsing.  found a bunch more object types in the client - now categorizes ALL portal files.

---
+++ Source/ACE.DatLoader/CellDatDatabase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACE.DatLoader
+{
+    public class CellDatDatabase : DatDatabase
+    {
+        public CellDatDatabase(string filename) : base(filename, DatDatabaseType.Cell)
+        {
+
+        }
+
+        public void ExtractLandblockContents(string path)
+        {
+            using (FileStream stream = new FileStream(this.FilePath, FileMode.Open))
+            {
+                foreach (var file in this.AllFiles)
+                {
+                    string thisFolder = Path.Combine(path, (file.ObjectId >> 16).ToString("X4"));
+
+                    if (!Directory.Exists(thisFolder))
+                    {
+                        Directory.CreateDirectory(thisFolder);
+                    }
+
+                    byte[] buffer = new byte[file.FileSize];
+                    stream.Seek(file.FileOffset, SeekOrigin.Begin);
+                    stream.Read(buffer, 0, buffer.Length);
+
+                    string hex = file.ObjectId.ToString("X8");
+                    string thisFile = Path.Combine(thisFolder, hex + ".bin");
+                    File.WriteAllBytes(thisFile, buffer);
+                }
+            }
+        }
+    }
+}
---
+++ Source/ACE.DatLoader/DatFileTypeIdRangeAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACE.DatLoader
+{
+    [AttributeUsage(AttributeTargets.Field, AllowMultiple = false)]
+    public class DatFileTypeIdRangeAttribute : Attribute
+    {
+        public uint BeginRange { get; set; }
+
+        public uint EndRange { get; set; }
+
+        public DatFileTypeIdRangeAttribute(uint beginRange, uint endRange)
+        {
+            this.BeginRange = beginRange;
+            this.EndRange = endRange;
+        }
+    }
+}
