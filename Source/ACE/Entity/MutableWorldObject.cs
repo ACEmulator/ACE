bad1a55ff171bc83ddd24ded68d107dce1b5d020
StackOverflow <xinteractx@gmail.com>
2017-03-16 14:47:04 -0400

    Fixed

--- Source/ACE/Entity/MutableWorldObject.cs
+++ Source/ACE/Entity/MutableWorldObject.cs
@@ -7,10 +7,12 @@ using ACE.Entity.Enum;
 
 namespace ACE.Entity
 {
+
     /// <summary>
     /// any world object that can change a state of some sort that requires clients be updated.  players, monsters,
     /// doors, etc.
-     public abstract class MutableWorldObject : WorldObject/// </summary>
+    /// </summary>
+    public abstract class MutableWorldObject : WorldObject
    
     {
         public MutableWorldObject(ObjectType type, ObjectGuid guid) : base(type, guid)
