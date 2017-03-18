59d6ebb7a7978e9b15c02984c13df435ba4f329c
ogmage78 <cfscott@gmail.com>
2017-03-18 15:00:47 -0500

    Made changes for my training wand test.

--- Source/ACE/Command/Handlers/DebugCommands.cs
+++ Source/ACE/Command/Handlers/DebugCommands.cs
@@ -125,7 +125,7 @@ namespace ACE.Command.Handlers
         [CommandHandler("createtrainingwand", AccessLevel.Developer, CommandHandlerFlag.RequiresWorld)]
         public static void CreateTrainingWand(Session session, params string[] parameters)
         {
-            session.Network.EnqueueSend(new GameMessageCreateTrainingWand(session.Player));
+            LandblockManager.AddObject(AdminObjectFactory.CreateTrainingWand(session.Player));
         }
     }
 }
