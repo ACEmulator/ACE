313eddbcc9ca1e139ea52708b4389fe432a628eb
Ty Conner <tyconner@itcproductions.com>
2017-03-23 08:58:10 -0400

    Made changes to make Host config uniform with Port config and added logging/output. (#165)

--- Source/ACE/Network/Managers/SocketManager.cs
+++ Source/ACE/Network/Managers/SocketManager.cs
@@ -2,20 +2,40 @@
 using System.Net.Sockets;
 
 using ACE.Common;
+using System.Net;
+using System;
+using log4net;
 
 namespace ACE.Network.Managers
 {
     public static class SocketManager
     {
+        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
         private static readonly List<ConnectionListener> Listeners = new List<ConnectionListener>();
 
         public static void Initialise()
         {
-            Listeners.Add(new ConnectionListener(ConfigManager.Config.Server.Network.Port));
-            Listeners.Add(new ConnectionListener(ConfigManager.Config.Server.Network.Port + 1));
+            IPAddress host;
 
-            foreach (var loginListener in Listeners)
-                loginListener.Start();
+            try
+            {
+                host = IPAddress.Parse(ConfigManager.Config.Server.Network.Host);
+            }
+            catch (Exception ex)
+            {
+                log.Error($"Unable to use {ConfigManager.Config.Server.Network.Host} as host due to: {ex.ToString()}");
+                log.Error("Using IPAddress.Any as host instead.");
+                host = IPAddress.Any;
+            }
+
+            Listeners.Add(new ConnectionListener(host, ConfigManager.Config.Server.Network.Port));
+            log.Info($"Binding ConnectionListener to {host}:{ConfigManager.Config.Server.Network.Port}");
+            Listeners.Add(new ConnectionListener(host, ConfigManager.Config.Server.Network.Port + 1));
+            log.Info($"Binding ConnectionListener to {host}:{ConfigManager.Config.Server.Network.Port + 1}");
+
+            foreach (var listener in Listeners)
+                listener.Start();
         }
 
         public static Socket GetSocket()
