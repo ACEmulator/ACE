0924b02a7da12f602408eb39f5e1ee9e2a2a4cb0
Og II <cfscott@gmail.com>
2017-04-02 16:54:29 -0500

    Work in progress to PutItemInContainer

---
+++ Source/ACE/Network/GameAction/Actions/GameActionPutItemInContainer.cs
@@ -0,0 +1,16 @@
+﻿
+namespace ACE.Network.GameAction.Actions
+{
+    using global::ACE.Entity;
+
+    public static class GameActionPutItemInContainer
+    {
+        [GameAction(GameActionType.PutItemInContainer)]
+        public static void Handle(ClientMessage message, Session session)
+        {
+            var itemGuid = new ObjectGuid(message.Payload.ReadUInt32());
+            var containerGuid = new ObjectGuid(message.Payload.ReadUInt32());
+            session.Player.HandlePutItemInContainer(itemGuid, containerGuid, session);
+        }
+    }
+}
