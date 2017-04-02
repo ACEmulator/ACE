4b3b25e2f76a79d15f4ec3fd0be7385f1543d708
Og II <cfscott@gmail.com>
2017-04-01 21:42:44 -0500

    Added missing file

---
+++ Source/ACE.Entity/CharacterPositionExtensions.cs
@@ -0,0 +1,20 @@
+using ACE.Entity.Enum;
+
+namespace ACE.Entity
+{
+
+    public class CharacterPositionExtensions
+    {
+        public static Position StartingPosition(uint characterId)
+        {
+            var startingPosition = new Position(characterId, PositionType.Location, 2130969005, 12.3199f, -28.482f, 0.0049999995f, 0.0f, 0.0f, -0.9408059f, -0.3389459f);
+            return startingPosition;
+        }
+
+        public static Position InvalidPosition(uint characterId, PositionType type)
+        {
+            var invalidPosition = new Position(characterId, type, 0, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f, 0.0f);
+            return invalidPosition;
+        }
+    }
+}
