04b7716d54715c6c139a0aef0f808f0c2b1aadec
Zegeger <chris.tart@outlook.com>
2017-04-04 23:02:18 -0400

    Fixed issue in InboundFragmentCombining, also fixed exception in ConnectionListener which would stop the server from accepting connections, finally added Logging statement in Main which seems to allow log4net to work (#206)

--- Source/ACE/ACE.cs
+++ Source/ACE/ACE.cs
@@ -6,13 +6,16 @@ using ACE.Database;
 using ACE.Managers;
 using ACE.Network.Managers;
 using ACE.DatLoader;
+using log4net;
 
 namespace ACE
 {
     public class ACE
     {
+        private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         public static void Main(string[] args)
         {
+            log.Info("Starting ACEmulator...");
             Console.Title = "ACEmulator";
 
             ConfigManager.Initialise();
