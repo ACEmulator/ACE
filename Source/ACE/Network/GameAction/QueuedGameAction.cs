5bbb9182069ac06c23a3dbffdbd551858abdd63f
Mogwai-AC <mogwai@adayfam.net>
2017-04-06 13:36:47 -0400

    first pass of action queue

---
+++ Source/ACE/Network/GameAction/QueuedGameAction.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACE.Network.GameAction
+{
+    public class QueuedGameAction
+    {
+        public QueuedGameAction(uint objectId, GameActionType actionType)
+        {
+            this.ObjectId = objectId;
+            this.ActionType = actionType;
+        }
+
+        public QueuedGameAction(uint objectId, uint secondaryObjectId, GameActionType actionType)
+        {
+            this.ObjectId = objectId;
+            this.SecondaryObjectId = secondaryObjectId;
+            this.ActionType = actionType;
+        }
+
+        public uint ObjectId { get; private set; }
+
+        public uint SecondaryObjectId { get; private set; }
+
+        public GameActionType ActionType { get; private set; }
+    }
+}
