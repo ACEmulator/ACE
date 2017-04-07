c8edc71b2835076d70195c6715477c58c8a3bb5d
Lidefeath <lidefeath@gmx.net>
2017-04-08 01:49:56 +0200

    Fix AppVeyors complaints

--- Source/AppVeyor/MySqlInstall.bat
+++ Source/AppVeyor/MySqlInstall.bat
@@ -2,4 +2,4 @@
 "C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_auth < database\base\AuthenticationBase.sql
 "C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_character < database\base\CharacterBase.sql
 "C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_world < database\base\WorldBase.sql
-"C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_world < database\updates\005_vw_ace_creature_object.sql
+"C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_world < database\updates\world\005_vw_ace_creature_object.sql
