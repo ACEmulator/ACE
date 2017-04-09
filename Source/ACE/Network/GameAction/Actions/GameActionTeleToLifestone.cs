455afe04591711e8e9b3841074d091553abdd395
Ty Conner <tyconner@itcproductions.com>
2017-04-09 17:33:12 -0400

    Updated Lifestone Recall to use DelayedTeleport + AnimationEvent

--- Source/ACE/Network/GameAction/Actions/GameActionTeleToLifestone.cs
+++ Source/ACE/Network/GameAction/Actions/GameActionTeleToLifestone.cs
@@ -2,10 +2,13 @@
 using ACE.Database;
 using ACE.Entity;
 using ACE.Entity.Enum;
+using ACE.Entity.Enum.Properties;
 using ACE.Managers;
+using ACE.Network.Enum;
 using ACE.Network.GameEvent;
 using ACE.Network.GameEvent.Events;
 using ACE.Network.GameMessages.Messages;
+using ACE.Network.Motion;
 using System;
 using System.Collections.Generic;
 
@@ -17,7 +20,26 @@ namespace ACE.Network.GameAction.Actions
         public static void Handle(ClientMessage message, Session session)
         {
             if (session.Player.Positions.ContainsKey(PositionType.Sanctuary)) {
-                session.Player.Teleport(session.Player.Positions[PositionType.Sanctuary]);
+                // session.Player.Teleport(session.Player.Positions[PositionType.Sanctuary]);
+                string msg = $"{session.Player.Name} is recalling to the lifestone.";
+
+                var sysChatMessage = new GameMessageSystemChat(msg, ChatMessageType.Recall);
+
+                session.Player.Mana.Current = session.Player.Mana.Current / 2;
+                var updatePlayersMana = new GameMessagePrivateUpdateAttribute2ndLevel(session, Vital.Mana, session.Player.Mana.Current);
+
+                var updateCombatMode = new GameMessagePrivateUpdatePropertyInt(session, PropertyInt.CombatMode, 1);
+
+                var motionLifestoneRecall = new GeneralMotion(MotionStance.Standing, new MotionItem(MotionCommand.LifestoneRecall));
+
+                var animationEvent = new GameMessageUpdateMotion(session.Player, session, motionLifestoneRecall);
+
+                // TODO: This needs to be changed to broadcast sysChatMessage to only those in local chat hearing range
+                // FIX: I think this is only broadcasting to client, not to any other connected players. Likely animationEvent and sysChatMessage need to be broadcast to
+                //      clients that are currently tracking this player and the chat message should be within local chat hearing range.
+                session.Network.EnqueueSend(updatePlayersMana, updateCombatMode, animationEvent, sysChatMessage);
+
+                session.Player.SetDelayedTeleport(TimeSpan.FromSeconds(14), session.Player.Positions[PositionType.Sanctuary]);
             }
             else
             {
