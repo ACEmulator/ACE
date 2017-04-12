c02bdf73feddbe88dc19e7d99f579f6f023dd86c
Zegeger <chris.tart@outlook.com>
2017-04-12 01:52:50 -0400

    Rebase NetworkSession, Session, SocketManager merged into WorldManager, added stateful progression of events.  Trying to encapsulate logic better. This is lead up to major rebase of how Messages/Actions are handled.

---
+++ Source/ACE/Network/Enum/NetworkSessionState.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACE.Network.Enum
+{
+    public enum NetworkSessionState
+    {
+        Connecting,
+        Connected,
+        Disconnecting,
+        Disconnected
+    }
+}
