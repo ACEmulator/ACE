21f07425dcd4344ff6f3e98b67e334c4f5bf436b
fantoms <fantoms@users.noreply.github.com>
2017-04-12 16:18:43 -0500

    Add missing files

---
+++ Source/ACE/Entity/Events/OutboundMessageArgs.cs
@@ -0,0 +1,12 @@
+﻿using ACE.Network.GameMessages;
+using System;
+
+namespace ACE.Entity.Events
+{
+    public class OutboundMessageArgs : EventArgs
+    {
+        public GameMessage Message { get; set; }
+
+        public BroadcastAction BroadcastType { get; set; }
+    }
+}
---
+++ Source/ACE/Network/GameEvent/Events/GameEventPlayerKilled.cs
@@ -0,0 +1,14 @@
+﻿using ACE.Entity;
+
+namespace ACE.Network.GameEvent.Events
+{
+    public class GameEventPlayerKilled : GameEventMessage
+    {
+        public GameEventPlayerKilled(Session session, string deathText)
+            : base(GameEventType.PlayerKilled, GameMessageGroup.Group0A, session)
+        {
+            // Do nothing yet.
+            Writer.WriteString16L(deathText);
+        }
+    }
+}
