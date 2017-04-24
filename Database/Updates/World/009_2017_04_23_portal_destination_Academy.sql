8f346490f51c827db5563bf3ad6e6e01a4cdb1af
Keith McCormick <kdmxp512@gmail.com>
2017-04-23 21:35:55 -0700

    SQL update statements fixed

--- Database/Updates/World/009_2017_04_23_portal_destination_Academy.sql
+++ Database/Updates/World/009_2017_04_23_portal_destination_Academy.sql
@@ -1,10 +1,10 @@
 INSERT INTO `portal_destination` (`weenieClassId`, `cell`, `x`, `y`, `z`, `qx`, `qy`, `qz`, `qw`, `min_lvl`, `max_lvl`) VALUES
-(42841,4133027845,23.741,107.236,20,0,0,-0.691,-0.722,0,0);
-ON DUPLICATE KEY UPDATE cell=`4133027845`, x=`23.741`, y=`107.236`, z=`20`, qx=`0`, qy=`0`, qz=`-0.691`, qw=`-0.722`, min_lvl=`0`, max_lvl=`0`;
+(42841,4133027845,23.741,107.236,20,0,0,-0.691,-0.722,0,0)
+ON DUPLICATE KEY UPDATE cell=4133027845, x=23.741, y=107.236, z=20, qx=0, qy=0, qz=-0.691, qw=-0.722, min_lvl=0, max_lvl=0;
 
 INSERT INTO `portal_destination` (`weenieClassId`, `cell`, `x`, `y`, `z`, `qx`, `qy`, `qz`, `qw`, `min_lvl`, `max_lvl`) VALUES
 (2068,27132180,10,-40,0.005,0,0,-0.0375863,0.999293,5,0)
-ON DUPLICATE KEY UPDATE cell=`27132180`, x=`10`, y=`-40`, z=`0.005`, qx=`0`, qy=`0`, qz=`-0.0375863`, qw=`0.999293`, min_lvl=`5`, max_lvl=`0`;
+ON DUPLICATE KEY UPDATE cell=27132180, x=10, y=-40, z=0.005, qx=0, qy=0, qz=-0.0375863, qw=0.999293, min_lvl=5, max_lvl=0;
 
 INSERT INTO `portal_destination` (`weenieClassId`, `cell`, `x`, `y`, `z`, `qx`, `qy`, `qz`, `qw`, `min_lvl`, `max_lvl`) VALUES
 (29337,3663003677,84.8,99,20,0,0,0,1,0,0),
