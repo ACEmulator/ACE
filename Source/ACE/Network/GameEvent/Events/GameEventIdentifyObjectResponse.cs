873c26695b9f9766b757ae41adc1b91c991c9774
OptimShi <OptimShi@users.noreply.github.com>
2017-04-27 22:34:07 -0700

    Added sample code for a float property and int64 property to IdentifyObjectResponse

--- Source/ACE/Network/GameEvent/Events/GameEventIdentifyObjectResponse.cs
+++ Source/ACE/Network/GameEvent/Events/GameEventIdentifyObjectResponse.cs
@@ -16,12 +16,31 @@ namespace ACE.Network.GameEvent.Events
             Writer.Write(1u); // Success bool
 
             // Write ints
-            // Writer.Write(1u + (8 << 16)); // One int
+            // Writer.Write(2u + (8 << 16)); // count + tableSize (0x0010)
+            // Writer.Write(320u); // ITEM_XP_STYLE_INT
+            // Writer.Write(2); // Zero.
+            // Writer.Write(319u); // ITEM_MAX_LEVEL_INT.
+            // Writer.Write(2); // Zero.
+
+            // Write int64s/long
+            // Writer.Write(1u + (8 << 16)); // count + tableSize (0x0008)
+            // Writer.Write(4u); // ITEM_TOTAL_XP_INT64
+            // Writer.Write((long)1639577393); // Zero.
+            // Writer.Write(5u); // ITEM_BASE_XP_INT64
+            // Writer.Write((long)1000000000); // Zero.
+
+            // Write BIGints
+            // Writer.Write(1u + (8 << 16)); // count + tableSize (0x0008)
             // Writer.Write(19u); // VALUE (Pyreals)
             // Writer.Write(0); // Zero.
 
             // Write Bool
-            // Writer.Write(1u + (1 << 16)); // One bool
+            // Writer.Write(1u + (1 << 16)); // count + tableSize (0x0008)
+            // Writer.Write(2u); // IS_OPEN
+            // Writer.Write(0); // False.
+
+            // Write Float
+            // Writer.Write(1u + (1 << 16)); // count + tableSize (0x0008)
             // Writer.Write(2u); // IS_OPEN
             // Writer.Write(0); // False.
 
