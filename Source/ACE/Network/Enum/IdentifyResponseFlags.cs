33c78385db9b1c701310535bfaabf97f182ec4a7
OptimShi <OptimShi@users.noreply.github.com>
2017-04-27 18:03:15 -0700

    Basic stuff to try to get ID to work

---
+++ Source/ACE/Network/Enum/IdentifyResponseFlags.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ACE.Network.Enum
+{
+    [Flags]
+    public enum IdentifyResponseFlags
+    {
+        None = 0x0000,
+        PropertyInt32 = 0x0001,
+        PropertyBool = 0x0002,
+        PropertyDouble = 0x0004,
+        PropertyString = 0x0008,
+        Resource = 0x0100,
+        PropertyInt64 = 0x2000,
+        PropertySpell = 0x0010,       
+        PropertyWeapon = 0x0020,
+    }
+}
--- Source/ACE/Network/GameEvent/Events/GameEventIdentifyObjectResponse.cs
+++ Source/ACE/Network/GameEvent/Events/GameEventIdentifyObjectResponse.cs
@@ -1,4 +1,6 @@
-﻿namespace ACE.Network.GameEvent.Events
+﻿using ACE.Network.Enum;
+
+namespace ACE.Network.GameEvent.Events
 {
     public class GameEventIdentifyObjectResponse : GameEventMessage
     {
@@ -7,9 +9,26 @@
         {
             // TODO
 
+            // Writer.Write(objectID);
+            // Writer.Write(0u);
+            // Writer.Write(1u);
             Writer.Write(objectID);
-            Writer.Write(0u);
-            Writer.Write(1u);
+
+            IdentifyResponseFlags flags = IdentifyResponseFlags.None;
+            flags = IdentifyResponseFlags.PropertyString;
+            Writer.Write((uint)flags); // Flags
+            Writer.Write(1u); // Success bool
+            // Writer.Write(1u); // Flags
+
+            // Write Int32
+            // Writer.Write(1u); // One Item
+            // Writer.Write(0x13); // Value
+            // Writer.Write(0x0); // No Value
+
+            // Write Strings
+            Writer.Write(1u); // One String
+            Writer.Write(0x0E); // Use String
+            Writer.WriteString16L("Use this item to set your resurrection point.");
         }
     }
 }
