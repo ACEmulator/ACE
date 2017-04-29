8e27e603a08cd05631b10e4d712491509cb9622a
Ty Conner <tyconner@itcproductions.com>
2017-04-29 17:09:58 -0400

    Clean up Door.cs of debugging code.

--- Source/ACE/Entity/Door.cs
+++ Source/ACE/Entity/Door.cs
@@ -15,10 +15,8 @@ namespace ACE.Entity
         private static MovementData movementOpen = new MovementData();
         private static MovementData movementClosed = new MovementData();
 
-        // private static MotionState motionOpen = new UniversalMotion(MotionStance.Standing, new MotionItem(MotionCommand.On));
         private static MotionState motionStateOpen = new UniversalMotion(MotionStance.Standing, movementOpen);
         private static MotionState motionStateClosed = new UniversalMotion(MotionStance.Standing, movementClosed);
-        // private static MotionState motionClosed = new UniversalMotion(MotionStance.Standing, new MotionItem(MotionCommand.Off));
 
         private static UniversalMotion motionOpen = new UniversalMotion(MotionStance.Standing, new MotionItem(MotionCommand.On));
         private static UniversalMotion motionClosed = new UniversalMotion(MotionStance.Standing, new MotionItem(MotionCommand.Off));
@@ -26,35 +24,7 @@ namespace ACE.Entity
         public Door(AceObject aceO)
             : base((ObjectType)aceO.TypeId, new ObjectGuid(aceO.AceObjectId))
         {
-            // this.Name = aceO.Name;
-            // this.DescriptionFlags = (ObjectDescriptionFlag)aceO.WdescBitField;
-            // this.Location = aceO.Position;
-            // this.WeenieClassid = aceO.WeenieClassId;
-            // this.WeenieFlags = (WeenieHeaderFlag)aceO.WeenieFlags;
-
-            // this.PhysicsData.MTableResourceId = aceO.MotionTableId;
-            // this.PhysicsData.Stable = aceO.SoundTableId;
-            // this.PhysicsData.CSetup = aceO.ModelTableId;
-
-            //// this should probably be determined based on the presence of data.
-            // this.PhysicsData.PhysicsDescriptionFlag = (PhysicsDescriptionFlag)aceO.PhysicsBitField;
-            // this.PhysicsData.PhysicsState = (PhysicsState)aceO.PhysicsState;
-
-            //// game data min required flags;
-            // this.Icon = aceO.IconId;
-
-            // this.GameData.Usable = (Usable)aceO.Usability;
-            // this.GameData.RadarColour = (RadarColor)aceO.BlipColor;
-            // this.GameData.RadarBehavior = (RadarBehavior)aceO.Radar;
-            // this.GameData.UseRadius = aceO.UseRadius;
-
-            // aceO.AnimationOverrides.ForEach(ao => this.ModelData.AddModel(ao.Index, (ushort)ao.AnimationId));
-            // aceO.TextureOverrides.ForEach(to => this.ModelData.AddTexture(to.Index, (ushort)to.OldId, (ushort)to.NewId));
-            // aceO.PaletteOverrides.ForEach(po => this.ModelData.AddPalette(po.SubPaletteId, po.Offset, po.Length));
-
             this.Name = aceO.Name;
-            // if (this.Name == null)
-            //    this.Name = "NULL";
 
             this.DescriptionFlags = (ObjectDescriptionFlag)aceO.WdescBitField;
             this.Location = aceO.Position;
@@ -71,15 +41,7 @@ namespace ACE.Entity
             // this.PhysicsData.PhysicsState = (PhysicsState)aceO.PhysicsState;
             this.PhysicsData.PhysicsState |= PhysicsState.HasPhysicsBsp | PhysicsState.ReportCollision;
 
-            // if (aceO.CurrentMotionState == "0")
-            //    this.PhysicsData.CurrentMotionState = null;
-            // else
-            //    this.PhysicsData.CurrentMotionState = new UniversalMotion(Convert.FromBase64String(aceO.CurrentMotionState));
-
             this.PhysicsData.CurrentMotionState = motionStateClosed;
-
-            // this.PhysicsData.CurrentMotionState = new GeneralMotion(MotionStance.Standing, new MotionItem(MotionCommand.Off));
-            // this.PhysicsData.CurrentMotionState = new GeneralMotion(MotionStance.Standing, new MotionItem(MotionCommand.Dead));
 
             this.PhysicsData.ObjScale = aceO.ObjectScale;
             this.PhysicsData.AnimationFrame = aceO.AnimationFrameId;
@@ -88,9 +50,6 @@ namespace ACE.Entity
             // game data min required flags;
             this.Icon = aceO.IconId;
 
-            // if (this.GameData.NamePlural == null)
-            //    this.GameData.NamePlural = "NULLs";
-
             this.GameData.Type = aceO.WeenieClassId;
 
             this.GameData.Usable = (Usable)aceO.Usability;
@@ -98,16 +57,9 @@ namespace ACE.Entity
             this.GameData.RadarBehavior = (RadarBehavior)aceO.Radar;
             this.GameData.UseRadius = aceO.UseRadius;
 
-            // this.GameData.HookType = (ushort)aceO.HookTypeId;
-            // this.GameData.HookItemTypes = aceO.HookItemTypes;
-            // this.GameData.Burden = (ushort)aceO.Burden;
-            // this.GameData.Value = aceO.Value;
-            // this.GameData.ItemCapacity = aceO.ItemsCapacity;
-
             aceO.AnimationOverrides.ForEach(ao => this.ModelData.AddModel(ao.Index, ao.AnimationId));
             aceO.TextureOverrides.ForEach(to => this.ModelData.AddTexture(to.Index, to.OldId, to.NewId));
             aceO.PaletteOverrides.ForEach(po => this.ModelData.AddPalette(po.SubPaletteId, po.Offset, po.Length));
-            // aceO.PaletteOverrides.ForEach(po => this.ModelData.AddPalette(po.SubPaletteId, (byte)(po.Offset / 8), (byte)(po.Length / 8)));
             this.ModelData.PaletteGuid = aceO.PaletteId;
 
             movementOpen.ForwardCommand = (ushort)MotionCommand.On;
@@ -119,33 +71,9 @@ namespace ACE.Entity
             // TODO: implement auto close timer, check if door is locked, send locked soundfx if locked and fail to open.
 
             if (this.PhysicsData.CurrentMotionState == motionStateClosed)
-            {
-                // test here
-                // player.EnqueueMovementEvent(motionOpen, player.Guid);
-                // var motionOpen = new UniversalMotion(MotionStance.Standing, new MotionItem(MotionCommand.On));
-
-                // player.EnqueueMovementEvent(motionOpen, this.Guid);
-                // this.PhysicsData.CurrentMotionState = motionOpen;
-                // this.PhysicsData.PhysicsState |= PhysicsState.Ethereal;
-
                 Open(player);
-                // var autoEvent = new System.Threading.AutoResetEvent(false);
-                //// var test = new System.Threading.Timer(Door.Close(stateInfo, player), autoEvent, 5000, System.Threading.Timeout.Infinite);
-                // StateObjClass StateObj = new StateObjClass();
-                // System.Threading.TimerCallback TimerDelegate = new System.Threading.TimerCallback(Close);
-                //// var test = new System.Threading.Timer(TimerDelegate, autoEvent, 5000, 0);
-                // System.Threading.Timer TimerItem = new System.Threading.Timer(TimerDelegate, StateObj, 2000, 2000);
-            }
             else
-            {
-                // var motionOpen = new UniversalMotion(MotionStance.Standing, new MotionItem(MotionCommand.Off));
-
-                // player.EnqueueMovementEvent(motionClosed, this.Guid);
-                // this.PhysicsData.CurrentMotionState = motionClosed;
-                // this.PhysicsData.PhysicsState ^= PhysicsState.Ethereal;
-
-                // Close(player);
-            }
+                Close(player);
 
             var sendUseDoneEvent = new GameEventUseDone(player.Session);
             player.Session.Network.EnqueueSend(sendUseDoneEvent);
@@ -158,23 +86,11 @@ namespace ACE.Entity
             this.PhysicsData.PhysicsState |= PhysicsState.Ethereal;
         }
 
-        private void Close(Player player, object StateObj)
-        //private void Close(Object stateInfo)
-        // private void Close(Player player)
+        private void Close(Player player)
         {
-            // this.StateObj.player.EnqueueMovementEvent(motionClosed, this.Guid);
+            player.EnqueueMovementEvent(motionClosed, this.Guid);
             this.PhysicsData.CurrentMotionState = motionClosed;
             this.PhysicsData.PhysicsState ^= PhysicsState.Ethereal;
         }
-
-        // private class StateObjClass
-        // {
-        //    // Used to hold parameters for calls to TimerTask.  
-        //    public int SomeValue;
-        //    public System.Threading.Timer TimerReference;
-        //    public bool TimerCanceled;
-        //
-        //    public Player player;
-        // }
     }
 }
