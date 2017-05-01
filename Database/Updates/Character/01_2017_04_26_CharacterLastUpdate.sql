93d7bfb181b12cd11eefd0913d3d851eae11c6e9
Charles Scott <Charles.Scott@pointsmith.com>
2017-05-01 17:50:28 -0500

    Fixed correclty this time - I hope

--- Database/Updates/Character/01_2017_04_26_CharacterLastUpdate.sql
+++ Database/Updates/Character/01_2017_04_26_CharacterLastUpdate.sql
@@ -1,13 +1,1 @@
-SET @preparedStatement = (SELECT IF(
-    (SELECT COUNT(*)
-        FROM INFORMATION_SCHEMA.COLUMNS
-        WHERE  table_name = 'character'
-        AND table_schema = DATABASE()
-        AND column_name = 'birth'
-    ) = 0,
-    "SELECT 1",
-    "ALTER TABLE `character` CHANGE `birth` `lastUpdate` TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;"
-));
-PREPARE alterIfNotExists FROM @preparedStatement;
-EXECUTE alterIfNotExists;
-DEALLOCATE PREPARE alterIfNotExists;
+ALTER TABLE `character` CHANGE `birth` `lastUpdate` TIMESTAMP on update CURRENT_TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
