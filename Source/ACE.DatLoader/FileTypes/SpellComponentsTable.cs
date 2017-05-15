3303747ee9e5a0dfb0e0ae4597cc5ad6df680499
OptimShi <OptimShi@users.noreply.github.com>
2017-05-15 12:47:07 -0700

    started spell comps table

---
+++ Source/ACE.DatLoader/FileTypes/SpellComponentsTable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ACE.DatLoader.Entity;
+
+namespace ACE.DatLoader.FileTypes
+{
+    public class SpellComponentsTable
+    {
+        uint FileId { get; set; }
+        List<CSpellBase> SpellComponents { get; set; } = new List<CSpellBase>();
+
+        public static CharGen ReadFromDat()
+        {
+            // Check the FileCache so we don't need to hit the FileSystem repeatedly
+            if (DatManager.PortalDat.FileCache.ContainsKey(0x0E00000F))
+            {
+                return (CharGen)DatManager.PortalDat.FileCache[0x0E00000F];
+            }
+            else
+            {                
+                // Create the datReader for the proper file
+                DatReader datReader = DatManager.PortalDat.GetReaderForFile(0x0E00000F);
+                SpellComponentsTable comps = new SpellComponentsTable();
+
+                comps.FileId = datReader.ReadInt32();
+
+
+            }
+        }
