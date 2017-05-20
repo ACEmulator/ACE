87a31c4c1306e206ef30b2635bf0786d099ed6a3
OptimShi <OptimShi@users.noreply.github.com>
2017-05-19 17:01:27 -0700

    StyleCop Cleanup

--- Source/ACE.DatLoader/FileTypes/SpellComponentsTable.cs
+++ Source/ACE.DatLoader/FileTypes/SpellComponentsTable.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using ACE.DatLoader.Entity;
-using System.Globalization.TextInfo;
 
 namespace ACE.DatLoader.FileTypes
 {
