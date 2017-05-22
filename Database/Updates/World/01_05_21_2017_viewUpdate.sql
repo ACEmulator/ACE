d836634c998015ef2e87900b794722ec7d9fe8ab
Ogmage78 <cfscott@gmail.com>
2017-05-21 20:44:48 -0500

    Forgot to update views.   Apveyor going to not be happy.

--- Database/Updates/World/01_05_21_2017_viewUpdate.sql
+++ Database/Updates/World/01_05_21_2017_viewUpdate.sql
@@ -9,8 +9,11 @@ MySQL - 10.1.23-MariaDB : Database - ace_world
 /*!40101 SET SQL_MODE=''*/;
 
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`ace_world` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
 USE `ace_world`;
 
 /*Table structure for table `vw_ace_object` */
@@ -51,6 +54,57 @@ DROP TABLE IF EXISTS `vw_ace_object`;
  `qX` float ,
  `qY` float ,
  `qZ` float 
+)*/;
+
+/*Table structure for table `vw_ace_portal_object` */
+
+DROP TABLE IF EXISTS `vw_ace_portal_object`;
+
+/*!50001 DROP VIEW IF EXISTS `vw_ace_portal_object` */;
+/*!50001 DROP TABLE IF EXISTS `vw_ace_portal_object` */;
+
+/*!50001 CREATE TABLE  `vw_ace_portal_object`(
+ `aceObjectId` int(10) unsigned ,
+ `destLandblockId` int(10) unsigned ,
+ `destX` float ,
+ `destY` float ,
+ `destZ` float ,
+ `destQW` float ,
+ `destQX` float ,
+ `destQY` float ,
+ `destQZ` float ,
+ `positionId` int(10) unsigned ,
+ `min_lvl` int(11) unsigned ,
+ `max_lvl` int(11) unsigned ,
+ `societyId` tinyint(3) unsigned ,
+ `isTieable` tinyint(1) unsigned ,
+ `isRecallable` tinyint(1) unsigned ,
+ `isSummonable` tinyint(1) unsigned ,
+ `landblock` int(10) unsigned ,
+ `posX` float ,
+ `posY` float ,
+ `posZ` float ,
+ `qW` float ,
+ `qX` float ,
+ `qY` float ,
+ `qZ` float ,
+ `weenieClassId` int(10) unsigned ,
+ `aceObjectDescriptionFlags` int(10) unsigned ,
+ `animationFrameId` int(10) unsigned ,
+ `currentMotionState` text ,
+ `iconId` int(10) unsigned ,
+ `iconOverlayId` int(10) unsigned ,
+ `iconUnderlayId` int(10) unsigned ,
+ `modelTableId` int(10) unsigned ,
+ `motionTableId` int(10) unsigned ,
+ `physicsDescriptionFlag` int(10) unsigned ,
+ `playScript` smallint(5) unsigned ,
+ `physicsTableId` int(10) unsigned ,
+ `soundTableId` int(10) unsigned ,
+ `weenieHeaderFlags` int(10) unsigned ,
+ `spellId` smallint(5) unsigned ,
+ `defaultScript` int(10) unsigned ,
+ `itemType` int(10) unsigned 
 )*/;
 
 /*Table structure for table `vw_ace_weenie_class` */
@@ -139,6 +193,13 @@ DROP TABLE IF EXISTS `vw_teleport_location`;
 
 /*!50001 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vw_ace_object` AS select `ao`.`aceObjectId` AS `aceObjectId`,`aops`.`propertyValue` AS `name`,`ao`.`weenieClassId` AS `weenieClassId`,`awc`.`weenieClassDescription` AS `weenieClassDescription`,`ao`.`aceObjectDescriptionFlags` AS `aceObjectDescriptionFlags`,`ao`.`animationFrameId` AS `animationFrameId`,`ao`.`currentMotionState` AS `currentMotionState`,`ao`.`iconId` AS `iconId`,`ao`.`iconOverlayId` AS `iconOverlayId`,`ao`.`iconUnderlayId` AS `iconUnderlayId`,`ao`.`modelTableId` AS `modelTableId`,`ao`.`motionTableId` AS `motionTableId`,`ao`.`physicsDescriptionFlag` AS `physicsDescriptionFlag`,`ao`.`playScript` AS `playScript`,`ao`.`physicsTableId` AS `physicsTableId`,`ao`.`soundTableId` AS `soundTableId`,`ao`.`weenieHeaderFlags` AS `weenieHeaderFlags`,`ao`.`spellId` AS `spellId`,`ao`.`defaultScript` AS `defaultScript`,`aopi`.`propertyValue` AS `itemType`,`ap`.`positionId` AS `positionId`,`ap`.`positionType` AS `positionType`,`ap`.`landblock` AS `landblock`,`ap`.`posX` AS `posX`,`ap`.`posY` AS `posY`,`ap`.`posZ` AS `posZ`,`ap`.`qW` AS `qW`,`ap`.`qX` AS `qX`,`ap`.`qY` AS `qY`,`ap`.`qZ` AS `qZ` from ((((`ace_object` `ao` join `ace_weenie_class` `awc` on((`ao`.`weenieClassId` = `awc`.`weenieClassId`))) join `ace_object_properties_string` `aops` on(((`ao`.`aceObjectId` = `aops`.`aceObjectId`) and (`aops`.`strPropertyId` = 1)))) join `ace_object_properties_int` `aopi` on(((`ao`.`aceObjectId` = `aopi`.`aceObjectId`) and (`aopi`.`intPropertyId` = 1)))) join `ace_position` `ap` on(((`ao`.`aceObjectId` = `ap`.`aceObjectId`) and (`ap`.`positionType` = 1)))) */;
 
+/*View structure for view vw_ace_portal_object */
+
+/*!50001 DROP TABLE IF EXISTS `vw_ace_portal_object` */;
+/*!50001 DROP VIEW IF EXISTS `vw_ace_portal_object` */;
+
+/*!50001 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vw_ace_portal_object` AS (select `ao`.`aceObjectId` AS `aceObjectId`,`ap`.`landblock` AS `destLandblockId`,`ap`.`posX` AS `destX`,`ap`.`posY` AS `destY`,`ap`.`posZ` AS `destZ`,`ap`.`qW` AS `destQW`,`ap`.`qX` AS `destQX`,`ap`.`qY` AS `destQY`,`ap`.`qZ` AS `destQZ`,`apo`.`positionId` AS `positionId`,`apo`.`min_lvl` AS `min_lvl`,`apo`.`max_lvl` AS `max_lvl`,`apo`.`societyId` AS `societyId`,`apo`.`isTieable` AS `isTieable`,`apo`.`isRecallable` AS `isRecallable`,`apo`.`isSummonable` AS `isSummonable`,`ao`.`landblock` AS `landblock`,`ao`.`posX` AS `posX`,`ao`.`posY` AS `posY`,`ao`.`posZ` AS `posZ`,`ao`.`qW` AS `qW`,`ao`.`qX` AS `qX`,`ao`.`qY` AS `qY`,`ao`.`qZ` AS `qZ`,`ao`.`weenieClassId` AS `weenieClassId`,`ao`.`aceObjectDescriptionFlags` AS `aceObjectDescriptionFlags`,`ao`.`animationFrameId` AS `animationFrameId`,`ao`.`currentMotionState` AS `currentMotionState`,`ao`.`iconId` AS `iconId`,`ao`.`iconOverlayId` AS `iconOverlayId`,`ao`.`iconUnderlayId` AS `iconUnderlayId`,`ao`.`modelTableId` AS `modelTableId`,`ao`.`motionTableId` AS `motionTableId`,`ao`.`physicsDescriptionFlag` AS `physicsDescriptionFlag`,`ao`.`playScript` AS `playScript`,`ao`.`physicsTableId` AS `physicsTableId`,`ao`.`soundTableId` AS `soundTableId`,`ao`.`weenieHeaderFlags` AS `weenieHeaderFlags`,`ao`.`spellId` AS `spellId`,`ao`.`defaultScript` AS `defaultScript`,`ao`.`itemType` AS `itemType` from ((`vw_ace_object` `ao` left join `ace_portal_object` `apo` on((`ao`.`aceObjectId` = `apo`.`aceObjectId`))) left join `ace_position` `ap` on(((`apo`.`aceObjectId` = `ap`.`aceObjectId`) and (`ap`.`positionType` = 2)))) where (`ao`.`itemType` = 65536)) */;
+
 /*View structure for view vw_ace_weenie_class */
 
 /*!50001 DROP TABLE IF EXISTS `vw_ace_weenie_class` */;
@@ -161,5 +222,6 @@ DROP TABLE IF EXISTS `vw_teleport_location`;
 /*!50001 CREATE ALGORITHM=UNDEFINED DEFINER=`root`@`localhost` SQL SECURITY DEFINER VIEW `vw_teleport_location` AS (select `apoi`.`name` AS `name`,`ap`.`landblock` AS `landblock`,`ap`.`posX` AS `posX`,`ap`.`posY` AS `posY`,`ap`.`posZ` AS `posZ`,`ap`.`qW` AS `qW`,`ap`.`qX` AS `qX`,`ap`.`qY` AS `qY`,`ap`.`qZ` AS `qZ` from (`ace_poi` `apoi` join `ace_position` `ap` on((`apoi`.`positionId` = `ap`.`positionId`))) where (`ap`.`positionType` = 28)) */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
 /*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
