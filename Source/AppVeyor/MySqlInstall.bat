28014cd09bfa350cebfb786561a000283863be57
Ogmage78 <cfscott@gmail.com>
2017-05-21 22:12:13 -0500

    Update apveyor bat file

--- Source/AppVeyor/MySqlInstall.bat
+++ Source/AppVeyor/MySqlInstall.bat
@@ -5,3 +5,4 @@
 "C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_character < database\Updates\Character\01_2017_04_26_CharacterLastUpdate.sql
 "C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_character < database\Updates\Character\01_2017_05_01_vw_ace_character.sql
 "C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_character < database\Updates\World\01_05_21_2017_viewUpdate.sql
+"C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_character < database\Updates\World\02_05_21_2017_ace_portal_object.sql
