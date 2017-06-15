e9c7020984327776a1eeb3f207e5628f53e2a57f
Ogmage78 <cfscott@gmail.com>
2017-06-14 21:58:30 -0500

    Added first part of wield item.

---
+++ Source/ACE/Network/GameAction/Actions/GameActionGetAndWieldItem.cs
@@ -0,0 +1,15 @@
+﻿using ACE.Network.GameEvent.Events;
+
+namespace ACE.Network.GameAction.Actions
+{
+    public static class GameActionGetAndWieldItem
+    {
+        [GameAction(GameActionType.EvtInventoryGetAndWieldItem)]
+        public static void Handle(ClientMessage message, Session session)
+        {
+            uint itemGuid = message.Payload.ReadUInt32();
+            uint location = message.Payload.ReadUInt32();
+            session.Network.EnqueueSend(new GameEventWieldItem(session, itemGuid, location));
+        }
+    }
+}
