77313985367af1b090c8772266eecce60e659e35
OptimShi <OptimShi@users.noreply.github.com>
2017-06-17 14:50:50 -0700

    Grammar fix.

--- Source/ACE/Network/GameMessages/Messages/GameMessageObjDescEvent.cs
+++ Source/ACE/Network/GameMessages/Messages/GameMessageObjDescEvent.cs
@@ -4,7 +4,7 @@ namespace ACE.Network.GameMessages.Messages
 {
     /// <summary>
     /// via Skunkwors Protocol Docs (F625: Change Model)
-    /// Sent whenever a character changes their clothes. It contains the entire description of what their wearing (and possibly their facial features as well). 
+    /// Sent whenever a character changes their clothes. It contains the entire description of what they're wearing (and possibly their facial features as well). 
     /// This message is only sent for changes, when the character is first created, the body of this message is included inside the creation message.
     /// </summary>
     public class GameMessageObjDescEvent : GameMessage
