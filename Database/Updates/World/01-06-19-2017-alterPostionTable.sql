3bc0e2c446fd525b1b225814ec6cca636a02b4c3
Charles <cfscott@gmail.com>
2017-06-20 18:35:14 -0500

    Update 01-06-19-2017-alterPostionTable.sql
    put back accidental edit

--- Database/Updates/World/01-06-19-2017-alterPostionTable.sql
+++ Database/Updates/World/01-06-19-2017-alterPostionTable.sql
@@ -1,6 +1,6 @@
 ALTER TABLE `ace_world`.`ace_position`   
   DROP COLUMN `landblock` ,
-  ADD COLUMN `landblock` INT(5) UNSIGNED AS ( ((landblockraw >> 16) & 0xFFFF)) STORED,
+  ADD COLUMN `landblock` INT(5) UNSIGNED AS ( ((landblockraw >> 16) & 0xFFFF)) VIRTUAL,
   DROP COLUMN `cell` ,
   DROP INDEX `idx_landblock`,
   DROP INDEX `idx_cell`,
