6816b6eccc5e30eabad7203d738d0804d9501ac1
fantoms <fantoms@users.noreply.github.com>
2017-06-23 07:49:51 -0500

    * Added Shutdown control. (#406)

--- Source/ACE/ACE.cs
+++ Source/ACE/ACE.cs
@@ -23,6 +23,7 @@ namespace ACE
             Console.Title = @"ACEmulator";
 
             ConfigManager.Initialize();
+            ServerManager.Initialize();
             DatabaseManager.Initialize();
             DbManager.Initialize();
             AssetManager.Initialize();
