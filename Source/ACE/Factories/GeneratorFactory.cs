e76823a8a3f95ff54b25b255dd885b73adeba5d4
Lidefeath <lidefeath@gmx.net>
2017-06-30 20:31:15 +0200

    Renamed generatorId to generatorWeenieClassId

--- Source/ACE/Factories/GeneratorFactory.cs
+++ Source/ACE/Factories/GeneratorFactory.cs
@@ -182,7 +182,7 @@ namespace ACE.Factories
             if (index < 0)
                 index = ~index; // bitwise compliment of the index returned from BinarySearch for the real index
 
-            return generatorObjects[index].GeneratorId;
+            return generatorObjects[index].GeneratorWeenieClassId;
         }
     }
 }
