8ed20868e920e8fe88fcd43541ff752e24df15a5
Mogwai <mogwai.thefurry@gmail.com>
2017-07-03 21:39:22 -0400

    forgot db scripts and appveyor script

--- Source/AppVeyor/MySqlInstall.bat
+++ Source/AppVeyor/MySqlInstall.bat
@@ -10,4 +10,6 @@ REM execute Update Scripts
 REM "C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_shard < database\updates\authentication\changeme.sql
 REM "C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_shard < database\updates\shard\changeme.sql
 REM "C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_shard < database\updates\world\changeme.sql
+"C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_shard < database\updates\shard\001-add-propertyIndex.sql
+"C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_world < database\updates\world\001-add-propertyIndex.sql
 "C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_shard < database\updates\shard\01-2017-07-06-LoginTimestamp.sql
