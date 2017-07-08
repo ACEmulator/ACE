e8c6cd7105084df47ee20c672ae1e0002ca2d3ef
Ty Conner <tyconner@itcproductions.com>
2017-07-08 12:59:57 -0400

    appveyor WIP 2

--- Source/AppVeyor/MySqlInstall.bat
+++ Source/AppVeyor/MySqlInstall.bat
@@ -13,9 +13,9 @@ REM "C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root
 REM curl -O http://www.openss7.org/repos/tarballs/strx25-0.9.2.1.tar.bz2
 
 REM $latestTag=$(git describe --abbrev=0 --tags)
-$latestTag=$(git ls-remote --tags https://github.com/ACEmulator/ACE-World.git | awk '{print $2}' | grep -v '{}' | awk -F"/" '{print $3}' | sort -n -t. -k1,1 -k2,2 -k3,3 | tail -n 1.chomp)
+set latestTag=$(git ls-remote --tags https://github.com/ACEmulator/ACE-World.git | awk '{print $2}' | grep -v '{}' | awk -F"/" '{print $3}' | sort -n -t. -k1,1 -k2,2 -k3,3 | tail -n 1.chomp)
 
-echo $latestTag
+echo latestTag
 REM curl -L https://github.com/reactiveui/ReactiveUI/releases/download/$latestTag/ReactiveUI-$latestTag.zip
 
 REM execute Update Scripts for World Database
