ff8a084d2b39c6c7d4334cea5f8dc338a95fc0be
Ty Conner <tyconner@itcproductions.com>
2017-07-12 03:06:15 -0400

    Altered PortalBitmask slightly

--- Source/ACE.Entity/Enum/PortalBitmask.cs
+++ Source/ACE.Entity/Enum/PortalBitmask.cs
@@ -9,12 +9,12 @@ namespace ACE.Entity.Enum
     [Flags]
     public enum PortalBitmask : byte
     {
-        Undef       = 0x00,
-        Default     = 0x01,
-        NoPk        = 0x02,
-        NoPKLite    = 0x04,
-        NoNPK       = 0x08,
-        NoSummon    = 0x10,
-        NoRecall    = 0x20
+        Undef           = 0x00,
+        Unrestricted    = 0x01,
+        NoPk            = 0x02,
+        NoPKLite        = 0x04,
+        NoNPK           = 0x08,
+        NoSummon        = 0x10,
+        NoRecall        = 0x20
     }
 }
