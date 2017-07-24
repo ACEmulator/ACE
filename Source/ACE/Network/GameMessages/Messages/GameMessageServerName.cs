083817ea03982648ed62c3dfe2973db048c479ae
Ty Conner <tyconner@itcproductions.com>
2017-07-23 20:49:42 -0400

    Changed GameMessageServerName to include connection count and max.

--- Source/ACE/Network/GameMessages/Messages/GameMessageServerName.cs
+++ Source/ACE/Network/GameMessages/Messages/GameMessageServerName.cs
@@ -4,11 +4,11 @@ namespace ACE.Network.GameMessages.Messages
 {
     public class GameMessageServerName : GameMessage
     {
-        public GameMessageServerName(string serverName)
+        public GameMessageServerName(string serverName, int currentConnections = 0, int maxConnections = -1)
             : base(GameMessageOpcode.ServerName, GameMessageGroup.Group09)
         {
-            Writer.Write(0u);
-            Writer.Write(0u);
+            Writer.Write(currentConnections);
+            Writer.Write(maxConnections);
             Writer.WriteString16L(serverName);
         }
     }
