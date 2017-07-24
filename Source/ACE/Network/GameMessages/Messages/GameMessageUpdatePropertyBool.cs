cf63d8b38509771bc49fe6e706ed706b30224ccc
Ty Conner <tyconner@itcproductions.com>
2017-07-24 03:54:20 -0400

    Fix sequence error

--- Source/ACE/Network/GameMessages/Messages/GameMessageUpdatePropertyBool.cs
+++ Source/ACE/Network/GameMessages/Messages/GameMessageUpdatePropertyBool.cs
@@ -8,7 +8,8 @@ namespace ACE.Network.GameMessages.Messages
     {
         public GameMessageUpdatePropertyBool(WorldObject worldObject, PropertyBool property, bool value) : base(GameMessageOpcode.PublicUpdatePropertyBool, GameMessageGroup.Group09)
         {
-            Writer.Write(worldObject.Sequences.GetNextSequence(Sequence.SequenceType.PrivateUpdatePropertyBool));
+            // Writer.Write(worldObject.Sequences.GetNextSequence(Sequence.SequenceType.PrivateUpdatePropertyBool));
+            Writer.Write(0u); // Public updates seem to always be 0? It's possible they're worldObject.Sequences.GetCurrentSequence(Sequence.SequenceType.PrivateUpdatePropertyBool) as well
             Writer.Write(worldObject.Guid.Full);
             Writer.Write((uint)property);
             Writer.Write(Convert.ToUInt32(value));
