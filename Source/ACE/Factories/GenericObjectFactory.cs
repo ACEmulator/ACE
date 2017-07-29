dbf2f1ed0f81c1131053b6147a3f7b5bb86d7a8c
Ogmage78 <cfscott@gmail.com>
2017-07-28 20:43:51 -0500

    Wip

--- Source/ACE/Factories/GenericObjectFactory.cs
+++ Source/ACE/Factories/GenericObjectFactory.cs
@@ -16,34 +16,34 @@ namespace ACE.Factories
             foreach (var aceO in sourceObjects)
             {
                 var oType = (ItemType)aceO.ItemType;
-                var oDescFlag = (ObjectDescriptionFlag)aceO.AceObjectDescriptionFlags;
+                ////var oDescFlag = (ObjectDescriptionFlag)aceO.AceObjectDescriptionFlags;
 
-                if (aceO.GeneratorStatus ?? false)  // Generator
-                {
-                    aceO.Location = aceO.Location.InFrontOf(-2.0);
-                    aceO.Location.PositionZ = aceO.Location.PositionZ - 0.5f;
-                    results.Add(new Generator(new ObjectGuid(aceO.AceObjectId), aceO));
-                    aceO.GeneratorEnteredWorld = true;
-                    var objectList = GeneratorFactory.CreateWorldObjectsFromGenerator(aceO) ?? new List<WorldObject>();
-                    objectList.ForEach(o => results.Add(o));
-                    continue;
-                }
+                ////if (aceO.GeneratorStatus ?? false)  // Generator
+                ////{
+                ////    aceO.Location = aceO.Location.InFrontOf(-2.0);
+                ////    aceO.Location.PositionZ = aceO.Location.PositionZ - 0.5f;
+                ////    results.Add(new Generator(new ObjectGuid(aceO.AceObjectId), aceO));
+                ////    aceO.GeneratorEnteredWorld = true;
+                ////    var objectList = GeneratorFactory.CreateWorldObjectsFromGenerator(aceO) ?? new List<WorldObject>();
+                ////    objectList.ForEach(o => results.Add(o));
+                ////    continue;
+                ////}
 
-                if ((oDescFlag & ObjectDescriptionFlag.LifeStone) != 0)
-                {
-                    results.Add(new Lifestone(aceO));
-                    continue;
-                }
-                if ((oDescFlag & ObjectDescriptionFlag.Portal) != 0)
-                {
-                    results.Add(new Portal(aceO));
-                    continue;
-                }
-                if ((oDescFlag & ObjectDescriptionFlag.Door) != 0)
-                {
-                    results.Add(new Door(aceO));
-                    continue;
-                }
+                ////if ((oDescFlag & ObjectDescriptionFlag.LifeStone) != 0)
+                ////{
+                ////    results.Add(new Lifestone(aceO));
+                ////    continue;
+                ////}
+                ////if ((oDescFlag & ObjectDescriptionFlag.Portal) != 0)
+                ////{
+                ////    results.Add(new Portal(aceO));
+                ////    continue;
+                ////}
+                ////if ((oDescFlag & ObjectDescriptionFlag.Door) != 0)
+                ////{
+                ////    results.Add(new Door(aceO));
+                ////    continue;
+                ////}
 
                 switch (oType)
                 {
@@ -52,7 +52,7 @@ namespace ACE.Factories
                         // Use the DebugObject to assist in building proper objects for weenies
                         // FIXME(ddevec): Some objects have null location.  This freaks out the landblock... ignore them?
                         // && aceO.AceObjectId == 2056994930 Og II debug chest issue create object.
-                        if (aceO.Location != null)
+                        if (aceO.Location != null && aceO.AceObjectId == 2056994836)
                         {
                             results.Add(new DebugObject(aceO));
                         }
