1811f0a843c0c3d7131b5cfecd6012cfbad4da6b
Charles <cfscott@gmail.com>
2017-07-31 10:19:24 -0500

    Expecto Patronum - Spell bar managment (#481)
    * WIP
    
    * Updated Enums and impacted files.
    
    * Working on spell bars
    
    * Wip
    
    * ### 2017-07-30
    [Og II]
    * Added spellbar management and persistance.   This continues the work for learning and persisting spells.
    * Cleaned up some of the enum duplication in GameEventPlayerDescription.cs
    * Added some debug code around universial motion.
    * Added some debug asserts in PlayerDescription
    * To test - use @learnspell to get some spells in your spell book if you do not have any.   They will persist with your character.
    * You can also unlearn them and they will be removed.
    * Now, drag your spells into any configuration in your spell bars you like.   You can add them, remove them or reorder them.
    * They will be saved on our periodic save, or when you log out or you can force a save with the debug command @save-now.
    * That's all for this PR.
    
    * Forgot to add the table.
    
    * Added back the action chains.
    
    * Added accessor properties - fixed path issue in mysqlinstall

--- Source/ACE/Entity/WorldObject.cs
+++ Source/ACE/Entity/WorldObject.cs
@@ -1318,7 +1318,7 @@ namespace ACE.Entity
         /// TODO: implement
         /// </summary>
         public double LastAnimatedTicks { get; set; }
-       
+
         public virtual void PlayScript(Session session) { }
 
         public virtual float ListeningRadius { get; protected set; } = 5f;
@@ -1341,7 +1341,7 @@ namespace ACE.Entity
             get { return (MotionStance?)AceObject.DefaultCombatStyle; }
             set { AceObject.DefaultCombatStyle = (uint?)value; }
         }
-      
+
         public uint? GeneratorId
         {
             get { return AceObject.GeneratorIID; }
@@ -1364,7 +1364,7 @@ namespace ACE.Entity
         {
             get { return AceObject.ItemMaxMana; }
             set { AceObject.ItemMaxMana = value; }
-        }        
+        }
 
         public bool? AdvocateState
         {
@@ -2440,7 +2440,7 @@ namespace ACE.Entity
 
             return physicsDescriptionFlag;
         }
-        
+
         // todo: return bytes of data for network write ? ?
         public void SerializePhysicsData(BinaryWriter writer)
         {
@@ -2464,16 +2464,21 @@ namespace ACE.Entity
                     }
                     else
                     {
+                        // Adding these debug lines - don't think we can hit these, but want to make sure. Og II
+                        log.Debug($"Our flag is set but we have no data length. {this.Guid.Full:X}");
                         writer.Write(0u);
                     }
                 }
                 else
-                {                  
+                {
+                    log.Debug($"Our flag is set but our current motion state is null. {this.Guid.Full:X}");
                     writer.Write(0u);
                 }
             }
             else if ((PhysicsDescriptionFlag & PhysicsDescriptionFlag.AnimationFrame) != 0)
                 writer.Write((AnimationFrame ?? 0u));
+            // TODO: Keep an eye on this, are we sure the client does not just ignore it?   I would think they way it reads by buffer length that this would blow up.
+            // probably an edge case - just watch this - Og II
 
             if ((PhysicsDescriptionFlag & PhysicsDescriptionFlag.Position) != 0)
                 Location.Serialize(writer);
