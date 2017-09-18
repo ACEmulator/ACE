8c0f8cac598a2e8dce7fabb36a8a8dc4ef249c11
Mogwai <mogwai.thefurry@gmail.com>
2017-09-18 12:55:05 -0400

    starter gear file and object structure done, validated parsing with unit test

---
+++ Source/ACE/Entity/StarterItem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ACE.Entity
+{
+    public class StarterItem
+    {
+        [JsonProperty("weenieId")]
+        public uint WeenieId { get; set; }
+
+        [JsonProperty("stacksize")]
+        public ushort StackSize { get; set; }
+    }
+}
