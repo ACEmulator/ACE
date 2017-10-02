e6168e4636db2943b2f40d19b0e7a314fee30c84
OptimShi <OptimShi@users.noreply.github.com>
2017-10-02 16:23:47 -0700

    The Code Alignment Gods have blessed the enumerations, and it is good.

--- Source/ACE.Entity/Enum/MotionStance.cs
+++ Source/ACE.Entity/Enum/MotionStance.cs
@@ -13,24 +13,24 @@ namespace ACE.Entity.Enum
     /// </summary>
     public enum MotionStance : uint
     {
-        Invalid = 0x80000000,
-        UaNoShieldAttack = 0x8000003c, // HandCombat 
-        Standing = 0x8000003d, // NonCombat
-        MeleeNoShieldAttack = 0x8000003e, // SwordCombat
-        BowAttack = 0x8000003f, // BowCombat
-        MeleeShieldAttack = 0x80000040, // SwordShieldCombat
-        CrossBowAttack = 0x80000041, // CrossbowCombat
-        Unused = 0x80000042, // Unused Combat
-        SlingAttack = 0x80000043, // SlingCombat 
+        Invalid              = 0x80000000,
+        UaNoShieldAttack     = 0x8000003c, // HandCombat 
+        Standing             = 0x8000003d, // NonCombat
+        MeleeNoShieldAttack  = 0x8000003e, // SwordCombat
+        BowAttack            = 0x8000003f, // BowCombat
+        MeleeShieldAttack    = 0x80000040, // SwordShieldCombat
+        CrossBowAttack       = 0x80000041, // CrossbowCombat
+        Unused               = 0x80000042, // Unused Combat
+        SlingAttack          = 0x80000043, // SlingCombat 
         TwoHandedSwordAttack = 0x80000044, // 2HandedSwordCombat 
         TwoHandedStaffAttack = 0x80000045, // 2HandedStaffCombat 
-        DualWieldAttack = 0x80000046, // DualWieldCombat 
-        ThrownWeaponAttack = 0x80000047, // ThrownWeaponCombat 
-        Graze = 0x80000048,
-        Spellcasting = 0x80000049, // Magic 
-        BowNoAmmo = 0x800000e8,
-        CrossBowNoAmmo = 0x800000e9,
-        AtlatlCombat = 0x80000138,
-        ThrownShieldCombat = 0x80000139,
+        DualWieldAttack      = 0x80000046, // DualWieldCombat 
+        ThrownWeaponAttack   = 0x80000047, // ThrownWeaponCombat 
+        Graze                = 0x80000048,
+        Spellcasting         = 0x80000049, // Magic 
+        BowNoAmmo            = 0x800000e8,
+        CrossBowNoAmmo       = 0x800000e9,
+        AtlatlCombat         = 0x80000138,
+        ThrownShieldCombat   = 0x80000139,
     }
 }
