f21dfc5244d8880d21e495eab6804a13f0fdf80a
Slushnas <Slushnas@users.noreply.github.com>
2017-10-06 05:05:05 -0700

    Added some chat enums that were found in the client. (#525)

--- Source/ACE.Entity/Enum/GroupChatType.cs
+++ Source/ACE.Entity/Enum/GroupChatType.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 
 namespace ACE.Entity.Enum
 {
@@ -10,79 +10,105 @@ namespace ACE.Entity.Enum
     public enum GroupChatType
     {
         /// <summary>
+        /// Unknown
+        /// </summary>
+        /// Included for completeness since it is listed in the client
+        Unknown                = 0x00000000,
+
+        /// <summary>
         /// @abuse - Abuse Channel
         /// </summary>
-        TellAbuse           = 0x00000001,
+        TellAbuse              = 0x00000001,
 
         /// <summary>
         /// @admin - Admin Channel (@ad)
         /// </summary>
-        TellAdmin           = 0x00000002,
+        TellAdmin              = 0x00000002,
 
         /// <summary>
         /// @audit - Audit Channel (@au)
         /// This channel was used to echo copies of enforcement commands (such as: ban, gag, boot) to all other online admins
         /// </summary>
-        TellAudit           = 0x00000004,
+        TellAudit              = 0x00000004,
 
         /// <summary>
         /// @av1 - Advocate Channel (@advocate) (@advocate1)
         /// </summary>
-        TellAdvocate        = 0x00000008,
+        TellAdvocate           = 0x00000008,
 
         /// <summary>
         /// @av2 - Advocate2 Channel (@advocate2)
         /// </summary>
-        TellAdvocate2       = 0x00000010,
+        TellAdvocate2          = 0x00000010,
 
         /// <summary>
         /// @av3 - Advocate3 Channel (@advocate3)
         /// </summary>
-        TellAdvocate3       = 0x000000020,
+        TellAdvocate3          = 0x000000020,
 
         /// <summary>
         /// @sent - Sentinel Channel (@sentinel)
         /// </summary>
-        TellSentinel        = 0x000000200,
+        TellSentinel           = 0x000000200,
 
         /// <summary>
         /// @[command name tbd] - Help Channel
         /// </summary>
-        TellHelp            = 0x000000400,
+        TellHelp               = 0x000000400,
 
         /// <summary>
         /// @f - Tell Fellowship
         /// </summary>
-        TellFellowship      = 0x00000800,
+        TellFellowship         = 0x00000800,
 
         /// <summary>
         /// @v - Tell Vassals
         /// </summary>
-        TellVassals         = 0x00001000,
+        TellVassals            = 0x00001000,
 
         /// <summary>
         /// @p - Tell Patron
         /// </summary>
-        TellPatron          = 0x00002000,
+        TellPatron             = 0x00002000,
 
         /// <summary>
         /// @m - Tell Monarch
         /// </summary>
-        TellMonarch         = 0x00004000,
+        TellMonarch            = 0x00004000,
 
         /// <summary>
         /// @c - Tell Co-Vassals
         /// </summary>
-        TellCoVassals       = 0x01000000,
+        TellCoVassals          = 0x01000000,
 
         /// <summary>
         /// @allegiance broadcast - Tell All Allegiance Members
         /// </summary>
-        AllegianceBroadcast = 0x02000000,
+        AllegianceBroadcast    = 0x02000000,
 
         /// <summary>
         /// Player is now the leader of this fellowship.
         /// </summary>
-        FellowshipBroadcast = 0x04000000
+        FellowshipBroadcast    = 0x04000000,
+
+        /// <summary>
+        /// Celestial Hand Society
+        /// </summary>
+        CelestialHandBroadcast = 0x08000000,
+
+        /// <summary>
+        /// Eldrytch Web Society
+        /// </summary>
+        EldrytchWebBroadcast   = 0x10000000,
+
+        /// <summary>
+        /// Radiant Blood Society
+        /// </summary>
+        RadiantBloodBroadcast  = 0x20000000,
+
+        /// <summary>
+        /// Olthoi
+        /// </summary>
+        OlthoiBroadcast        = 0x40000000
     }
 }
