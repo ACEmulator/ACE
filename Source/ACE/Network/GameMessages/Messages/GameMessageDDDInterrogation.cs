e450d8f0df330e163b62f1661c38a6e507cc1be3
Felix A. Croes <felix@dworkin.nl>
2017-10-10 23:22:37 +0200

    DDDInterrogation uses 32 bit quantities, not 64 bit.
    Though the client doesn't seem to mind.

--- Source/ACE/Network/GameMessages/Messages/GameMessageDDDInterrogation.cs
+++ Source/ACE/Network/GameMessages/Messages/GameMessageDDDInterrogation.cs
@@ -7,12 +7,12 @@ namespace ACE.Network.GameMessages.Messages
         public GameMessageDDDInterrogation()
             : base(GameMessageOpcode.DDD_Interrogation, GameMessageGroup.DatabaseQueue)
         {
-            Writer.Write(1ul);
-            Writer.Write(1ul);
-            Writer.Write(1ul);
-            Writer.Write(2ul);
-            Writer.Write(0ul);
-            Writer.Write(1ul);
+            Writer.Write(1u);
+            Writer.Write(1u);
+            Writer.Write(1u);
+            Writer.Write(2u);
+            Writer.Write(0u);
+            Writer.Write(1u);
         }
     }
 }
