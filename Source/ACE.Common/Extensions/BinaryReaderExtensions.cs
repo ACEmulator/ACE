7dbe825d953720ba85fa41245fbf760316c644d3
Mogwai <mogwai.thefurry@gmail.com>
2017-10-20 15:49:38 -0400

    updated ReadString32L to suck a little less.  i dont think this is 100% correct though.

--- Source/ACE.Common/Extensions/BinaryReaderExtensions.cs
+++ Source/ACE.Common/Extensions/BinaryReaderExtensions.cs
@@ -12,11 +12,28 @@ namespace ACE.Common.Extensions
         public static string ReadString32L(this BinaryReader reader)
         {
             uint length = reader.ReadUInt32();
-            // return length != 0u ? new string(reader.ReadChars((int)length)) : string.Empty;
+
+            // 32L strings are crazy.  the only place this is known as of time of writing this is in the
+            // Login header packet.  it's a DWORD of the data length, followed by a packed word of the 
+            // string length.  for most cases, this means the string comes out with a 1 or 2 character
+            // prefix that just needs to get tossed.
+
+            if (length == 0)
+                return "";
+
+            reader.Skip(1);
+            length--;
+
+            if (length > 255)
+            {
+                reader.Skip(1);
+                length--;
+            }
 
             string rdrStr = (length != 0 ? new string(reader.ReadChars((int)length)) : string.Empty);
 
-            // client pads string length to be a multiple of 4 including the 4 bytes for length
+            // in the login header, this is completely unnecessary as it's the end of the packet.  if
+            // we find this is ever used somewhere else, we would need to validate it.
             reader.Skip(CalculatePadMultiple(sizeof(uint) + length, 4u));
             return rdrStr;
         }
