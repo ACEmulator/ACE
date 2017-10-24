b043213d3eaa980cd90f8aa06704b3c748d07733
Ogmage78 <cfscott@gmail.com>
2017-10-23 23:38:11 -0500

    wip

--- Source/ACE.Entity/Vector3.cs
+++ Source/ACE.Entity/Vector3.cs
@@ -5,6 +5,7 @@ namespace ACE.Entity
 {
     /// <summary>
     /// Vector3 with ACE Serialize
+    /// Adding methods ported from Anonymous' physics code.   Thanks for all the hard work! Og II
     /// </summary>
     public class AceVector3
     {
@@ -13,6 +14,58 @@ namespace ACE.Entity
         public AceVector3(float x, float y, float z)
         {
             vector.X = x; vector.Y = y; vector.Z = z;
+        }
+
+        /// <summary>
+        /// Copy one vector3 to another.
+        /// </summary>
+        /// <param name="a">Vector3</param>
+        /// <param name="b">Vector3</param>
+        public static void Vector3Copy(Vector3 a, Vector3 b)
+        {
+            a.X = b.X;
+            a.Y = b.Y;
+            a.Z = b.Z;
+        }
+
+        /// <summary>
+        /// Vector4 is used internally to convert vec3 vectors into a suitable form for multiplying with matrices.
+        /// Generally you want to have a W of 0 for direction vectors, and a W of 1 for position vectors.
+        /// This ensures that direction vectors aren't translated, and that position vectors are translated -
+        /// </summary>
+        /// <param name="a">Vector4</param>
+        /// <param name="b">Vector3</param>
+        /// <param name="w">W component</param>
+        public static void Vector3ToVector4(out Vector4 a, Vector3 b, float w)
+        {
+            a.X = b.X;
+            a.Y = b.Y;
+            a.Z = b.Z;
+            a.W = w;
+        }
+
+        /// <summary>
+        /// Add two vector3's together
+        /// </summary>
+        /// <param name="a">Vector3</param>
+        /// <param name="b">Vector3</param>
+        public static void Vector3Add(Vector3 a, Vector3 b)
+        {
+            a.X += b.X;
+            a.Y += b.Y;
+            a.Z += b.Z;
+        }
+
+        /// <summary>
+        /// Subtract two vector3's from each other
+        /// </summary>
+        /// <param name="a">Vector3</param>
+        /// <param name="b">Vector3</param>
+        public static void Vector3Subtract(Vector3 a, Vector3 b)
+        {
+            a.X -= b.X;
+            a.Y -= b.Y;
+            a.Z -= b.Z;
         }
 
         public void Update(float x, float y, float z)
