60e3647ae047816d6384cb3977655ae21bee5e28
Ogmage78 <cfscott@gmail.com>
2017-10-24 20:43:37 -0500

    Wip

---
+++ Source/ACE.Entity/AceMatrix4X4.cs
@@ -0,0 +1,69 @@
+﻿using System.IO;
+using System.Numerics;
+
+// Matrix4x4
+// void Mat4_Identity(mat4x4t &m);
+// void Mat4_Copy(mat4x4t &a, mat4x4t &b);
+// void Mat4_Transpose(mat4x4t &a);
+// void Mat4_Add(mat4x4t &a, mat4x4t &b);
+// void Mat4_Sub(mat4x4t &a, mat4x4t &b);
+// void Mat4_Scale(mat4x4t &a, float s);
+// void Mat4_Mul_Mat4(mat4x4t &a, mat4x4t &b);
+// void Mat4_Mul_Vec4(vec4t &v, mat4x4t &m);
+// void Mat4_Mul_Vec3(vec3t &v3, mat4x4t &m);
+// void Mat4_Translate(mat4x4t &m, float x, float y, float z);
+// void Mat4_Scale(mat4x4t &m, float x, float y, float z);
+// void Mat4_ScaleUniform(mat4x4t &m, float s);
+// void Mat4_Rotate(mat4x4t &m, float angle, vec3t &axis);
+// bool Mat4_Invert(mat4x4t &m);
+namespace ACE.Entity
+{
+    /// <summary>
+    /// Matrix4X4
+    /// Adding methods ported from Anonymous' physics code.   Thanks for all the hard work! Og II
+    /// </summary>
+    public class AceMatrix4X4
+    {
+        /// <summary>
+        ///  4x4 matrix
+        /// internally stored as column-major (i.e. OpenGL style, float[x][y])
+        /// </summary>
+        private readonly float[,] matrix4X4 = new float[4, 4];
+
+        /// <summary>
+        /// Gets the multiplicative identity matrix.
+        /// </summary>
+        /// <param name="matrix"></param>
+        public static void Matrix4Identity(ref float[,] matrix)
+        {
+            // x,y (col major)
+            matrix[0, 0] = 1.0f;
+            matrix[1, 0] = 0.0f;
+            matrix[2, 0] = 0.0f;
+            matrix[3, 0] = 0.0f;
+            matrix[0, 1] = 0.0f;
+            matrix[1, 1] = 1.0f;
+            matrix[2, 1] = 0.0f;
+            matrix[3, 1] = 0.0f;
+            matrix[0, 2] = 0.0f;
+            matrix[1, 2] = 0.0f;
+            matrix[2, 2] = 1.0f;
+            matrix[3, 2] = 0.0f;
+            matrix[0, 3] = 0.0f;
+            matrix[1, 3] = 0.0f;
+            matrix[2, 3] = 0.0f;
+            matrix[3, 3] = 1.0f;
+        }
+
+        /// <summary>
+        /// Matrix4 Copy - Copies from the array a to the array b
+        /// </summary>
+        /// <param name="fromArray">array we are copying from</param>
+        /// <param name="toArray">array we are copying to</param>
+        public static void Matrix4Copy(ref float[,] fromArray, ref float[,] toArray)
+        {
+            fromArray.CopyTo(toArray, 0);
+        }
+
+    }
+}
--- Source/ACE.Entity/Vector3.cs
+++ Source/ACE.Entity/Vector3.cs
@@ -29,14 +29,32 @@ using System.Numerics;
 // USE:  public float Length() : Vector3 aVector.Length();
 
 // void Vec3_Normalize(vec3t &a);
+// Notes: make a vector unit length (length = 1).
+// USE: public static Vector3 Normalize(Vector3 value) : Vector3 normVector = Vector3.Normalize(value);
 
 // float Vec3_Dot(vec3t &a, vec3t &b);
+// USE: public static float Dot(Vector3 vector1, Vector3 vector2) : float result = Vector3.Dot(vector1, vector2)
+// Notes: dot product (aka inner product)
+// *** if you need to use this for lighting or angle calculations, normalize the vectors first! ***
+// for normalized vectors a & b, effectively returns the cosine of the angle between them.
 
 // void Vec3_Cross(vec3t &result, vec3t &a, vec3t &b);
+// USE: public static Vector3 Cross(Vector3 vector1, Vector3 vector2) : Vector3 crossVector = Vector3.Cross(vector1, vector2)
+// Notes: cross product returns a vector perpendicular to the two input vectors (the "normal vector").
+// If the inputs are of unit length, then the result vector is also of unit length.
+// AC calculates the slope of a triangle by taking the Z value of the normal.
 
 // void Vec3_Reflect(vec3t &r, vec3t &v, vec3t &n);
+// USE: public static Vector3 Reflect(Vector3 vector, Vector3 normal) : Vector3 reflVector = Vector3.Reflect(vector, normal)
+// Notes: // vector reflection  inputs: vector v, normal vector n (normal vector of the plane you want to reflect from)
+// resulting vector in r.
 
 // bool Vec3_EqZero(vec3t &a, float tolerance);
+// USE:  public static bool Vector3EqualZero(Vector3 a) : bool isZero = Vector3EqualZero(vector);
+
+// public const double M_PI = (3.1415926535897932384626433832795);
+// USE: Math.PI
+
 
 
 
@@ -69,16 +87,25 @@ namespace ACE.Entity
         }
 
         /// <summary>
-        /// This method retures the square of the length of the vector.
+        /// This method returns the square of the length of the vector.
         /// It may not be used, it looked like it was called to get the length which we have built in.
         /// </summary>
-        /// <param name="a">The lenght of the vector squared.</param>
+        /// <param name="a">The length of the vector squared.</param>
         /// <returns></returns>
         public static float Vector3LengthSquared(Vector3 a)
         {
             return ((a.X * a.X) + (a.Y * a.Y) + (a.Z * a.Z));
         }
 
+        /// <summary>
+        /// Compare to see if a vector is 0 within a given fault tolerance.
+        /// </summary>
+        /// <param name="a">Vector3 you want to compare to zero.</param>
+        /// <returns></returns>
+        public static bool Vector3EqualZero(Vector3 a)
+        {
+            return (AceMath3d.FloatCompareEqual(a.X, 0.0f) && AceMath3d.FloatCompareEqual(a.Y, 0.0f) && AceMath3d.FloatCompareEqual(a.Z, 0.0f));
+        }
 
         public void Update(float x, float y, float z)
         {
