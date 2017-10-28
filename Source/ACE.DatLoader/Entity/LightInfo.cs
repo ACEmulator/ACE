6d927fc69f639cd5276453adf5ab9b3ad7363020
OptimShi <OptimShi@users.noreply.github.com>
2017-10-27 22:55:52 -0700

    Update SetupModel (#556)

---
+++ Source/ACE.DatLoader/Entity/LightInfo.cs
@@ -0,0 +1,32 @@
+﻿using ACE.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACE.DatLoader.Entity
+{
+    public class LightInfo
+    {
+        Position ViewerspaceLocation { get; set; }
+        uint Color { get; set; } // _RGB Color. Red is bytes 3-4, Green is bytes 5-6, Blue is bytes 7-8. Bytes 1-2 are always FF (?)
+        float Intensity { get; set; }
+        float Falloff { get; set; }
+        float ConeAngle { get; set; }
+
+        public static LightInfo Read(DatReader datReader)
+        {
+            LightInfo obj = new LightInfo();
+
+            obj.ViewerspaceLocation = PositionExtensions.ReadPosition(datReader);
+            obj.Color = datReader.ReadUInt32();
+            obj.Intensity = datReader.ReadSingle();
+            obj.Falloff = datReader.ReadSingle();
+            obj.ConeAngle = datReader.ReadSingle();
+
+            return obj;
+        }
+
+    }
+}
---
+++ Source/ACE.DatLoader/Entity/LocationType.cs
@@ -0,0 +1,21 @@
+﻿using ACE.Entity;
+
+namespace ACE.DatLoader.Entity
+{
+    public class LocationType
+    {
+
+        public uint PartId;
+        public Position Frame;
+
+        public static LocationType Read(DatReader datReader)
+        {
+            LocationType obj = new LocationType();
+
+            obj.PartId = datReader.ReadUInt32();
+            obj.Frame = PositionExtensions.ReadPosition(datReader);
+
+            return obj;
+        }
+    }
+}
