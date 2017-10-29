f9b921ee290a1590887d8e7981222080efabcc4a
Ogmage78 <cfscott@gmail.com>
2017-10-28 22:01:24 -0500

    last check in

---
+++ Source/ACE.Entity/Enum/PlaneSide.cs
@@ -0,0 +1,11 @@
+﻿namespace ACE.Entity.Enum
+{
+    public enum PlaneSide
+    {
+        kSidePOSITIVE = 0,
+        kSideNEGATIVE,
+        kSideIN_PLANE,
+        kSideCROSSING,
+        kSidednessMax
+    }
+}
