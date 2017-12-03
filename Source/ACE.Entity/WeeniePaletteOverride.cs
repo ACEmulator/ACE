7b55cf8198d682814bbacb9ad6f5e5d9bbe65e02
Mag-nus <Mag-nus@users.noreply.github.com>
2017-12-02 18:57:02 -0600

    Revert ".net framework to .net standard for ACE.Common and ACE.Entity"
    This reverts commit 288c59ba815a2022937bb0bcc2a9d1a057f4ba7e.

---
+++ Source/ACE.Entity/WeeniePaletteOverride.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ACE.Common;
+using MySql.Data.MySqlClient;
+
+namespace ACE.Entity
+{
+    [DbTable("ace_object_palette_change")]
+    [DbList("ace_object_palette_change", "aceObjectId")]
+    public class WeeniePaletteOverride : ICloneable
+    {
+        [DbField("aceObjectId", (int)MySqlDbType.UInt32, IsCriteria = true)]
+        public uint AceObjectId { get; set; }
+
+        [DbField("subPaletteId", (int)MySqlDbType.UInt32)]
+        public uint SubPaletteId { get; set; }
+
+        [DbField("offset", (int)MySqlDbType.UInt16)]
+        public ushort Offset { get; set; }
+
+        [DbField("length", (int)MySqlDbType.UInt16)]
+        public ushort Length { get; set; }
+
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+    }
+}
