4ae221284e41e82cc479770c163b712ceba34dc9
Ty Conner <tyconner@itcproductions.com>
2017-12-19 01:54:56 -0500

    Update AppVeyor

--- Source/AppVeyor/MySqlInstall.bat
+++ Source/AppVeyor/MySqlInstall.bat
@@ -13,10 +13,10 @@ REM execute Update Scripts for Shard Database
 REM "C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_shard < database\updates\shard\changeme.sql
 
 REM Download latest ACE-World database, extract and import it
-appveyor DownloadFile https://github.com/ACEmulator/ACE-World/releases/download/v0.3.0/ACE-World-db-v0.3.0.sql.zip
-7z x ACE-World-db-v0.3.0.sql.zip
-"C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_world < ACE-World-db-v0.3.0.sql
+appveyor DownloadFile https://github.com/ACEmulator/ACE-World/releases/download/v0.3.1/ACE-World-db-v0.3.1.sql.zip
+7z x ACE-World-db-v0.3.1.sql.zip
+"C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_world < ACE-World-db-v0.3.1.sql
 
 REM execute Update Scripts for World Database
 REM "C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_world < database\updates\world\changeme.sql
-"C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_world < database\updates\world\2017-06-30-00-generator-chains-testdata.sql
+"C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_world < database\updates\world\2017-12-19-00-generator-test-data.sql
