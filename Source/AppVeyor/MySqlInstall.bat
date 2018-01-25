19501a6678e5f4bef6eeb6a6723f00ea22f1f65c
Ty Conner <tyconner@itcproductions.com>
2018-01-25 01:51:27 -0500

    Update MySqlInstall.bat
    Update CI to use latest 16PY database release

--- Source/AppVeyor/MySqlInstall.bat
+++ Source/AppVeyor/MySqlInstall.bat
@@ -16,14 +16,15 @@ REM execute Update Scripts for World Database
 REM "C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_world < database\updates\world\changeme.sql
 REM "C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_world < database\updates\world\2018-01-06-00-generator-profiles-table.sql
 REM "C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_world < database\updates\world\2018-01-06-01-landblock-links.sql
+REM "C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_world < database\updates\world\2018-01-07-00-inventory-shade.sql
 
 REM Download latest ACE-World database, extract and import it
-appveyor DownloadFile https://github.com/ACEmulator/ACE-World-16PY/releases/download/v0.0.2/ACE-World-16PY-db-v0.0.2.sql.zip
-7z x ACE-World-16PY-db-v0.0.2.sql.zip
-"C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_world < ACE-World-16PY-db-v0.0.2.sql
+appveyor DownloadFile https://github.com/ACEmulator/ACE-World-16PY/releases/download/v0.0.3/ACE-World-16PY-db-v0.0.3.sql.zip
+7z x ACE-World-16PY-db-v0.0.3.sql.zip
+"C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_world < ACE-World-16PY-db-v0.0.3.sql
 
 REM execute Update Scripts for World Database
 REM "C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_world < database\updates\world\changeme.sql
 REM "C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_world < database\updates\world\2018-01-06-00-generator-profiles-table.sql
 REM "C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_world < database\updates\world\2018-01-06-01-landblock-links.sql
-"C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_world < database\updates\world\2018-01-07-00-inventory-shade.sql
+REM "C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_world < database\updates\world\2018-01-07-00-inventory-shade.sql
