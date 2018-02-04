edb2c16d0879e92baefc6e8aec52f13828ff91e3
Ty Conner <tyconner@itcproductions.com>
2018-02-04 17:59:15 -0500

    Correct path

--- Source/AppVeyor/MySqlInstall.bat
+++ Source/AppVeyor/MySqlInstall.bat
@@ -19,7 +19,7 @@ REM Download latest ACE-World database, extract and import it
 REM appveyor DownloadFile https://github.com/ACEmulator/ACE-World-16PY/releases/download/v0.0.3/ACE-World-16PY-db-v0.0.3.sql.zip
 REM 7z x ACE-World-16PY-db-v0.0.3.sql.zip
 REM "C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_world < ACE-World-16PY-db-v0.0.3.sql
-DownloadACEWorld.bat
+Source\AppVeyor\DownloadACEWorld.bat
 
 REM execute Update Scripts for World Database
 REM "C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_world < database\updates\world\changeme.sql
