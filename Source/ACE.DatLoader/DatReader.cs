f5c7b44d81a23c38fa396feb146f4c418fe83dc3
Mag-nus <Mag-nus@users.noreply.github.com>
2018-02-06 10:18:38 -0600

    ACE.DatLoader DatDirectory reading cleaned up (#624)

--- Source/ACE.DatLoader/DatReader.cs
+++ Source/ACE.DatLoader/DatReader.cs
@@ -7,44 +7,53 @@ namespace ACE.DatLoader
     {
         public byte[] Buffer { get; }
 
-        public DatReader(string datFilePath, uint offset, uint size, uint sectorSize)
+        public DatReader(string datFilePath, uint offset, uint size, uint blockSize)
         {
-            uint nextAddress = 0;
-
             using (var stream = new FileStream(datFilePath, FileMode.Open, FileAccess.Read))
             {
-                Buffer = new byte[size];
-
-                stream.Seek(offset, SeekOrigin.Begin);
-
-                // Dat "file" is broken up into sectors that are not neccessarily congruous. Next address is stored in first four bytes of each sector.
-                if (size > sectorSize)
-                    nextAddress = GetNextAddress(stream, -4);
-
-                int bufferOffset = 0;
-
-                while (size > 0)
-                {
-                    if (size < sectorSize)
-                    {
-                        stream.Read(Buffer, bufferOffset, Convert.ToInt32(size));
-                        size = 0; // We know we've read the only/last sector, so just set this to zero to proceed.
-                    }
-                    else
-                    {
-                        stream.Read(Buffer, bufferOffset, Convert.ToInt32(sectorSize) - 4); // Read in our sector into the buffer[]
-                        bufferOffset += Convert.ToInt32(sectorSize) - 4; // Adjust this so we know where in our buffer[] the next sector gets appended to
-                        stream.Seek(nextAddress, SeekOrigin.Begin); // Move the file pointer to the start of the next sector we read above.
-                        nextAddress = GetNextAddress(stream, 0); // Get the start location of the next sector.
-                        size -= (sectorSize - 4); // Decrease this by the amount of data we just read into buffer[] so we know how much more to go
-                    }
-                }
+                Buffer = ReadDat(stream, offset, size, blockSize);
 
                 stream.Close();
             }
         }
 
-        private uint GetNextAddress(FileStream stream, int relOffset)
+        public DatReader(FileStream stream, uint offset, uint size, uint blockSize)
+        {
+            Buffer = ReadDat(stream, offset, size, blockSize);
+        }
+
+        private static byte[] ReadDat(FileStream stream, uint offset, uint size, uint blockSize)
+        {
+            var buffer = new byte[size];
+
+            stream.Seek(offset, SeekOrigin.Begin);
+
+            // Dat "file" is broken up into sectors that are not neccessarily congruous. Next address is stored in first four bytes of each sector.
+            uint nextAddress = GetNextAddress(stream, 0);
+
+            int bufferOffset = 0;
+
+            while (size > 0)
+            {
+                if (size < blockSize)
+                {
+                    stream.Read(buffer, bufferOffset, Convert.ToInt32(size));
+                    size = 0; // We know we've read the only/last sector, so just set this to zero to proceed.
+                }
+                else
+                {
+                    stream.Read(buffer, bufferOffset, Convert.ToInt32(blockSize) - 4); // Read in our sector into the buffer[]
+                    bufferOffset += Convert.ToInt32(blockSize) - 4; // Adjust this so we know where in our buffer[] the next sector gets appended to
+                    stream.Seek(nextAddress, SeekOrigin.Begin); // Move the file pointer to the start of the next sector we read above.
+                    nextAddress = GetNextAddress(stream, 0); // Get the start location of the next sector.
+                    size -= (blockSize - 4); // Decrease this by the amount of data we just read into buffer[] so we know how much more to go
+                }
+            }
+
+            return buffer;
+        }
+
+        private static uint GetNextAddress(FileStream stream, int relOffset)
         {
             // The location of the start of the next sector is the first four bytes of the current sector. This should be 0x00000000 if no next sector.
             byte[] nextAddressBytes = new byte[4];
