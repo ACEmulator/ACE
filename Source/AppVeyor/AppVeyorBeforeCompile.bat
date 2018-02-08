6ad62279d25285fb8b66fac3c535db90df3e476c
Mag-nus <Mag-nus@users.noreply.github.com>
2018-02-08 08:31:29 -0600

    Appveyor fix I think

--- Source/AppVeyor/AppVeyorBeforeCompile.bat
+++ Source/AppVeyor/AppVeyorBeforeCompile.bat
@@ -1,4 +1,4 @@
 @echo on
-copy Source\AppVeyor\Config.json Source\ACE\Config.json
+copy Source\AppVeyor\Config.json Source\ACE.Server\Config.json
 nuget restore source\ACE.sln
 source\AppVeyor\MySqlInstall.bat
