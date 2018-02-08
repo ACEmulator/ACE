66b870912867a1ed4044750b91253422e8acfeab
Mag-nus <Mag-nus@users.noreply.github.com>
2018-02-08 08:08:37 -0600

    Unused references removed

--- Source/ACE.Database.Tests/WeenieSearchTests.cs
+++ Source/ACE.Database.Tests/WeenieSearchTests.cs
@@ -6,7 +6,6 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
