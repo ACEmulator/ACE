b35a6b382926f8d169654e24fa19ffc3cd8a7e37
Mag-nus <Mag-nus@users.noreply.github.com>
2018-02-07 22:08:31 -0600

    conflicts

--- Source/ACE.DatLoader/FileTypes/Wave.cs
+++ Source/ACE.DatLoader/FileTypes/Wave.cs
@@ -7,16 +7,13 @@ namespace ACE.DatLoader.FileTypes
     /// I'm not sure of an instance where the server would ever need this data, but it's fun nonetheless and included for completion sake.
     /// </summary>
     [DatFileType(DatFileType.Wave)]
-    public class Wave : IUnpackable
+    public class Wave : FileType
     {
-        public uint Id { get; private set; }
         public byte[] Header { get; private set; }
         public byte[] Data { get; private set; }
 
-        public void Unpack(BinaryReader reader)
+        public override void Unpack(BinaryReader reader)
         {
-            Id = reader.ReadUInt32();
-
             int headerSize  = reader.ReadInt32() - 2; // not sure why this is required, it just is.
             int dataSize    = reader.ReadInt32();
 
@@ -27,33 +24,11 @@ namespace ACE.DatLoader.FileTypes
             reader.BaseStream.Position = reader.BaseStream.Length;
         }
 
-        public static Wave ReadFromDat(uint fileId)
-        {
-            // Check the FileCache so we don't need to hit the FileSystem repeatedly
-            if (DatManager.PortalDat.FileCache.TryGetValue(fileId, out var result))
-                return (Wave)result;
-
-            DatReader datReader = DatManager.PortalDat.GetReaderForFile(fileId);
-
-            var obj = new Wave();
-
-            using (var memoryStream = new MemoryStream(datReader.Buffer))
-            using (var reader = new BinaryReader(memoryStream))
-                obj.Unpack(reader);
-
-            // Store this object in the FileCache
-            DatManager.PortalDat.FileCache[fileId] = obj;
-
-            return obj;
-        }
-
         /// <summary>
         /// Exports Wave to a playable .wav file
         /// </summary>
-        public static void ExportWave(uint fileId, string directory)
+        public static void ExportWave(Wave wav, uint fileId, string directory)
         {
-            Wave wav = ReadFromDat(fileId);
-
             string filename = Path.Combine(directory, fileId.ToString("X8") + ".wav");
 
             // Good summary of the header for a WAV file and what all this means
