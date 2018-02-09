e167108a7f13e25a95278069750fa4a2d877bb0e
Mag-nus <Mag-nus@users.noreply.github.com>
2018-02-09 10:28:06 -0600

    ACE.Server: Arrange initialization of manager in a more appropriate order

--- Source/ACE.Server/ACE.cs
+++ Source/ACE.Server/ACE.cs
@@ -1,13 +1,15 @@
 using System;
 using System.IO;
+
+using log4net;
+using log4net.Config;
+
 using ACE.Common;
 using ACE.Database;
 using ACE.DatLoader;
 using ACE.Server.Command;
 using ACE.Server.Managers;
 using ACE.Server.Network.Managers;
-using log4net;
-using log4net.Config;
 
 namespace ACE.Server
 {
@@ -22,23 +24,51 @@ namespace ACE.Server
             var logRepository = LogManager.GetRepository(System.Reflection.Assembly.GetEntryAssembly());
             XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
 
+
             log.Info("Starting ACEmulator...");
             Console.Title = @"ACEmulator";
 
+            log.Info("Initializing ConfigManager...");
             ConfigManager.Initialize();
+
+            log.Info("Initializing ServerManager...");
             ServerManager.Initialize();
-            DatabaseManager.Initialize();
-            AssetManager.Initialize();
-            InboundMessageManager.Initialize();
-            DatabaseManager.Start();
+
+
+            log.Info("Initializing DatManager...");
             DatManager.Initialize(ConfigManager.Config.Server.DatFilesDirectory);
+
+
+            log.Info("Initializing DatabaseManager...");
+            DatabaseManager.Initialize();
+
+            log.Info("Starting DatabaseManager...");
+            DatabaseManager.Start();
+
+            log.Info("Initializing GuidManager...");
             GuidManager.Initialize();
-            SocketManager.Initialize();
-            WorldManager.Initialize();
-            CommandManager.Initialize();
+
+            log.Info("Initializing AssetManager...");
+            AssetManager.Initialize();
+
+
+            log.Info("Initializing RecipeManager...");
             RecipeManager.Initialize();
 
-            DatabaseManager.Start();
+
+            log.Info("Initializing InboundMessageManager...");
+            InboundMessageManager.Initialize();
+
+            log.Info("Initializing SocketManager...");
+            SocketManager.Initialize();
+
+            log.Info("Initializing WorldManager...");
+            WorldManager.Initialize();
+
+
+            // This should be last
+            log.Info("Initializing CommandManager...");
+            CommandManager.Initialize();
         }
 
         private static void OnProcessExit(object sender, EventArgs e)
