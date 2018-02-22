deb9672c1dbebf438e6d35b39bdd0c3d1dbd11ed
Ty Conner <tyconner@itcproductions.com>
2018-02-22 14:24:58 -0500

    IdOnly to AssessmentProperty

--- Source/ACE.Entity/Enum/Properties/PropertyDataId.cs
+++ Source/ACE.Entity/Enum/Properties/PropertyDataId.cs
@@ -27,11 +27,11 @@ namespace ACE.Entity.Enum.Properties
         ClothingBase               = 7,
         [ServerOnly]
         Icon                       = 8,
-        [IdOnly]
+        [AssessmentPropertyAttribute]
         EyesTexture                = 9,
-        [IdOnly]
+        [AssessmentPropertyAttribute]
         NoseTexture                = 10,
-        [IdOnly]
+        [AssessmentPropertyAttribute]
         MouthTexture               = 11,
         [ServerOnly]
         DefaultEyesTexture         = 12,
@@ -39,11 +39,11 @@ namespace ACE.Entity.Enum.Properties
         DefaultNoseTexture         = 13,
         [ServerOnly]
         DefaultMouthTexture        = 14,
-        [IdOnly]
+        [AssessmentPropertyAttribute]
         HairPalette                = 15,
-        [IdOnly]
+        [AssessmentPropertyAttribute]
         EyesPalette                = 16,
-        [IdOnly]
+        [AssessmentPropertyAttribute]
         SkinPalette                = 17,
         [ServerOnly]
         HeadObject                 = 18,
