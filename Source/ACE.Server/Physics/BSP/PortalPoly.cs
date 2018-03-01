d5426062d4c97c2e54fc270583563acc831618ed
gmriggs <gmriggs@gmail.com>
2018-03-01 18:30:42 -0500

    Adding BSP tree stubs

---
+++ Source/ACE.Server/Physics/BSP/PortalPoly.cs
@@ -0,0 +1,13 @@
+namespace ACE.Server.Physics.BSP
+{
+    public class PortalPoly
+    {
+        public int PortalIdx;
+        public Polygon Portal;
+
+        public PortalPoly()
+        {
+            PortalIdx = -1;
+        }
+    }
+}
