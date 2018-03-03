a46ce9254cc59f1aa5ae32335256262b88a7549c
Ty Conner <tyconner@itcproductions.com>
2018-03-03 18:55:29 -0500

    Add Missed Classes

---
+++ Source/ACE.Entity/SubPalette.cs
@@ -0,0 +1,9 @@
+namespace ACE.Entity
+{
+    public class SubPalette
+    {
+        public uint SubID { get; set; }
+        public uint Offset { get; set; }
+        public uint NumColors { get; set; }
+    }
+}
