c0386524171aed1a8e0ee1cc97c9c79537ab9b38
Ty Conner <tyconner@itcproductions.com>
2018-03-06 16:17:15 -0500

    Update DownloadACEWorld.bat

--- Source/AppVeyor/DownloadACEWorld.bat
+++ Source/AppVeyor/DownloadACEWorld.bat
@@ -1,4 +1,4 @@
 REM Download latest ACE-World database, extract and import it 
-appveyor DownloadFile https://ci.appveyor.com/api/buildjobs/tcv394f0q590n1ik/artifacts/ACE-World-16PY-db-v0.0.5-ixhngyfm.sql.zip
-7z x ACE-World-16PY-db-v0.0.5-ixhngyfm.sql.zip
-"C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_world < ACE-World-16PY-db-v0.0.5-ixhngyfm.sql
+appveyor DownloadFile https://github.com/ACEmulator/ACE-World-16PY/releases/download/v0.0.5/ACE-World-16PY-db-v0.0.5.sql.zip
+7z x ACE-World-16PY-db-v0.0.5.sql.zip
+"C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_world < ACE-World-16PY-db-v0.0.5.sql
