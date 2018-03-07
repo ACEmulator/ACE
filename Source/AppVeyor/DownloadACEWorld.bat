57d0db1da7023947cb438c5cdcf8d0f1956fd521
Ty Conner <tyconner@itcproductions.com>
2018-03-07 01:07:22 -0500

    Change to AppVeyor script
    This also fixes I think an issue with our gitignore

--- Source/AppVeyor/DownloadACEWorld.bat
+++ Source/AppVeyor/DownloadACEWorld.bat
@@ -1,4 +1,5 @@
+set /p dbversion=<dbversion.txt
 REM Download latest ACE-World database, extract and import it 
-appveyor DownloadFile https://github.com/ACEmulator/ACE-World-16PY/releases/download/v0.0.5/ACE-World-16PY-db-v0.0.5.sql.zip
-7z x ACE-World-16PY-db-v0.0.5.sql.zip
-"C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_world < ACE-World-16PY-db-v0.0.5.sql
+appveyor DownloadFile https://github.com/ACEmulator/ACE-World-16PY/releases/download/v%dbversion%/ACE-World-16PY-db-v%dbversion%.sql.zip
+7z x ACE-World-16PY-db-v%dbversion%.sql.zip
+"C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_world < ACE-World-16PY-db-v%dbversion%.sql
