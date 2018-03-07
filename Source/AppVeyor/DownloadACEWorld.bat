75a4c04cc7171144ffb7dcce1bae4a93e366b9bb
Ty Conner <tyconner@itcproductions.com>
2018-03-07 03:23:20 -0500

    Fix for AppVeyor

--- Source/AppVeyor/DownloadACEWorld.bat
+++ Source/AppVeyor/DownloadACEWorld.bat
@@ -1,4 +1,4 @@
-set /p dbversion=<dbversion.txt
+set /p dbversion=<Source\AppVeyor\dbversion.txt
 REM Download latest ACE-World database, extract and import it 
 appveyor DownloadFile https://github.com/ACEmulator/ACE-World-16PY/releases/download/v%dbversion%/ACE-World-16PY-db-v%dbversion%.sql.zip
 7z x ACE-World-16PY-db-v%dbversion%.sql.zip
