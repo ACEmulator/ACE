027d65a021177401779910cc3d9afe46454e8daa
Ty Conner <tyconner@itcproductions.com>
2018-03-11 18:01:46 -0400

    setlocal

--- Source/AppVeyor/DownloadACEWorld.bat
+++ Source/AppVeyor/DownloadACEWorld.bat
@@ -1,3 +1,5 @@
+@echo off
+setlocal enabledelayedexpansion
 IF EXIST Source\AppVeyor\db-pr-override.txt (
 set /p dbversion=<Source\AppVeyor\db-pr-override.txt
 FOR /f "delims=" %%i in ("%dbversion%") do ( 
@@ -14,3 +16,4 @@ appveyor DownloadFile https://github.com/ACEmulator/ACE-World-16PY/releases/down
 7z x ACE-World-16PY-db-v%dbversion%.sql.zip
 "C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_world < ACE-World-16PY-db-v%dbversion%.sql
 )
+@echo on
