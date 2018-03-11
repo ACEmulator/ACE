5dc9ea18fa7a08bf24e0f0a6cc949f0ae98775fc
Ty Conner <tyconner@itcproductions.com>
2018-03-11 18:29:15 -0400

    yay, script clean up

--- Source/AppVeyor/DownloadACEWorld.bat
+++ Source/AppVeyor/DownloadACEWorld.bat
@@ -1,34 +1,9 @@
-REM @echo off
-rem IF EXIST Source\AppVeyor\db-pr-override.txt (
-rem set /p dbversion=<Source\AppVeyor\db-pr-override.txt
-rem FOR /f "delims=" %%i in ("%dbversion%") do ( 
-rem set dbfullfilename=%%~nxi
-rem )
+@echo off
 REM Download latest ACE-World database, extract and import it 
-rem appveyor DownloadFile %dbversion%
-rem 7z x %dbfullfilename%
-rem "C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_world < %dbfullfilename:~0,-4%
-rem ) ELSE (
-rem set /p dbversion=<Source\AppVeyor\dbversion.txt
-REM Download latest ACE-World database, extract and import it 
-rem appveyor DownloadFile https://github.com/ACEmulator/ACE-World-16PY/releases/download/v%dbversion%/ACE-World-16PY-db-v%dbversion%.sql.zip
-rem 7z x ACE-World-16PY-db-v%dbversion%.sql.zip
-rem "C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_world < ACE-World-16PY-db-v%dbversion%.sql
-rem )
+
 REM echo %downloadfile%
 REM echo %zipfile%
 REM echo %sqlfile%
-rem FOR /f "delims=" %%i in ("%dbversion%") do ( 
-rem set dbfullfilename=%%~nxi
-rem )
-rem echo %dbfullfilename%
-REM @echo on
-
-rem @echo off
-
-echo %downloadfile%
-echo %zipfile%
-echo %sqlfile%
 
 set /p dbversion=<Source\AppVeyor\dbversion.txt
 
@@ -44,12 +19,12 @@ FOR /f "delims=" %%i in ("%downloadfile%") DO (
 
 set sqlfile=%zipfile:~0,-4%
 
-echo %downloadfile%
-echo %zipfile%
-echo %sqlfile%
+REM echo %downloadfile%
+REM echo %zipfile%
+REM echo %sqlfile%
 
 appveyor DownloadFile %downloadfile%
 7z x %zipfile%
 "C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_world < %sqlfile%
 
-rem @echo on
+@echo on
