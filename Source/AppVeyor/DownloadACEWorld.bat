c07e8dec18ae2384e122d6f3bdb3e387baed8d57
Ty Conner <tyconner@itcproductions.com>
2018-03-11 16:52:42 -0400

    AppVeyor fix for PRs

--- Source/AppVeyor/DownloadACEWorld.bat
+++ Source/AppVeyor/DownloadACEWorld.bat
@@ -1,5 +1,14 @@
+IF EXIST Source\AppVeyor\db-pr-override.txt (
+set /p dbversion=<Source\AppVeyor\db-pr-override.txt
+FOR /f "delims=/" %%G in (%dbversion%) do set /p dbfullfilename=%%G
+REM Download latest ACE-World database, extract and import it 
+appveyor DownloadFile %dbversion%
+7z x %dbfullfilename%
+"C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_world < %dbfullfilename:~0,-4%
+) ELSE (
 set /p dbversion=<Source\AppVeyor\dbversion.txt
 REM Download latest ACE-World database, extract and import it 
 appveyor DownloadFile https://github.com/ACEmulator/ACE-World-16PY/releases/download/v%dbversion%/ACE-World-16PY-db-v%dbversion%.sql.zip
 7z x ACE-World-16PY-db-v%dbversion%.sql.zip
 "C:\Program Files\MySql\MySQL Server 5.7\bin\mysql.exe" -h localhost -u root -pPassword12! ace_world < ACE-World-16PY-db-v%dbversion%.sql
+)
