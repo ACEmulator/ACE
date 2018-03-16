cd95777829f6024c1711b69ba34bd0f3bd784b4e
Slushnas <Slushnas@users.noreply.github.com>
2018-03-16 09:03:54 -0700

    Fix for allegiance update done message. (#689)

--- Source/ACE.Server/Network/GameEvent/Events/GameEventAllegianceAllegianceUpdateDone.cs
+++ Source/ACE.Server/Network/GameEvent/Events/GameEventAllegianceAllegianceUpdateDone.cs
@@ -2,14 +2,11 @@ using ACE.Server.Network.GameMessages;
 
 namespace ACE.Server.Network.GameEvent.Events
 {
-    public class GameEventAllegianceAllegianceUpdateDone : GameMessage
+    public class GameEventAllegianceAllegianceUpdateDone : GameEventMessage
     {
         public GameEventAllegianceAllegianceUpdateDone(Session session)
-            : base(GameMessageOpcode.GameEvent, GameMessageGroup.UIQueue)
+            : base(GameEventType.AllegianceAllegianceUpdateDone, GameMessageGroup.UIQueue, session)
         {
-            //Writer.Write(session.Player.Guid.Full);
-            //Writer.Write(session.GameEventSequence++);
-            //Writer.Write((uint)GameEvent.GameEventType.AllegianceAllegianceUpdateDone);
             Writer.Write(0x00);
         }
     }
