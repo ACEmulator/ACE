9aa7eb6b78edd75eaa72eb609aae6e4dd1a15fb2
Slushnas <Slushnas@users.noreply.github.com>
2018-03-23 21:25:01 -0700

    Adding Weenie Error Messages (#695)
    * Adding weenie errors enum.
    
    * Change messages to allow the use of weenie errors.

--- Source/ACE.Server/Network/GameEvent/Events/GameEventFellowshipFellowUpdateDone.cs
+++ Source/ACE.Server/Network/GameEvent/Events/GameEventFellowshipFellowUpdateDone.cs
@@ -1,16 +1,17 @@
+using ACE.Entity.Enum;
 using ACE.Server.Network.GameMessages;
 
 namespace ACE.Server.Network.GameEvent.Events
 {
     public class GameEventFellowshipFellowUpdateDone : GameMessage
     {
-        public GameEventFellowshipFellowUpdateDone(Session session)
+        public GameEventFellowshipFellowUpdateDone(Session session, WERROR errorType = WERROR.WERROR_NONE)
             : base(GameMessageOpcode.GameEvent, GameMessageGroup.UIQueue)
         {
             //Writer.Write(session.Player.Guid.Full);
             //Writer.Write(session.GameEventSequence++);
             //Writer.Write((uint)GameEventType.FellowshipFellowUpdateDone);
-            Writer.Write(0x00);
+            Writer.Write((uint)errorType);
         }
     }
 }
