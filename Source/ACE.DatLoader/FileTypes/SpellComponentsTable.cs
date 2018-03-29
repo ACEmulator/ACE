fc3e0ca0f313c44cb474a62d39a313957b4de037
Keith McCormick <kdmxp512@gmail.com>
2018-03-29 07:27:04 -0700

    Add notes for GetSpellWords method

--- Source/ACE.DatLoader/FileTypes/SpellComponentsTable.cs
+++ Source/ACE.DatLoader/FileTypes/SpellComponentsTable.cs
@@ -8,6 +8,19 @@ namespace ACE.DatLoader.FileTypes
     [DatFileType(DatFileType.SpellComponentTable)]
     public class SpellComponentsTable : FileType
     {
+        public enum Type
+        {
+            Scarab      = 1,
+            Herb        = 2,
+            Powder      = 3,
+            Potion      = 4,
+            Talisman    = 5,
+            Taper       = 6,
+            PotionPea   = 7,
+            TalismanPea = 5,
+            TaperPea    = 7
+        }
+
         internal const uint FILE_ID = 0x0E00000F;
 
         public Dictionary<uint, SpellComponentBase> SpellComponents { get; } = new Dictionary<uint, SpellComponentBase>();
@@ -28,21 +41,24 @@ namespace ACE.DatLoader.FileTypes
             string secondSpellWord = null;
             string thirdSpellWord = null;
 
+            // Locate the herb component in the Spell formula
             for (int i = 0; i < formula.Count; i++)
             {
-                if (comps.SpellComponents[formula[i]].Type == 2)
+                if (comps.SpellComponents[formula[i]].Type == (uint)Type.Herb)
                     firstSpellWord = comps.SpellComponents[formula[i]].Text;
             }
 
+            // Locate the powder component in the Spell formula
             for (int i = 0; i < formula.Count; i++)
             {
-                if (comps.SpellComponents[formula[i]].Type == 3)
+                if (comps.SpellComponents[formula[i]].Type == (uint)Type.Powder)
                     secondSpellWord = comps.SpellComponents[formula[i]].Text;
             }
 
+            // Locate the potion component in the Spell formula
             for (int i = 0; i < formula.Count; i++)
             {
-                if (comps.SpellComponents[formula[i]].Type == 4)
+                if (comps.SpellComponents[formula[i]].Type == (uint)Type.Potion)
                     thirdSpellWord = comps.SpellComponents[formula[i]].Text;
             }
 
