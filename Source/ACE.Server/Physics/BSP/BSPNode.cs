7c863fd38e0749708ca0239e85f6aa51de48afce
gmriggs <gmriggs@gmail.com>
2018-03-30 19:39:14 -0400

    Fixing objects loading into dungeon

--- Source/ACE.Server/Physics/BSP/BSPNode.cs
+++ Source/ACE.Server/Physics/BSP/BSPNode.cs
@@ -63,14 +63,14 @@ namespace ACE.Server.Physics.BSP
             {
                 if (!(node.PosNode is DatLoader.Entity.BSPLeaf))
                     PosNode = new BSPNode(node.PosNode, polys, vertexArray);
-                else if (!(node.PosNode is DatLoader.Entity.BSPPortal))
+                else // portal?
                     PosNode = new BSPLeaf((DatLoader.Entity.BSPLeaf)node.PosNode, polys, vertexArray);
             }
             if (node.NegNode != null)
             {
                 if (!(node.NegNode is DatLoader.Entity.BSPLeaf))
                     NegNode = new BSPNode(node.NegNode, polys, vertexArray);
-                else if (!(node.NegNode is DatLoader.Entity.BSPPortal))
+                else // portal?
                     NegNode = new BSPLeaf((DatLoader.Entity.BSPLeaf)node.NegNode, polys, vertexArray);
             }
         }
