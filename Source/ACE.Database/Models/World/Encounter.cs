f73cee55b54eecfe5f4bed3a9031e2c177b2acfa
Ty Conner <tyconner@itcproductions.com>
2018-04-14 23:59:48 -0400

    Update WorldDB scaffolding

--- Source/ACE.Database/Models/World/Encounter.cs
+++ Source/ACE.Database/Models/World/Encounter.cs
@@ -6,8 +6,9 @@ namespace ACE.Database.Models.World
     public partial class Encounter
     {
         public uint Id { get; set; }
-        public int Index { get; set; }
+        public int Landblock { get; set; }
         public uint WeenieClassId { get; set; }
-        public string EncounterMap { get; set; }
+        public int CellX { get; set; }
+        public int CellY { get; set; }
     }
 }
