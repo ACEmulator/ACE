2d46f1178a8cbd0cef322871b3ce8625f77ce395
dgatewood <ragnarok1003@yahoo.com>
2018-04-28 23:03:49 -0500

    got rid of console.writelines

--- Source/ACE.Server/WorldObjects/Player_Inventory.cs
+++ Source/ACE.Server/WorldObjects/Player_Inventory.cs
@@ -488,8 +488,6 @@ namespace ACE.Server.WorldObjects
                 isAttuned = false;
                 return false;
             }
-            Console.WriteLine("Item is not null");
-            Console.WriteLine("Item name: " + item.Name);
             int attunedProperty = (int)item.GetProperty(PropertyInt.Attuned);
             if (attunedProperty == 1)
             {
