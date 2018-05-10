df77851041a42ddf618682b2305fe5a1e3928ae6
dgatewood <ragnarok1003@yahoo.com>
2018-05-09 23:54:47 -0500

    Handing objects to other WO's
    GameEvent and Action changes, and reimplementation of the old code for HandleGiveObjectRequest.

--- Source/ACE.Server/Network/GameAction/Actions/GameActionGiveObjectRequest.cs
+++ Source/ACE.Server/Network/GameAction/Actions/GameActionGiveObjectRequest.cs
@@ -1,3 +1,4 @@
+using ACE.Entity;
 namespace ACE.Server.Network.GameAction.Actions
 {
     public static class GameActionGiveObjectRequest
@@ -8,8 +9,10 @@ namespace ACE.Server.Network.GameAction.Actions
             uint targetID = message.Payload.ReadUInt32();
             uint objectID = message.Payload.ReadUInt32();
             uint amount = message.Payload.ReadUInt32();
+            ObjectGuid targetGuid = new ObjectGuid(targetID);
+            ObjectGuid objectGuid = new ObjectGuid(objectID);
 
-            session.Player.HandleActionGiveObjectRequest(targetID, objectID, amount);
+            session.Player.HandleActionGiveObjectRequest(targetGuid, objectGuid, amount);
         }
     }
 }
