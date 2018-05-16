49a283e4e34543c23794d13714de5799fa8e329d
Derrick Gatewood <dgatewood@ems-isd.net>
2018-05-16 13:30:04 -0500

    Thoroughly tested. Got all the lead out.

--- Source/ACE.Server/WorldObjects/Player_Inventory.cs
+++ Source/ACE.Server/WorldObjects/Player_Inventory.cs
@@ -701,8 +701,22 @@ namespace ACE.Server.WorldObjects
             WorldObject target = CurrentLandblock.GetObject(targetID) as WorldObject;
             WorldObject item = GetInventoryItem(itemGuid) as WorldObject;
             var actionChain = new ActionChain();
-            if (!target.GetProperty(PropertyBool.AllowGive)??false)
+            if (target.GetProperty(PropertyBool.AiAcceptEverything) ?? false)
             {
+                ///Item accepted by NPC that accepts anything
+                actionChain.AddDelaySeconds(Rotate(target));
+                actionChain.AddAction(this, () =>
+                {
+                    Session.Network.EnqueueSend(new GameEventItemServerSaysContainId(Session, item, target));
+                    Session.Network.EnqueueSend(new GameMessageSystemChat("You give " + target.Name + " " + item.Name + ".", ChatMessageType.System));
+                    Session.Network.EnqueueSend(new GameMessageSound(this.Guid, Sound.ReceiveItem, 1));
+                    TryRemoveItemFromInventoryWithNetworking(item, (ushort)amount);
+                    Session.Network.EnqueueSend(new GameEventInventoryRemoveObject(Session, item));
+                });
+            }
+            else if (!target.GetProperty(PropertyBool.AllowGive)??false)
+            {
+                actionChain.AddDelaySeconds(Rotate(target));
                 actionChain.AddAction(this, () =>
                 {
                     Session.Network.EnqueueSend(new GameMessageSystemChat(target.Name + WeenieErrorWithString._IsNotAcceptingGiftsRightNow , ChatMessageType.System));
@@ -712,6 +726,7 @@ namespace ACE.Server.WorldObjects
             }
             else if(item.GetProperty(PropertyBool.Retained)??false)
             {
+                actionChain.AddDelaySeconds(Rotate(target));
                 actionChain.AddAction(this, () =>
                 {
                     Session.Network.EnqueueSend(new GameMessageSystemChat("You can't give this item away.(Retained)", ChatMessageType.System));
@@ -761,20 +776,8 @@ namespace ACE.Server.WorldObjects
                     }
                     }
             }
-            if (target.GetProperty(PropertyBool.AiAcceptEverything) ?? false)
-            {
-                ///Item accepted by NPC that accepts anything
-                actionChain.AddAction(this, () =>
-                {
-                    Session.Network.EnqueueSend(new GameEventItemServerSaysContainId(Session, item, target));
-                    Session.Network.EnqueueSend(new GameMessageSystemChat("You give " + target.Name + " " + item.Name + ".", ChatMessageType.System));
-                    Session.Network.EnqueueSend(new GameMessageSound(this.Guid, Sound.ReceiveItem, 1));
-                    TryRemoveItemFromInventoryWithNetworking(item, (ushort)amount);
-                    Session.Network.EnqueueSend(new GameEventInventoryRemoveObject(Session, item));
-                });
-            }
+            
             actionChain.EnqueueChain();
-            //SendUseDoneEvent();
         }
 
 
