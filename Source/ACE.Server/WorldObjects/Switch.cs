fbc0571bb7613dd6e20cbcb6a5757076bd1be75b
fartwhif <fartwhif@tuta.io>
2018-06-01 02:07:39 -0400

    spell traps (#812)
    * spell traps
    
    * Update Switch.cs
    
    * Update WorldObject_Magic.cs

--- Source/ACE.Server/WorldObjects/Switch.cs
+++ Source/ACE.Server/WorldObjects/Switch.cs
@@ -1,6 +1,13 @@
+using ACE.Database;
 using ACE.Database.Models.Shard;
 using ACE.Database.Models.World;
+using ACE.DatLoader;
 using ACE.Entity;
+using ACE.Entity.Enum;
+using ACE.Entity.Enum.Properties;
+using ACE.Server.Entity;
+using ACE.Server.Network.GameMessages.Messages;
+using System.Collections.Generic;
 
 namespace ACE.Server.WorldObjects
 {
@@ -24,6 +31,58 @@ namespace ACE.Server.WorldObjects
 
         private void SetEphemeralValues()
         {
+        }
+        public double? ResetTime
+        {
+            get => GetProperty(PropertyFloat.ResetInterval);
+            set { if (value == null) RemoveProperty(PropertyFloat.ResetInterval); else SetProperty(PropertyFloat.ResetInterval, (double)value); }
+        }
+        public string ActivationFailiure
+        {
+            get => GetProperty(PropertyString.ActivationFailure);
+            set { if (value == null) RemoveProperty(PropertyString.ActivationFailure); else SetProperty(PropertyString.ActivationFailure, value); }
+        }
+
+        Dictionary<ObjectGuid, double> HitTimes = new Dictionary<ObjectGuid, double>();
+
+        private void pruneHits()
+        {
+            var currentTime = Timer.CurrentTime;
+            List<ObjectGuid> resets = new List<ObjectGuid>();
+            foreach (var hit in HitTimes.Keys)
+                if (currentTime - HitTimes[hit] > ResetTime)
+                    resets.Add(hit);
+            resets.ForEach(k => HitTimes.Remove(k));
+        }
+
+        public override void HandleActionOnCollide(ObjectGuid playerId)
+        {
+            if (!playerId.IsPlayer()) return;
+            var player = CurrentLandblock.GetObject(playerId) as Player;
+            if (player == null) return;
+
+            if (ResetTime.HasValue && ResetTime > 0)
+            {
+                pruneHits();
+                if (HitTimes.ContainsKey(playerId))
+                    return;
+                HitTimes.Add(playerId, Timer.CurrentTime);
+            }
+
+            //spell traps
+            if (Spell.HasValue && SpellDID.HasValue)
+            {
+                CurrentLandblock.EnqueueBroadcastSound(player, Sound.TriggerActivated);
+                var spellTable = DatManager.PortalDat.SpellTable;
+                if (!spellTable.Spells.ContainsKey((uint)SpellDID)) return;
+                var spellBase = DatManager.PortalDat.SpellTable.Spells[(uint)SpellDID];
+                var spell = DatabaseManager.World.GetCachedSpell((uint)SpellDID);
+                var msg = string.Empty;
+                player.Session.Network.EnqueueSend(new GameMessagePrivateUpdateInstanceID(player, PropertyInstanceId.CurrentAttacker, this.Guid.Full));
+                // TODO: Perform a spell resist check. Cast spell on resist failure or output ActivationFailiure text upon resisting the spell
+                LifeMagic(player, spellBase, spell, out msg);
+                player.PlayParticleEffect((PlayScript)spellBase.TargetEffect, player.Guid);
+            }
         }
     }
 }
