5217574c0335efb30650d09bc01da711d55314a2
Evan Merlock <vnmrlck@gmail.com>
2018-06-06 15:13:45 -0500

    Added in the initial caching layer for configuration

---
+++ Source/ACE.Server/Managers/ConfigurationManager.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using ACE.Database;
+
+namespace ACE.Server.Managers
+{
+    public static class ConfigurationManager
+    {
+        // caching internally to the server
+        private static Dictionary<string, ConfigurationEntry<bool>> CachedBooleanSettings = new Dictionary<string, ConfigurationEntry<bool>>();
+        private static Dictionary<string, ConfigurationEntry<int>> CachedIntegerSettings = new Dictionary<string, ConfigurationEntry<int>>();
+        private static Dictionary<string, ConfigurationEntry<float>> CachedFloatSettings = new Dictionary<string, ConfigurationEntry<float>>();
+        private static Dictionary<string, ConfigurationEntry<string>> CachedStringSettings = new Dictionary<string, ConfigurationEntry<string>>();
+
+        public static bool GetBool(string key)
+        {
+            // first, check the cache. If the key exists in the cache, grab it regardless of it's modified value
+            // then, check the database. if the key exists in the database, grab it and cache it
+            // finally, set it to a default of false.
+            if (CachedBooleanSettings.ContainsKey(key))
+            {
+                return CachedBooleanSettings[key].Item;
+            }
+
+            var dbValue = DatabaseManager.ServerConfig.GetBool(key);
+
+            var boolVal = dbValue?.Value ?? false;
+            CachedBooleanSettings.Add(key, new ConfigurationEntry<bool>(false, boolVal));
+            return boolVal;
+        }
+
+        public static int GetInt(string key)
+        {
+            if (CachedIntegerSettings.ContainsKey(key))
+            {
+                return CachedIntegerSettings[key].Item;
+            }
+
+            var dbValue = DatabaseManager.ServerConfig.GetInt(key);
+
+            var intVal = dbValue?.Value ?? 0;
+            CachedIntegerSettings.Add(key, new ConfigurationEntry<int>(false, intVal));
+            return intVal;
+        }
+
+        public static float GetFloat(string key)
+        {
+            if (CachedFloatSettings.ContainsKey(key))
+            {
+                return CachedFloatSettings[key].Item;
+            }
+
+            var dbValue = DatabaseManager.ServerConfig.GetFloat(key);
+
+            var floatVal = dbValue?.Value ?? 0.0f;
+            CachedFloatSettings.Add(key, new ConfigurationEntry<float>(false, floatVal));
+            return floatVal;
+        }
+
+        public static string GetString(string key)
+        {
+            if (CachedStringSettings.ContainsKey(key))
+            {
+                return CachedStringSettings[key].Item;
+            }
+
+            var dbValue = DatabaseManager.ServerConfig.GetString(key);
+
+            var stringVal = dbValue?.Value ?? "";
+            CachedStringSettings.Add(key, new ConfigurationEntry<string>(false, stringVal));
+            return stringVal;
+        }
+
+    }
+
+    class ConfigurationEntry<T>
+    {
+        public bool Modified;
+        public T Item;
+
+        public ConfigurationEntry(bool modified, T item)
+        {
+            this.Modified = modified;
+            this.Item = item;
+        }
+    }
+}
