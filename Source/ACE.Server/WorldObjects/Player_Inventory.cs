3aedb75e73e8dfde16c7b8a40e6c1e6879374624
Keith McCormick <kdmxp512@gmail.com>
2018-07-26 19:04:05 -0700

    Prevent players from unloading on other player's corpses in PVP

--- Source/ACE.Server/WorldObjects/Player_Inventory.cs
+++ Source/ACE.Server/WorldObjects/Player_Inventory.cs
@@ -529,6 +529,13 @@ namespace ACE.Server.WorldObjects
                     }
                 }
 
+                var containerIsDeadPlayer = container as Corpse;
+                if (containerIsDeadPlayer != null)
+                {
+                    Session.Network.EnqueueSend(new GameEventInventoryServerSaveFailed(Session, WeenieError.YouCantDoThatYoureDead));
+                    return;
+                }
+
                 // Is this something I already have? If not, it has to be a pickup - do the pickup and out.
                 if (item == null)
                 {
