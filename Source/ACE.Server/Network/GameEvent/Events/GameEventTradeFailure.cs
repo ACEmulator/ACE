13daa9512f3e7c2a7c0ab4549b243f51746fa6ce
mcreedjr <mikejr@1024k.com>
2018-09-09 03:59:51 -0400

    Revised pass at secure trade

--- Source/ACE.Server/Network/GameEvent/Events/GameEventTradeFailure.cs
+++ Source/ACE.Server/Network/GameEvent/Events/GameEventTradeFailure.cs
@@ -1,13 +1,15 @@
 using ACE.Entity;
+using ACE.Entity.Enum;
 
 namespace ACE.Server.Network.GameEvent.Events
 {
     public class GameEventTradeFailure : GameEventMessage
     {
-        public GameEventTradeFailure(Session session, ObjectGuid item)
+        public GameEventTradeFailure(Session session, ObjectGuid item, WeenieError reason)
             : base(GameEventType.TradeFailure, GameMessageGroup.UIQueue, session)
         {
             Writer.WriteGuid(item);
+            Writer.Write((uint)reason);
         }
     }
 }
