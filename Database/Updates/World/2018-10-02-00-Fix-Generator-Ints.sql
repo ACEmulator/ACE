ecf416fbfa5b5993863cd53c8810b6f825b6f8d9
Ty Conner <tyconner@itcproductions.com>
2018-10-02 13:44:02 -0400

    Add SQL update scripts

---
+++ Database/Updates/World/2018-10-02-00-Fix-Generator-Ints.sql
@@ -0,0 +1,5 @@
+USE `ace_world`;
+
+ALTER TABLE `weenie_properties_generator` 
+CHANGE COLUMN `init_Create` `init_Create` INT(10) NOT NULL DEFAULT '1' COMMENT 'Number of object to generate initially' ,
+CHANGE COLUMN `max_Create` `max_Create` INT(10) NOT NULL DEFAULT '1' COMMENT 'Maximum amount of objects to generate' ;
