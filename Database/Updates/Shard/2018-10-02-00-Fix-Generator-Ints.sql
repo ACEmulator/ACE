a56676d1b6a936988dd2be4eaef4e68c0b987079
Ty Conner <tyconner@itcproductions.com>
2018-10-02 13:45:56 -0400

    Update Shard and World base scripts

--- Database/Updates/Shard/2018-10-02-00-Fix-Generator-Ints.sql
+++
@@ -1,5 +0,0 @@
-USE `ace_shard`;
-
-ALTER TABLE `biota_properties_generator` 
-CHANGE COLUMN `init_Create` `init_Create` INT(10) NOT NULL DEFAULT '1' COMMENT 'Number of object to generate initially' ,
-CHANGE COLUMN `max_Create` `max_Create` INT(10) NOT NULL DEFAULT '1' COMMENT 'Maximum amount of objects to generate' ;
