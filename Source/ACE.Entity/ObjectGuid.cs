4a82da7e9e80b203d32a4a09b608e81caf18f94b
Mag-nus <Mag-nus@users.noreply.github.com>
2018-10-15 22:32:26 -0500

    Landblock save progress

--- Source/ACE.Entity/ObjectGuid.cs
+++ Source/ACE.Entity/ObjectGuid.cs
@@ -57,6 +57,11 @@ namespace ACE.Entity
             return Type == GuidType.Player;
         }
 
+        public bool IsDynamic()
+        {
+            return Type == GuidType.Dynamic;
+        }
+
         public static bool operator ==(ObjectGuid g1, ObjectGuid g2)
         {
             return g1.Full == g2.Full;
