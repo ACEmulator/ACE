2335a2fda61d5a827274fa88b324819e70294a92
Mag-nus <Mag-nus@users.noreply.github.com>
2018-10-28 10:07:28 -0500

    Landblock LoadMeshes summary improved

--- Source/ACE.Server/Entity/Landblock.cs
+++ Source/ACE.Server/Entity/Landblock.cs
@@ -194,7 +194,10 @@ namespace ACE.Server.Entity
         }
 
         /// <summary>
-        /// Loads the meshes for the landblock
+        /// Loads the meshes for the landblock<para />
+        /// This isn't used by ACE, but we still retain it for the following reason:<para />
+        /// its useful, concise, high level overview code for everything needed to load landblocks, all their objects, scenery, polygons
+        /// without getting into all of the low level methods that acclient uses to do it
         /// </summary>
         private void LoadMeshes(List<LandblockInstance> objects)
         {
