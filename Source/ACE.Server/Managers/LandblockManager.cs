f244e350e2bbf227e7c573db148858c57e2a5ca6
Ty Conner <tyconner@itcproductions.com>
2018-11-01 13:58:55 -0400

    Add a hardcoded fallback incase config file not updated

--- Source/ACE.Server/Managers/LandblockManager.cs
+++ Source/ACE.Server/Managers/LandblockManager.cs
@@ -49,7 +49,9 @@ namespace ACE.Server.Managers
             if (ConfigManager.Config.Server.PreloadedLandblocks == null)
             {
                 log.Info("No configuration found for PreloadedLandblocks, please refer to Config.json.example");
-                return;
+                log.Warn("Initializing PreloadedLandblocks with single default for Hebian-To (Global Events)");
+                log.Warn("Add a PreloadedLandblocks section to your Config.json file and adjust to meet your needs");
+                ConfigManager.Config.Server.PreloadedLandblocks = new List<PreloadedLandblocks> { new PreloadedLandblocks { Id = "E74EFFFF", Description = "Hebian-To (Global Events)", Permaload = true, IncludeAdjacents = true, Enabled = true } };
             }
 
             log.InfoFormat("Found {0} landblock ids to preload.", ConfigManager.Config.Server.PreloadedLandblocks.Count);
