1e7a6f4561b09f35d15d427340530a28f6a9e714
Ty Conner <tyconner@itcproductions.com>
2018-12-02 18:44:33 -0500

    Update batch files

--- Source/AppVeyor/AppVeyorAfterCompile.bat
+++ Source/AppVeyor/AppVeyorAfterCompile.bat
@@ -1,3 +1,2 @@
 @echo on
-copy Source\AppVeyor\Config.json Source\ACE.Server\Config.json
 source\AppVeyor\MySqlInstall.bat
--- Source/AppVeyor/AppVeyorBeforeCompile.bat
+++ Source/AppVeyor/AppVeyorBeforeCompile.bat
@@ -1,2 +1,3 @@
 @echo on
 nuget restore source\ACE.sln
+copy Source\AppVeyor\Config.json Source\ACE.Server\Config.json
