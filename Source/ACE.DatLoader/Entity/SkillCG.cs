ad6e1a31ea7a09e1c2e507feb04d4a8daf02155d
Mag-nus <Mag-nus@users.noreply.github.com>
2018-12-08 13:14:11 -0600

    Use HeritageGroup to determine skill cost overrides

--- Source/ACE.DatLoader/Entity/SkillCG.cs
+++ Source/ACE.DatLoader/Entity/SkillCG.cs
@@ -5,14 +5,14 @@ namespace ACE.DatLoader.Entity
     public class SkillCG : IUnpackable
     {
         public uint SkillNum { get; private set; }
-        public uint NormalCost { get; private set; }
-        public uint PrimaryCost { get; private set; }
+        public int NormalCost { get; private set; }
+        public int PrimaryCost { get; private set; }
 
         public void Unpack(BinaryReader reader)
         {
             SkillNum    = reader.ReadUInt32();
-            NormalCost  = reader.ReadUInt32();
-            PrimaryCost = reader.ReadUInt32();
+            NormalCost  = reader.ReadInt32();
+            PrimaryCost = reader.ReadInt32();
         }
     }
 }
