ac156bd19f5212ef3638a833d04e3ff74d332d88
Mag-nus <Mag-nus@users.noreply.github.com>
2018-12-10 20:44:30 -0600

    This fixes ServerPackets that were built with too many fragments, and thus too large (#1131)
    Because of this bug, ACE has been sending packets with no size limit. While this works well on a local network, it could have had reliablity issues over a WAN.
    
    ServerPacket has a MaxPacketSize of 464. This stems from the original days of ACE.
    
    I suspect this is the maximum size of AC related data we can stuff into a UDP packet (before overhead is added) to keep the UDP packet sent over the WAN in a single fragment.
    
    If we increase this value, it could reduce some server load and byte overhead, but, if a single fragment of the UDP is lost, all fragments will be lost and have to be resent.

--- Source/ACE.Server/Network/Packet.cs
+++ Source/ACE.Server/Network/Packet.cs
@@ -5,10 +5,6 @@ namespace ACE.Server.Network
 {
     public abstract class Packet
     {
-        public static uint MaxPacketSize { get; } = 1024;
-
-        public static uint MaxPacketDataSize { get; } = 464u;
-
         public PacketHeader Header { get; protected set; }
         public MemoryStream Data { get; internal set; }
         public List<PacketFragment> Fragments { get; } = new List<PacketFragment>();
