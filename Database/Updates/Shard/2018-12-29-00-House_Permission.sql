bf1425f3a68c45f83bc9916926792bb38d07b60b
Ty Conner <tyconner@itcproductions.com>
2018-12-29 14:48:03 -0500

    Update SQL script

--- Database/Updates/Shard/2018-12-29-00-House_Permission.sql
+++ Database/Updates/Shard/2018-12-29-00-House_Permission.sql
@@ -5,10 +5,10 @@ DROP TABLE IF EXISTS `house_permission`;
 CREATE TABLE `house_permission` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `house_Id` int(10) unsigned NOT NULL COMMENT 'GUID of House Biota Object',
-  `player_Id` int(10) unsigned NOT NULL COMMENT 'GUID of Player Biota Object being granted permission to this house',
+  `player_Guid` int(10) unsigned NOT NULL COMMENT 'GUID of Player Biota Object being granted permission to this house',
   `storage` bit(1) NOT NULL COMMENT 'Permission includes access to House Storage',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `biota_Id_house_Id_player_Id_uidx` (`house_Id`,`player_Id`),
+  UNIQUE KEY `biota_Id_house_Id_player_Guid_uidx` (`house_Id`,`player_Guid`) /*!80000 INVISIBLE */,
   KEY `biota_Id_house_Id_idx` (`house_Id`) /*!80000 INVISIBLE */,
   CONSTRAINT `biota_Id_house_Id` FOREIGN KEY (`house_Id`) REFERENCES `biota` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
