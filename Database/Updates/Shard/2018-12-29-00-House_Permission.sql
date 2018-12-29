75f4260db172492764a34d1a638da8c48c54acd3
Ty Conner <tyconner@itcproductions.com>
2018-12-29 14:51:21 -0500

    Remove Invisible on keys

--- Database/Updates/Shard/2018-12-29-00-House_Permission.sql
+++ Database/Updates/Shard/2018-12-29-00-House_Permission.sql
@@ -8,7 +8,7 @@ CREATE TABLE `house_permission` (
   `player_Guid` int(10) unsigned NOT NULL COMMENT 'GUID of Player Biota Object being granted permission to this house',
   `storage` bit(1) NOT NULL COMMENT 'Permission includes access to House Storage',
   PRIMARY KEY (`id`),
-  UNIQUE KEY `biota_Id_house_Id_player_Guid_uidx` (`house_Id`,`player_Guid`) /*!80000 INVISIBLE */,
-  KEY `biota_Id_house_Id_idx` (`house_Id`) /*!80000 INVISIBLE */,
+  UNIQUE KEY `biota_Id_house_Id_player_Guid_uidx` (`house_Id`,`player_Guid`),
+  KEY `biota_Id_house_Id_idx` (`house_Id`),
   CONSTRAINT `biota_Id_house_Id` FOREIGN KEY (`house_Id`) REFERENCES `biota` (`id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
