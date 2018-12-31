5ff3161160c538ea643bdfe1fec4c04d9357f2fe
Ty Conner <tyconner@itcproductions.com>
2018-12-31 12:38:07 -0500

    Remove indexes added in #1206

--- Database/Updates/Shard/2018-12-30-00-Enchantment_Registry_Comp_Key.sql
+++ Database/Updates/Shard/2018-12-30-00-Enchantment_Registry_Comp_Key.sql
@@ -3,8 +3,5 @@ USE `ace_shard`;
 ALTER TABLE `biota_properties_enchantment_registry` 
 DROP COLUMN `id`,
 DROP PRIMARY KEY,
-ADD PRIMARY KEY (`object_Id`, `enchantment_Category`, `spell_Id`, `layer_Id`),
-ADD INDEX `layer_Id_idx` (`layer_Id` ASC),
-ADD INDEX `spell_Category_idx` (`spell_Category` ASC),
-ADD INDEX `power_Level_idx` (`power_Level` ASC);
+ADD PRIMARY KEY (`object_Id`, `enchantment_Category`, `spell_Id`, `layer_Id`);
 ;
