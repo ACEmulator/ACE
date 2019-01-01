e556a290b76ab600b0f7dc6bb31c27b65303e387
gmriggs <gmriggs@gmail.com>
2018-12-31 21:46:22 -0500

    Removing unused indices

--- Database/Updates/Shard/2018-12-31-00-Enchantment-Index.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `biota_properties_enchantment_registry` ADD INDEX `enchantment_Category_idx` (`enchantment_Category`);
-ALTER TABLE `biota_properties_enchantment_registry` ADD INDEX `power_Level_idx` (`power_Level`);
-ALTER TABLE `biota_properties_enchantment_registry` ADD INDEX `layer_Id_idx` (`layer_Id`);
