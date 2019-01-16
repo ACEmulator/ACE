cecdecc7567fb93583aac6998e744f121e65d2d1
gmriggs <gmriggs@gmail.com>
2019-01-15 23:04:53 -0500

    Additional indoor/outdoor monster navigation (#1284)
    * Additional indoor/outdoor monster navigation
    
    * Fixing monsters alerting unattackable friends
    
    * More fixes for unattackable cleave targets, and no player self-harm with spell projectiles

--- Source/ACE.Server/WorldObjects/Monster_Awareness.cs
+++ Source/ACE.Server/WorldObjects/Monster_Awareness.cs
@@ -68,6 +68,9 @@ namespace ACE.Server.WorldObjects
         /// </summary>
         public virtual void Sleep()
         {
+            if (DebugMove)
+                Console.WriteLine($"{Name}.Sleep()");
+
             AttackTarget = null;
             IsAwake = false;
             IsMoving = false;
@@ -228,7 +231,7 @@ namespace ACE.Server.WorldObjects
             foreach (var obj in visibleObjs)
             {
                 var nearbyCreature = obj.WeenieObj.WorldObject as Creature;
-                if (nearbyCreature == null || nearbyCreature.IsAwake/* || nearbyCreature.IsAlerted*/)
+                if (nearbyCreature == null || nearbyCreature.IsAwake/* || nearbyCreature.IsAlerted*/ || !(nearbyCreature.GetProperty(PropertyBool.Attackable) ?? false))
                     continue;
 
                 if (CreatureType != null && CreatureType == nearbyCreature.CreatureType ||
