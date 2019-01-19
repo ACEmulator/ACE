f1b17f83bb16985e5fd0955b64f470657e671912
gmriggs <gmriggs@gmail.com>
2019-01-19 05:18:53 -0500

    Improved chest generators

--- Source/ACE.Server/WorldObjects/Chest.cs
+++ Source/ACE.Server/WorldObjects/Chest.cs
@@ -1,4 +1,3 @@
-using System;
 using ACE.Database.Models.Shard;
 using ACE.Database.Models.World;
 using ACE.Entity;
@@ -6,14 +5,12 @@ using ACE.Entity.Enum;
 using ACE.Entity.Enum.Properties;
 using ACE.Server.Entity;
 using ACE.Server.Entity.Actions;
-using ACE.Server.Factories;
 using ACE.Server.Network.GameEvent.Events;
 using ACE.Server.Network.GameMessages.Messages;
-using Microsoft.EntityFrameworkCore.Internal;
 
 namespace ACE.Server.WorldObjects
 {
-    public class Chest : Container, Lock
+    public partial class Chest : Container, Lock
     {
         /// <summary>
         /// A new biota be created taking all of its values from weenie.
@@ -51,105 +48,16 @@ namespace ACE.Server.WorldObjects
             ContainerCapacity = ContainerCapacity ?? 10;
             ItemCapacity = ItemCapacity ?? 120;
 
-            //Adding loot to chests
-            //Eventually these case statements would be linked to indivual treasure generators. Each one should be a different profile, but currently it will be the complete appropriate tier profile.
-            for (int i = 0; i < GeneratorProfiles.Count; i++)
-            {
-                int amount = ThreadSafeRandom.Next(2, 14);  //r.Next(2, 14);
-                var generator = GeneratorProfiles[i];
-                switch(generator.Biota.WeenieClassId)
-                {
-                    case 414:
-                    case 459:
-                    case 0:
-                    case 6:
-                    case 18:
-                    case 465:
-                        for (int j = 0; j < amount; j++)
-                        {
-                            var wo = LootGenerationFactory.CreateRandomLootObjects(1);
-                            TryAddToInventory(wo);
-                        }
-                        break;
-                    case 413:
-                    case 410:
-                    case 16:
-                    case 457:
-                    case 4:
-                    case 463:
-                    case 395:
-                        for (int j = 0; j < amount; j++)
-                        {
-                            var wo = LootGenerationFactory.CreateRandomLootObjects(2);
-                            TryAddToInventory(wo);
-                        }
-                        break;
-                    case 411:
-                    case 15:
-                    case 313:
-                    case 462:
-                    case 3:
-                    case 456:
-                    case 340:
-                    case 365:
-                        for (int j = 0; j < amount; j++)
-                        {
-                            var wo = LootGenerationFactory.CreateRandomLootObjects(3);
-                            TryAddToInventory(wo);
-                        }
-                        break;
-                    case 460:
-                    case 412:
-                    case 354:
-                    case 1:
-                    case 13:
-                    case 59:
-                    case 339:
-                        for (int j = 0; j < amount; j++)
-                        {
-                            var wo = LootGenerationFactory.CreateRandomLootObjects(4);
-                            TryAddToInventory(wo);
-                        }
-                        break;
-                    case 334:
-                    case 341:
-                    case 317:
-                        for (int j = 0; j < amount; j++)
-                        {
-                            var wo = LootGenerationFactory.CreateRandomLootObjects(5);
-                            TryAddToInventory(wo);
-                        }
-                        break;
-                    case 449:
-                    case 32:
-                    case 2:
-                    case 421:
-                    case 349:
-                    case 351:
-                    case 422:
-                    case 338:
-                        for (int j = 0; j < amount; j++)
-                        {
-                            var wo = LootGenerationFactory.CreateRandomLootObjects(6);
-                            TryAddToInventory(wo);
-                        }
-                        break;
-                    default:
-                        if(generator.Biota.WeenieClassId > 500)
-                        {
-                            //If the WeenieClassId is greater than the profile Id's, then it will be an item that is created with that Id.
-                            var wo = WorldObjectFactory.CreateNewWorldObject((uint)generator.Biota.WeenieClassId);
-                            TryAddToInventory(wo);
-                        }
-                        break;
-                }
-            }
+            //if (Generator != null)
+                //GenerateTreasure();
 
             CurrentMotionState = motionClosed; // What chest defaults to open?
 
             if (UseRadius < 2)
                 UseRadius = 2; // Until DoMoveTo (Physics, Indoor/Outside range variance) is smarter, use 2 is safest.
         }
+
+
 
         protected static readonly Motion motionOpen = new Motion(MotionStance.NonCombat, MotionCommand.On);
         protected static readonly Motion motionClosed = new Motion(MotionStance.NonCombat, MotionCommand.Off);
