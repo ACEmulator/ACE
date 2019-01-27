ACEmulator/ACE

commit 6560cea71264fda3b97c9052e9910b9aa5a4d896

GeneratorHeartbeat() is now ticked independantly from Heartbeat() (#1377)
* GeneratorHeartbeat() is now ticked independantly from Heartbeat()

There is no longer a DefaultHeartbeatInterval.

Heartbeats only happen if a HeartbeatInterval exists and is greater than 0.
Heartbeats are still staggered over heartbeatSpreadInterval (5s).

Generators will GeneratorHeartBeat() on the next tick after object construction.

However, if RegenerationInterval is 0, they will not beat again.

* woops

Source/ACE.Server/WorldObjects/Container_Tick.cs
@@ -3,12 +3,12 @@ namespace ACE.Server.WorldObjects
 {
     partial class Container
     {
-        public override void HeartBeat(double currentUnixTime)
+        public override void Heartbeat(double currentUnixTime)
         {
             foreach (var wo in Inventory.Values)
-                wo.HeartBeat(currentUnixTime);
+                wo.Heartbeat(currentUnixTime);
 
-            base.HeartBeat(currentUnixTime);
+            base.Heartbeat(currentUnixTime);
         }
     }
 }

Source/ACE.Server/WorldObjects/Creature_Tick.cs
@@ -6,16 +6,16 @@ namespace ACE.Server.WorldObjects
         /// <summary>
         /// Called every ~5 seconds for Creatures
         /// </summary>
-        public override void HeartBeat(double currentUnixTime)
+        public override void Heartbeat(double currentUnixTime)
         {
             foreach (var wo in EquippedObjects.Values)
-                wo.HeartBeat(currentUnixTime);
+                wo.Heartbeat(currentUnixTime);
 
             VitalHeartBeat();
 
             EmoteManager.HeartBeat();
 
-            base.HeartBeat(currentUnixTime);
+            base.Heartbeat(currentUnixTime);
         }
     }
 }

Source/ACE.Server/Entity/Landblock.cs
@@ -61,7 +61,8 @@ namespace ACE.Server.Entity
         // Cache used for Tick efficiency
         private readonly List<Player> players = new List<Player>();
         private readonly LinkedList<Creature> sortedCreaturesByNextTick = new LinkedList<Creature>();
-        private readonly LinkedList<WorldObject> sortedWorldObjectsByNextHeartBeat = new LinkedList<WorldObject>();
+        private readonly LinkedList<WorldObject> sortedWorldObjectsByNextHeartbeat = new LinkedList<WorldObject>();
+        private readonly LinkedList<WorldObject> sortedGeneratorsByNextGeneratorHeartbeat = new LinkedList<WorldObject>();
 
         public List<Landblock> Adjacents = new List<Landblock>();
 
@@ -301,7 +302,7 @@ namespace ACE.Server.Entity
             foreach (var player in players)
                 player.Player_Tick(currentUnixTime);
 
-            while (sortedCreaturesByNextTick.Count > 0)
+            while (sortedCreaturesByNextTick.Count > 0) // Monster_Tick()
             {
                 var first = sortedCreaturesByNextTick.First.Value;
 
@@ -318,16 +319,33 @@ namespace ACE.Server.Entity
                 }
             }
 
-            while (sortedWorldObjectsByNextHeartBeat.Count > 0)
+            while (sortedWorldObjectsByNextHeartbeat.Count > 0) // Heartbeat()
             {
-                var first = sortedWorldObjectsByNextHeartBeat.First.Value;
+                var first = sortedWorldObjectsByNextHeartbeat.First.Value;
 
                 // If they wanted to run before or at now
-                if (first.NextHeartBeatTime <= currentUnixTime)
+                if (first.NextHeartbeatTime <= currentUnixTime)
                 {
-                    sortedWorldObjectsByNextHeartBeat.RemoveFirst();
-                    first.HeartBeat(currentUnixTime);
-                    InsertWorldObjectIntoSortedHeartBeatList(first); // WorldObjects can have heartbeats at different intervals
+                    sortedWorldObjectsByNextHeartbeat.RemoveFirst();
+                    first.Heartbeat(currentUnixTime);
+                    InsertWorldObjectIntoSortedHeartbeatList(first); // WorldObjects can have heartbeats at different intervals
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            while (sortedGeneratorsByNextGeneratorHeartbeat.Count > 0) // GeneratorHeartbeat()
+            {
+                var first = sortedGeneratorsByNextGeneratorHeartbeat.First.Value;
+
+                // If they wanted to run before or at now
+                if (first.NextGeneratorHeartbeatTime <= currentUnixTime)
+                {
+                    sortedGeneratorsByNextGeneratorHeartbeat.RemoveFirst();
+                    first.GeneratorHeartbeat(currentUnixTime);
+                    InsertWorldObjectIntoSortedGeneratorHeartbeatList(first); // Generators can have heartbeats at different intervals
                 }
                 else
                 {
@@ -378,7 +396,8 @@ namespace ACE.Server.Entity
                     else if (kvp.Value is Creature creature)
                         sortedCreaturesByNextTick.AddLast(creature);
 
-                    InsertWorldObjectIntoSortedHeartBeatList(kvp.Value);
+                    InsertWorldObjectIntoSortedHeartbeatList(kvp.Value);
+                    InsertWorldObjectIntoSortedGeneratorHeartbeatList(kvp.Value);
                 }
 
                 pendingAdditions.Clear();
@@ -395,7 +414,8 @@ namespace ACE.Server.Entity
                         else if (wo is Creature creature)
                             sortedCreaturesByNextTick.Remove(creature);
 
-                        sortedWorldObjectsByNextHeartBeat.Remove(wo);
+                        sortedWorldObjectsByNextHeartbeat.Remove(wo);
+                        sortedGeneratorsByNextGeneratorHeartbeat.Remove(wo);
                     }
                 }
 
@@ -403,38 +423,72 @@ namespace ACE.Server.Entity
             }
         }
 
-        private void InsertWorldObjectIntoSortedHeartBeatList(WorldObject worldObject)
+        private void InsertWorldObjectIntoSortedHeartbeatList(WorldObject worldObject)
         {
             // If you want to add checks to exclude certain object types from heartbeating, you would do it here
-            if (worldObject.NextHeartBeatTime == double.MaxValue)
+            if (worldObject.NextHeartbeatTime == double.MaxValue)
                 return;
 
-            if (sortedWorldObjectsByNextHeartBeat.Count == 0)
+            if (sortedWorldObjectsByNextHeartbeat.Count == 0)
             {
-                sortedWorldObjectsByNextHeartBeat.AddFirst(worldObject);
+                sortedWorldObjectsByNextHeartbeat.AddFirst(worldObject);
                 return;
             }
 
-            if (sortedWorldObjectsByNextHeartBeat.Last.Value.NextHeartBeatTime <= worldObject.NextHeartBeatTime)
+            if (sortedWorldObjectsByNextHeartbeat.Last.Value.NextHeartbeatTime <= worldObject.NextHeartbeatTime)
             {
-                sortedWorldObjectsByNextHeartBeat.AddLast(worldObject);
+                sortedWorldObjectsByNextHeartbeat.AddLast(worldObject);
                 return;
             }
 
-            var currentNode = sortedWorldObjectsByNextHeartBeat.First;
+            var currentNode = sortedWorldObjectsByNextHeartbeat.First;
 
             while (currentNode != null)
             {
-                if (worldObject.NextHeartBeatTime <= currentNode.Value.NextHeartBeatTime)
+                if (worldObject.NextHeartbeatTime <= currentNode.Value.NextHeartbeatTime)
                 {
-                    sortedWorldObjectsByNextHeartBeat.AddBefore(currentNode, worldObject);
+                    sortedWorldObjectsByNextHeartbeat.AddBefore(currentNode, worldObject);
                     return;
                 }
 
                 currentNode = currentNode.Next;
             }
 
-            sortedWorldObjectsByNextHeartBeat.AddLast(worldObject); // This line really shouldn't be hit
+            sortedWorldObjectsByNextHeartbeat.AddLast(worldObject); // This line really shouldn't be hit
+        }
+
+        private void InsertWorldObjectIntoSortedGeneratorHeartbeatList(WorldObject worldObject)
+        {
+            // If you want to add checks to exclude certain object types from heartbeating, you would do it here
+            if (worldObject.NextGeneratorHeartbeatTime == double.MaxValue)
+                return;
+
+            if (sortedGeneratorsByNextGeneratorHeartbeat.Count == 0)
+            {
+                sortedGeneratorsByNextGeneratorHeartbeat.AddFirst(worldObject);
+                return;
+            }
+
+            if (sortedGeneratorsByNextGeneratorHeartbeat.Last.Value.NextGeneratorHeartbeatTime <= worldObject.NextGeneratorHeartbeatTime)
+            {
+                sortedGeneratorsByNextGeneratorHeartbeat.AddLast(worldObject);
+                return;
+            }
+
+            var currentNode = sortedGeneratorsByNextGeneratorHeartbeat.First;
+
+            while (currentNode != null)
+            {
+                if (worldObject.NextGeneratorHeartbeatTime <= currentNode.Value.NextGeneratorHeartbeatTime)
+                {
+                    sortedGeneratorsByNextGeneratorHeartbeat.AddBefore(currentNode, worldObject);
+                    return;
+                }
+
+                currentNode = currentNode.Next;
+            }
+
+            sortedGeneratorsByNextGeneratorHeartbeat.AddLast(worldObject); // This line really shouldn't be hit
         }
 
         public void EnqueueAction(IAction action)

Source/ACE.Server/WorldObjects/Player_Tick.cs
@@ -40,7 +40,7 @@ namespace ACE.Server.WorldObjects
         /// <summary>
         /// Called every ~5 seconds for Players
         /// </summary>
-        public override void HeartBeat(double currentUnixTime)
+        public override void Heartbeat(double currentUnixTime)
         {
             NotifyLandblocks();
 
@@ -59,7 +59,7 @@ namespace ACE.Server.WorldObjects
             if (LastRequestedDatabaseSave + PlayerSaveInterval <= DateTime.UtcNow)
                 SavePlayerToDatabase();
 
-            base.HeartBeat(currentUnixTime);
+            base.Heartbeat(currentUnixTime);
         }
 
         /// <summary>

Source/ACE.Server/WorldObjects/WorldObject.cs
@@ -92,7 +92,7 @@ namespace ACE.Server.WorldObjects
 
             InitializePropertyDictionaries();
             SetEphemeralValues();
-            InitializeHeartBeat();
+            InitializeHeartbeats();
 
             CreationTimestamp = (int)Time.GetUnixTime();
         }
@@ -110,7 +110,7 @@ namespace ACE.Server.WorldObjects
 
             InitializePropertyDictionaries();
             SetEphemeralValues();
-            InitializeHeartBeat();
+            InitializeHeartbeats();
         }
 
         /// <summary>
@@ -248,10 +248,6 @@ namespace ACE.Server.WorldObjects
                 ephemeralPositions[(PositionType)x.PositionType] = new Position(x.ObjCellId, x.OriginX, x.OriginY, x.OriginZ, x.AnglesX, x.AnglesY, x.AnglesZ, x.AnglesW);
 
             AddGeneratorProfiles();
-
-            // mosswartfood is both a creature and a generator with a HeartbeatInterval of 5 and a RegenerationInterval of 0
-            if (!(this is Creature) && IsGenerator)
-                HeartbeatInterval = RegenerationInterval;
 
             BaseDescriptionFlags = ObjectDescriptionFlag.Attackable;
 

Source/ACE.Server/WorldObjects/WorldObject_Generators.cs
@@ -495,7 +495,7 @@ namespace ACE.Server.WorldObjects
         /// <summary>
         /// Called every ~5 seconds for object generators
         /// </summary>
-        public void Generator_HeartBeat()
+        protected void Generator_HeartBeat()
         {
             //Console.WriteLine($"{Name}.Generator_HeartBeat({HeartbeatInterval})");
 

Source/ACE.Server/WorldObjects/WorldObject_Tick.cs
@@ -1,10 +1,8 @@
 using System;
-using System.Numerics;
 
 using ACE.Common;
 using ACE.Entity;
 using ACE.Entity.Enum.Properties;
-using ACE.Server.Entity;
 using ACE.Server.Entity.Actions;
 using ACE.Server.Managers;
 using ACE.Server.Physics;
@@ -14,59 +12,69 @@ namespace ACE.Server.WorldObjects
 {
     partial class WorldObject
     {
-        public const int DefaultHeartbeatInterval = 5;
+        private const int heartbeatSpreadInterval = 5;
 
         protected double CachedHeartbeatInterval;
-        public double NextHeartBeatTime;
-
-        private void InitializeHeartBeat()
-        {
-            CachedHeartbeatInterval = HeartbeatInterval ?? DefaultHeartbeatInterval;
-            QueueFirstHeartbeat(Time.GetUnixTime());
-        }
-
         /// <summary>
-        /// Enqueues the first heartbeat on a staggered 0-5s delay
+        /// A value of Double.MaxValue indicates that there is no NextHeartbeat
         /// </summary>
-        public void QueueFirstHeartbeat(double currentUnixTime)
-        {
-            // some generators have RegenerationInterval / HeartbeatInterval 0,
-            // which effectively means no automatic heartbeats, and are called directly by use activation
+        public double NextHeartbeatTime;
 
-            if (CachedHeartbeatInterval == 0)
+        private double cachedRegenerationInterval;
+        /// <summary>
+        /// A value of Double.MaxValue indicates that there is no NextGeneratorHeartbeat
+        /// </summary>
+        public double NextGeneratorHeartbeatTime;
+
+        private void InitializeHeartbeats()
+        {
+            var currentUnixTime = Time.GetUnixTime();
+
+            CachedHeartbeatInterval = HeartbeatInterval ?? 0;
+
+            if (CachedHeartbeatInterval > 0)
             {
-                // This will cause the HeartBeat to be called on the next tick
-                NextHeartBeatTime = currentUnixTime;
-                return;
+                // The intention of this code was just to spread the heartbeat ticks out a little over a 0-5s range,
+                var delay = ThreadSafeRandom.Next(0.0f, heartbeatSpreadInterval);
+
+                NextHeartbeatTime = currentUnixTime + delay;
+            }
+            else
+            {
+                NextHeartbeatTime = double.MaxValue; // Disable future HeartBeats
             }
 
-            // The intention of this code was just to spread the heartbeat ticks out a little over a 0-5s range,
-            var delay = ThreadSafeRandom.Next(0.0f, DefaultHeartbeatInterval);
+            cachedRegenerationInterval = RegenerationInterval;
 
-            NextHeartBeatTime = currentUnixTime + delay;
+            if (IsGenerator)
+                NextGeneratorHeartbeatTime = currentUnixTime; // Generators start right away
+            else
+                NextGeneratorHeartbeatTime = double.MaxValue; // Disable future GeneratorHeartBeats
         }
 
         /// <summary>
         /// Called every ~5 seconds for WorldObject base
         /// </summary>
-        public virtual void HeartBeat(double currentUnixTime)
+        public virtual void Heartbeat(double currentUnixTime)
         {
-            if (IsGenerator)
-                Generator_HeartBeat();
-
             if (EnchantmentManager.HasEnchantments)
                 EnchantmentManager.HeartBeat();
 
             SetProperty(PropertyFloat.HeartbeatTimestamp, currentUnixTime);
+            NextHeartbeatTime = currentUnixTime + CachedHeartbeatInterval;
+        }
 
-            // If an object has a CachedHeartbeatInterval of 0, we only do an initial HeartBeat
-            if (CachedHeartbeatInterval == 0)
-            {
-                NextHeartBeatTime = double.MaxValue; // Disable future HeartBeats
-                return;
-            }
+        /// <summary>
+        /// Called every [RegenerationInterval] seconds for WorldObject base
+        /// </summary>
+        public void GeneratorHeartbeat(double currentUnixTime)
+        {
+            Generator_HeartBeat();
 
-            NextHeartBeatTime = currentUnixTime + CachedHeartbeatInterval;
+            if (cachedRegenerationInterval > 0)
+                NextGeneratorHeartbeatTime = currentUnixTime + cachedRegenerationInterval;
+            else
+                NextGeneratorHeartbeatTime = double.MaxValue;
         }
 
         /// <summary>

commit 2fd9bb019844eb30c100118ff73a8a21a5d0b400

Fix ticking bug caused by #1377

Source/ACE.Server/WorldObjects/Container_Tick.cs
@@ -6,7 +6,10 @@ namespace ACE.Server.WorldObjects
         public override void Heartbeat(double currentUnixTime)
         {
             foreach (var wo in Inventory.Values)
-                wo.Heartbeat(currentUnixTime);
+            {
+                if (wo.NextHeartbeatTime <= currentUnixTime)
+                    wo.Heartbeat(currentUnixTime);
+            }
 
             base.Heartbeat(currentUnixTime);
         }

Source/ACE.Server/WorldObjects/Creature_Tick.cs
@@ -9,7 +9,10 @@ namespace ACE.Server.WorldObjects
         public override void Heartbeat(double currentUnixTime)
         {
             foreach (var wo in EquippedObjects.Values)
-                wo.Heartbeat(currentUnixTime);
+            {
+                if (wo.NextHeartbeatTime <= currentUnixTime)
+                    wo.Heartbeat(currentUnixTime);
+            }
 
             VitalHeartBeat();
 

Source/ACE.Server/WorldObjects/Player_Inventory.cs
@@ -670,7 +670,7 @@ namespace ACE.Server.WorldObjects
                     Session.Network.EnqueueSend(new GameEventInventoryServerSaveFailed(Session, item.Guid.Full));
                 }
 
-                if (itemRootOwner != containerRootOwner)
+                if (itemRootOwner == this && containerRootOwner != this)
                 {
                     // We must update the database with the latest ContainerId and WielderId properties.
                     // If we don't, the player can drop the item, log out, and log back in. If the landblock hasn't queued a database save in that time,