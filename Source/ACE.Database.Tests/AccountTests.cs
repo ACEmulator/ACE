e142a863bc7fe26dfe7ed86b61b27c258c0434fa
fartwhif <fartwhif@tuta.io>
2019-02-16 16:27:46 -0500

    Configuration file change from JSON to JavaScript (#1404)
    * Changing configuration file format to JavaScript instead of JSON for the ability to embed comments, Backwards compatible, ACE.Server project pre-build renames Config.json to Config.js when needed, but if that never happens it's still OK and falls back to the old filename.
    Changed startup batch files to determine what the current directory should be and change to it before starting ACE
    
    * fixed
    
    * move config old to new filename upon program start

--- Source/ACE.Database.Tests/AccountTests.cs
+++ Source/ACE.Database.Tests/AccountTests.cs
@@ -17,8 +17,8 @@ namespace ACE.Database.Tests
         [ClassInitialize]
         public static void TestSetup(TestContext context)
         {
-            // copy config.json
-            File.Copy(Path.Combine(Environment.CurrentDirectory, "..\\..\\..\\..\\..\\ACE.Server\\Config.json"), ".\\Config.json", true);
+            // copy config.js
+            File.Copy(Path.Combine(Environment.CurrentDirectory, "..\\..\\..\\..\\..\\ACE.Server\\Config.js"), ".\\Config.js", true);
 
             ConfigManager.Initialize();
             authDb = new AuthenticationDatabase();
--- Source/ACE.Server/start_server.bat
+++ Source/ACE.Server/start_server.bat
@@ -1,1 +1,4 @@
+@cd %~dp0
+@if not exist "ACE.Server.dll" (echo please run the copy of this file residing in the output folder: .\bin\x64\XXXXX\netcoreapp2.1\
+pause)
 dotnet ACE.Server.dll
