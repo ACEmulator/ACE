99b6cef998627a2391b2aeab5efa5526a7ca13c9
gmriggs <gmriggs@gmail.com>
2019-03-27 20:26:35 -0400

    adding comment

--- Source/ACE.Server/WorldObjects/Container.cs
+++ Source/ACE.Server/WorldObjects/Container.cs
@@ -490,6 +490,11 @@ namespace ACE.Server.WorldObjects
 
             foreach (var item in Inventory.Values)
             {
+                // ideally this should be done elsewhere,
+                // when an item is first placed into a chest container
+                // this needs further investigation, ie. should items in a player's inventory
+                // have Placement.Resting? keeping this to Chest/Container.Open for now...
+
                 item.Placement = ACE.Entity.Enum.Placement.Resting;
 
                 // FIXME: only send messages for unknown objects
