596b6ed56df6ffdade48610086acfa0625ced194
Ty Conner <tyconner@itcproductions.com>
2019-03-31 05:23:04 -0400

    .

--- Source/ACE.Server/Entity/IPlayer.cs
+++ Source/ACE.Server/Entity/IPlayer.cs
@@ -1,5 +1,7 @@
 using System;
 
+using ACE.Database.Models.Auth;
+
 using ACE.Entity;
 using ACE.Entity.Enum.Properties;
 using ACE.Server.WorldObjects;
@@ -14,7 +16,7 @@ namespace ACE.Server.Entity
     {
         ObjectGuid Guid { get; }
 
-        Database.Models.Auth.Account Account { get; }
+        Account Account { get; }
 
         bool? GetProperty(PropertyBool property);
         uint? GetProperty(PropertyDataId property);
