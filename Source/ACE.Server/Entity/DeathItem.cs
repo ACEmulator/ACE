76a805098fce4e409b5e78c327854f8e537e976b
Mag-nus <Mag-nus@users.noreply.github.com>
2019-04-09 07:42:29 -0500

    Fix OrderBy -> ThenBy

--- Source/ACE.Server/Entity/DeathItem.cs
+++ Source/ACE.Server/Entity/DeathItem.cs
@@ -65,7 +65,7 @@ namespace ACE.Server.Entity
             Inventory = Inventory.Where(i => i.Category != DeathItemCategory.None).ToList();
 
             // sort by original value and category
-            Inventory = Inventory.OrderByDescending(i => i.AdjustedValue).OrderBy(i => i.Category).ToList();
+            Inventory = Inventory.OrderByDescending(i => i.AdjustedValue).ThenBy(i => i.Category).ToList();
 
             // halve the values of every item, except the most valued item in each category
             HalfValues(Inventory);
