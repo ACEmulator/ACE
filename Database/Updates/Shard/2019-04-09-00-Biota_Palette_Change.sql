1bab70c907b6d281a9b9d75c5019ffbc8f5a325b
Ty Conner <tyconner@itcproductions.com>
2019-04-10 14:02:59 -0400

    Update 2019-04-09-00-Biota_Palette_Change.sql

--- Database/Updates/Shard/2019-04-09-00-Biota_Palette_Change.sql
+++ Database/Updates/Shard/2019-04-09-00-Biota_Palette_Change.sql
@@ -1,7 +1,8 @@
 USE `ace_shard`;
 
-ALTER TABLE `biota_properties_palette` 
-DROP FOREIGN KEY `wcid_palette`,
+ALTER TABLE `biota_properties_palette`
+DROP FOREIGN KEY `wcid_palette`;
+ALTER TABLE `biota_properties_palette`
 ADD INDEX `wcid_palette_idx` (`object_Id` ASC),
 DROP INDEX `object_Id_subPaletteId_offset_length_uidx` ;
 ;
