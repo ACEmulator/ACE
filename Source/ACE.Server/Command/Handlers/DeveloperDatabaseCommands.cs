10d9d573939139a309a49f51cbd1a0f02f55309a
Ty Conner <tyconner@itcproductions.com>
2019-04-19 04:12:20 -0400

    Update DeveloperDatabaseCommands.cs

--- Source/ACE.Server/Command/Handlers/DeveloperDatabaseCommands.cs
+++ Source/ACE.Server/Command/Handlers/DeveloperDatabaseCommands.cs
@@ -10,12 +10,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 
-using Microsoft.EntityFrameworkCore;
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-
 namespace ACE.Server.Command.Handlers
 {
     public static class DeveloperDatabaseCommands
