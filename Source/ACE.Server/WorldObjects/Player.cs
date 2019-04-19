709777fb0cfcc7956c4a01de961b815001e0456f
gmriggs <gmriggs@gmail.com>
2019-04-18 22:16:21 -0400

    Adding busy check to food/consumables

--- Source/ACE.Server/WorldObjects/Player.cs
+++ Source/ACE.Server/WorldObjects/Player.cs
@@ -865,6 +865,10 @@ namespace ACE.Server.WorldObjects
         /// <param name="spellDID">Id of the spell cast by the consumable; can be null, if buffType != ConsumableBuffType.Spell</param>
         public void ApplyConsumable(string consumableName, Sound sound, ConsumableBuffType buffType, uint? boostAmount, uint? spellDID)
         {
+            if (IsBusy) return;
+
+            IsBusy = true;
+
             MotionCommand motionCommand;
 
             if (sound == Sound.Eat1)
@@ -947,6 +951,8 @@ namespace ACE.Server.WorldObjects
                 // return to original stance
                 var returnStance = new Motion(CurrentMotionState.Stance);
                 EnqueueBroadcastMotion(returnStance);
+
+                IsBusy = false;
             });
 
            actionChain.EnqueueChain();
