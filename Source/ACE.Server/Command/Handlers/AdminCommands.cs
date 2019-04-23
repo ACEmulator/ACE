f42423996c21bfe7d05575043f106b1f3e52a39a
gmriggs <gmriggs@gmail.com>
2019-04-23 02:18:59 -0400

    updating tinkering message to match retail, adding /cisalvage (#1779)
    * updating tinkering message to match retail, adding /cisalvage
    
    * adding /cm

--- Source/ACE.Server/Command/Handlers/AdminCommands.cs
+++ Source/ACE.Server/Command/Handlers/AdminCommands.cs
@@ -1322,15 +1322,6 @@ namespace ACE.Server.Command.Handlers
             if (notOK) ChatPacket.SendServerMessage(session, "Appraise a locked target before using @crack", ChatMessageType.Broadcast);
         }
 
-        // cm <material type> <quantity> <ave. workmanship>
-        [CommandHandler("cm", AccessLevel.Developer, CommandHandlerFlag.RequiresWorld, 3)]
-        public static void HandleCM(Session session, params string[] parameters)
-        {
-            // Format is: @cm <material type> <quantity> <ave. workmanship>
-
-            // TODO: output
-        }
-
         /// <summary>
         /// Displays how much experience the last appraised creature is worth when killed.
         /// </summary>
@@ -2354,6 +2345,50 @@ namespace ACE.Server.Command.Handlers
                 allegiance.ShowInfo();
                 Console.WriteLine("---------------");
             }
+        }
+
+        // cm <material type> <quantity> <ave. workmanship>
+        [CommandHandler("cm", AccessLevel.Developer, CommandHandlerFlag.RequiresWorld, 3)]
+        public static void HandleCM(Session session, params string[] parameters)
+        {
+            // Format is: @cm <material type> <quantity> <ave. workmanship>
+            HandleCISalvage(session);
+
+            // TODO: output
+        }
+
+        [CommandHandler("cisalvage", AccessLevel.Admin, CommandHandlerFlag.None, 1, "Create a salvage bag in your inventory", "<material_type>, optional: <structure> <workmanship> <num_items>")]
+        public static void HandleCISalvage(Session session, params string[] parameters)
+        {
+            if (!Enum.TryParse(parameters[0], out MaterialType materialType))
+            {
+                session.Network.EnqueueSend(new GameMessageSystemChat($"Couldn't find material type {parameters[0]}", ChatMessageType.Broadcast));
+                return;
+            }
+
+            var wcid = (uint)Player.MaterialSalvage[(int)materialType];
+            var salvageBag = WorldObjectFactory.CreateNewWorldObject(wcid);
+
+            ushort structure = 100;
+            if (parameters.Length > 1)
+                ushort.TryParse(parameters[1], out structure);
+
+            var workmanship = 10f;
+            if (parameters.Length > 2)
+                float.TryParse(parameters[2], out workmanship);
+
+            var numItemsInMaterial = (int)Math.Round(workmanship);
+            if (parameters.Length > 3)
+                int.TryParse(parameters[3], out numItemsInMaterial);
+
+            var itemWorkmanship = (int)Math.Round(workmanship * numItemsInMaterial);
+
+            salvageBag.Name = $"Salvage ({structure})";
+            salvageBag.Structure = structure;
+            salvageBag.ItemWorkmanship = itemWorkmanship;
+            salvageBag.NumItemsInMaterial = numItemsInMaterial;
+
+            session.Player.TryCreateInInventoryWithNetworking(salvageBag);
         }
     }
 }
