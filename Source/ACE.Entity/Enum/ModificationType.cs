355e775c3907a5fcf97b473317fbdc909568914e
Ty Conner <tyconner@itcproductions.com>
2019-05-01 11:38:46 -0400

    Reorg Enums

---
+++ Source/ACE.Entity/Enum/ModificationType.cs
@@ -0,0 +1,14 @@
+namespace ACE.Entity.Enum
+{
+    public enum ModificationType
+    {
+        SuccessTarget = 0,
+        SuccessSource = 1,
+        SuccessPlayer = 2,
+        SuccessResult = 3,
+        FailureTarget = 4,
+        FailureSource = 5,
+        FailurePlayer = 6,
+        FailureResult = 7
+    };
+}
