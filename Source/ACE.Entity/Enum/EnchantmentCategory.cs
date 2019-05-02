ae5b826f2a1b8e7e43c02e4f0de229b223588696
gmriggs <gmriggs@gmail.com>
2019-05-02 05:34:36 -0400

    updating enchantment masks (#1840)
    * updating enchantment masks
    
    * refactoring

--- Source/ACE.Entity/Enum/EnchantmentCategory.cs
+++ Source/ACE.Entity/Enum/EnchantmentCategory.cs
@@ -5,8 +5,8 @@ namespace ACE.Entity.Enum
     [Flags]
     public enum EnchantmentMask
     {
-        LifeSpells      = 0x1,
-        CreatureSpells  = 0x2,
+        Multiplicative  = 0x1,
+        Additive        = 0x2,
         Vitae           = 0x4,
         Cooldown        = 0x8,
     };
