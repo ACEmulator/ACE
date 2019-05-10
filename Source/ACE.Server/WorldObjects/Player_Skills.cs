d48eb432e2c74a151cb1c01b3c0aae33bd9e983d
Theran578 <49040547+Theran578@users.noreply.github.com>
2019-05-10 00:28:25 -0700

    Remove check, as GrantXP already does it

--- Source/ACE.Server/WorldObjects/Player_Skills.cs
+++ Source/ACE.Server/WorldObjects/Player_Skills.cs
@@ -208,15 +208,11 @@ namespace ACE.Server.WorldObjects
         /// <param name="amount"></param>
         public void AwardSkillXP(Skill skill, uint amount)
         {
-            var xpTable = DatManager.PortalDat.XpTable;
-            var maxLevelXp = xpTable.CharacterLevelXPList.Last();
-
             var playerSkill = GetCreatureSkill(skill);
 
             if (!IsSkillMaxRank(playerSkill.Ranks, playerSkill.AdvancementClass))
             {
-                if ((ulong)(AvailableExperience ?? 0) < maxLevelXp)
-                    GrantXP(amount, XpType.Emote, false);
+                GrantXP(amount, XpType.Emote, false);
                 RaiseSkillGameAction(skill, amount, false);
             }
         }
