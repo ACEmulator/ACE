5b5c99212e03fee02e1b1ef68b09b791816d1473
Ty Conner <tyconner@itcproductions.com>
2019-05-23 02:31:01 -0400

    Update 2019-05-23-00-Update_Biota_IID_Table.sql

--- Database/Updates/Shard/2019-05-23-00-Update_Biota_IID_Table.sql
+++ Database/Updates/Shard/2019-05-23-00-Update_Biota_IID_Table.sql
@@ -1,6 +1,6 @@
 USE `ace_shard`;
 
 ALTER TABLE `biota_properties_i_i_d`
-ADD COLUMN `type_value_combined` BIGINT(10) GENERATED ALWAYS AS ((`type` << 32) + `value`) VIRTUAL AFTER `value`,
-ADD INDEX `type_value_combined_idx` (`type_value_combined` ASC);
+ADD COLUMN `type_value_combined` BIGINT(10) GENERATED ALWAYS AS ((`type` << 32) + `value`) VIRTUAL AFTER `value`/*,*/
+/*ADD INDEX `type_value_combined_idx` (`type_value_combined` ASC);*/
 ;
