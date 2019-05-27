6429acd3ec7f4077aeb8002565471622fa62ce2c
Mag-nus <Mag-nus@users.noreply.github.com>
2019-05-27 06:26:42 -0500

    Create database index for GetDynamicObjectsByLandblock

--- Source/ACE.Database/ShardDatabase.cs
+++ Source/ACE.Database/ShardDatabase.cs
@@ -1254,12 +1254,15 @@ namespace ACE.Database
         {
             var dynamics = new List<Biota>();
 
+            var min = landblockId << 16;
+            var max = min | 0xFFFF;
+
             using (var context = new ShardDbContext())
             {
                 context.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
 
                 var results = context.BiotaPropertiesPosition
-                    .Where(p => p.PositionType == 1 && p.ObjCellId >> 16 == landblockId && p.ObjectId >= 0x80000000)
+                    .Where(p => p.PositionType == 1 && p.ObjCellId >= min && p.ObjCellId <= max && p.ObjectId >= 0x80000000)
                     .ToList();
 
                 foreach (var result in results)
@@ -1285,12 +1288,15 @@ namespace ACE.Database
         {
             var dynamics = new ConcurrentBag<Biota>();
 
+            var min = landblockId << 16;
+            var max = min | 0xFFFF;
+
             using (var context = new ShardDbContext())
             {
                 context.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
 
                 var results = context.BiotaPropertiesPosition
-                    .Where(p => p.PositionType == 1 && p.ObjCellId >> 16 == landblockId && p.ObjectId >= 0x80000000)
+                    .Where(p => p.PositionType == 1 && p.ObjCellId >= min && p.ObjCellId <= max && p.ObjectId >= 0x80000000)
                     .ToList();
 
                 Parallel.ForEach(results, result =>
