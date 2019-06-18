3d13293e86fb8076427ce24aff69a57cc53c1ff8
Ty Conner <tyconner@itcproductions.com>
2019-06-17 20:32:34 -0400

    Add CMS setting if MotionTableId is present

--- Source/ACE.Server/WorldObjects/WorldObject.cs
+++ Source/ACE.Server/WorldObjects/WorldObject.cs
@@ -265,9 +265,11 @@ namespace ACE.Server.WorldObjects
             if (Placement == null)
                 Placement = ACE.Entity.Enum.Placement.Resting;
 
-            //CurrentMotionState = new Motion(MotionStance.Invalid, new MotionItem(MotionCommand.Invalid));
+            if (MotionTableId != 0)
+                CurrentMotionState = new Motion(MotionStance.Invalid);
+
             if (WeenieType == WeenieType.Corpse)
-                HeartbeatInterval = 5;
+                HeartbeatInterval = 5;            
         }
 
         /// <summary>
