cd4d2cf7e7f42ac9fe935e3f0747267925b56b04
gmriggs <gmriggs@gmail.com>
2019-06-25 01:46:07 -0400

    adding import-sql

--- Source/ACE.Server/Command/Handlers/DeveloperContentCommands.cs
+++ Source/ACE.Server/Command/Handlers/DeveloperContentCommands.cs
@@ -13,29 +13,16 @@ namespace ACE.Server.Command.Handlers.Processors
 {
     public class DeveloperContentCommands
     {
-        [CommandHandler("import", AccessLevel.Developer, CommandHandlerFlag.None, 1, "Imports a weenie from the Content folder", "<wcid>")]
-        public static void HandleImport(Session session, params string[] parameters)
+        [CommandHandler("import-json", AccessLevel.Developer, CommandHandlerFlag.None, 1, "Imports a JSON weenie from the Content folder", "<wcid>")]
+        public static void HandleImportJson(Session session, params string[] parameters)
         {
-            var content_folder = PropertyManager.GetString("content_folder").Item;
+            DirectoryInfo di = VerifyContentFolder(session);
+            if (!di.Exists) return;
 
             var sep = Path.DirectorySeparatorChar;
 
-            // handle relative path
-            if (content_folder.StartsWith("."))
-            {
-                var cwd = Directory.GetCurrentDirectory() + sep;
-                content_folder = cwd + content_folder;
-            }
-
-            var di = new DirectoryInfo(content_folder);
-            if (!di.Exists)
-            {
-                CommandHandlerHelper.WriteOutputInfo(session, $"Couldn't find content folder: {di.FullName}");
-                CommandHandlerHelper.WriteOutputInfo(session, "To set your content folder, /modifystring content_folder <path>");
-                return;
-            }
-
             var json_folder = $"{di.FullName}{sep}json{sep}weenies{sep}";
+            var sql_folder = json_folder.Replace("json", "sql");
 
             var wcid = parameters[0];
             var prefix = wcid + " - ";
@@ -54,19 +41,72 @@ namespace ACE.Server.Command.Handlers.Processors
 
             // convert json -> sql
             var sqlFile = json2sql(session, json_folder, fi.Name);
-            if (sqlFile == null)
-                return;
+            if (sqlFile == null) return;
 
             // import sql to db
-            ImportSQL(sqlFile);
+            ImportSQL(sql_folder + sqlFile);
             CommandHandlerHelper.WriteOutputInfo(session, $"Imported {sqlFile}");
 
             // clear this weenie out of the cache
             if (uint.TryParse(wcid, out var weenieClassId))
-            {
                 DatabaseManager.World.ClearCachedWeenie(weenieClassId);
-                //var wo = WorldObjectFactory.CreateNewWorldObject(weenieClassId);
+        }
+
+        [CommandHandler("import-sql", AccessLevel.Developer, CommandHandlerFlag.None, 1, "Imports SQL weenie from the Content folder", "<wcid>")]
+        public static void HandleImportSQL(Session session, params string[] parameters)
+        {
+            DirectoryInfo di = VerifyContentFolder(session);
+            if (!di.Exists) return;
+
+            var sep = Path.DirectorySeparatorChar;
+
+            var sql_folder = $"{di.FullName}{sep}sql{sep}weenies{sep}";
+
+            var wcid = parameters[0];
+            var prefix = wcid + " - ";
+
+            di = new DirectoryInfo(sql_folder);
+
+            var files = di.Exists ? di.GetFiles($"{prefix}*.sql") : null;
+
+            if (files == null || files.Length == 0)
+            {
+                CommandHandlerHelper.WriteOutputInfo(session, $"Couldn't find {sql_folder}{prefix}*.sql");
+                return;
             }
+
+            var sqlFile = files[0].Name;
+
+            // import sql to db
+            ImportSQL(sql_folder + sqlFile);
+            CommandHandlerHelper.WriteOutputInfo(session, $"Imported {sqlFile}");
+
+            // clear this weenie out of the cache
+            if (uint.TryParse(wcid, out var weenieClassId))
+                DatabaseManager.World.ClearCachedWeenie(weenieClassId);
+        }
+
+        private static DirectoryInfo VerifyContentFolder(Session session)
+        {
+            var content_folder = PropertyManager.GetString("content_folder").Item;
+
+            var sep = Path.DirectorySeparatorChar;
+
+            // handle relative path
+            if (content_folder.StartsWith("."))
+            {
+                var cwd = Directory.GetCurrentDirectory() + sep;
+                content_folder = cwd + content_folder;
+            }
+
+            var di = new DirectoryInfo(content_folder);
+
+            if (!di.Exists)
+            {
+                CommandHandlerHelper.WriteOutputInfo(session, $"Couldn't find content folder: {di.FullName}");
+                CommandHandlerHelper.WriteOutputInfo(session, "To set your content folder, /modifystring content_folder <path>");
+            }
+            return di;
         }
 
         public static string json2sql(Session session, string folder, string json_filename)
