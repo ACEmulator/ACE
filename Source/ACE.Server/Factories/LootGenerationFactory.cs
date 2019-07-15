e567d24bad0cd036667d73df3435828066b0cb3a
gmriggs <gmriggs@gmail.com>
2019-07-15 17:47:12 -0400

    re-adjusting epic cantrip rates

--- Source/ACE.Server/Factories/LootGenerationFactory.cs
+++ Source/ACE.Server/Factories/LootGenerationFactory.cs
@@ -1913,15 +1913,15 @@ namespace ACE.Server.Factories
 
             var dropRateMod = 1.0 / dropRate;
 
-            // 0.1% chance for 1 Epic, 0.01% chance for 2 Epics,
-            // 0.001% chance for 3 Epics, 0.0001% chance for 4 Epics 
-            if (ThreadSafeRandom.Next(1, (int)(1000 * dropRateMod)) == 1)
+            // 1% chance for 1 Epic, 0.1% chance for 2 Epics,
+            // 0.01% chance for 3 Epics, 0.001% chance for 4 Epics 
+            if (ThreadSafeRandom.Next(1, (int)(100 * dropRateMod)) == 1)
                 numEpics = 1;
-            if (ThreadSafeRandom.Next(1, (int)(10000 * dropRateMod)) == 1)
+            if (ThreadSafeRandom.Next(1, (int)(1000 * dropRateMod)) == 1)
                 numEpics = 2;
-            if (ThreadSafeRandom.Next(1, (int)(100000 * dropRateMod)) == 1)
+            if (ThreadSafeRandom.Next(1, (int)(10000 * dropRateMod)) == 1)
                 numEpics = 3;
-            if (ThreadSafeRandom.Next(1, (int)(1000000 * dropRateMod)) == 1)
+            if (ThreadSafeRandom.Next(1, (int)(100000 * dropRateMod)) == 1)
                 numEpics = 4;
 
             return numEpics;
