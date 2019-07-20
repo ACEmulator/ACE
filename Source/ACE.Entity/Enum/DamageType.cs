82003bd90e3520c54cf9f4f2c90a9103ed8724e4
gmriggs <gmriggs@gmail.com>
2019-07-19 23:48:55 -0400

    fixing ebon rifts / adding support for multiple damage types

--- Source/ACE.Entity/Enum/DamageType.cs
+++ Source/ACE.Entity/Enum/DamageType.cs
@@ -1,5 +1,7 @@
 using System;
 
+using ACE.Common.Extensions;
+
 namespace ACE.Entity.Enum
 {
     [Flags]
@@ -26,21 +28,53 @@ namespace ACE.Entity.Enum
         {
             switch (damageType)
             {
-                case DamageType.Undef: return "Undefined";
-                case DamageType.Slash: return "Slashing";
-                case DamageType.Pierce: return "Piercing";
+                case DamageType.Undef:    return "Undefined";
+                case DamageType.Slash:    return "Slashing";
+                case DamageType.Pierce:   return "Piercing";
                 case DamageType.Bludgeon: return "Bludgeoning";
-                case DamageType.Cold: return "Cold";
-                case DamageType.Fire: return "Fire";
-                case DamageType.Acid: return "Acid";
+                case DamageType.Cold:     return "Cold";
+                case DamageType.Fire:     return "Fire";
+                case DamageType.Acid:     return "Acid";
                 case DamageType.Electric: return "Electric";
-                case DamageType.Health: return "Health";
-                case DamageType.Stamina: return "Stamina";
-                case DamageType.Mana: return "Mana";
-                case DamageType.Nether: return "Nether";
-                case DamageType.Base: return "Base";
-                default: return null;
+                case DamageType.Health:   return "Health";
+                case DamageType.Stamina:  return "Stamina";
+                case DamageType.Mana:     return "Mana";
+                case DamageType.Nether:   return "Nether";
+                case DamageType.Base:     return "Base";
+                default:
+                    return null;
             }
+        }
+
+        public static bool IsMultiDamage(this DamageType damageType)
+        {
+            switch (damageType)
+            {
+                case DamageType.Undef:
+                case DamageType.Slash:
+                case DamageType.Pierce:
+                case DamageType.Bludgeon:
+                case DamageType.Cold:
+                case DamageType.Fire:
+                case DamageType.Acid:
+                case DamageType.Electric:
+                case DamageType.Health:
+                case DamageType.Stamina:
+                case DamageType.Mana:
+                case DamageType.Nether:
+                case DamageType.Base:
+                    return false;
+            }
+            return true;
+        }
+
+        public static DamageType SelectDamageType(this DamageType damageType)
+        {
+            var damageTypes = EnumHelper.GetFlags(damageType);
+
+            var rng = ThreadSafeRandom.Next(0, damageTypes.Count - 1);
+
+            return (DamageType)damageTypes[rng];
         }
     }
 }
