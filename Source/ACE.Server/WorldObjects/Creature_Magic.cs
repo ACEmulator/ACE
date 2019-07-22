42421af052d15585d8f733b8529dd42c971e90e6
Slushnas <Slushnas@users.noreply.github.com>
2019-07-21 22:41:27 -0700

    Add Curse of Raven Fury spell (#2140)
    * Add NonTracking spell property.
    
    * Fix DefaultScript and DefaultScriptIntensity serialization.
    
    * Give EncumbranceVal a default value of 0.
    
    * Add support for Curse of Raven Fury spell and some minor refactoring.
    
    * A few minor tweaks.
    
    * Add CasterEffect

--- Source/ACE.Server/WorldObjects/Creature_Magic.cs
+++ Source/ACE.Server/WorldObjects/Creature_Magic.cs
@@ -180,7 +180,7 @@ namespace ACE.Server.WorldObjects
 
                 case MagicSchool.LifeMagic:
 
-                    LifeMagic(this, spell, out uint damage, out bool critical, out enchantmentStatus, item, true);
+                    LifeMagic(spell, out uint damage, out bool critical, out enchantmentStatus, this, item, true);
                     if (enchantmentStatus.Message != null)
                         EnqueueBroadcast(new GameMessageScript(Guid, spell.TargetEffect, spell.Formula.Scale));
 
