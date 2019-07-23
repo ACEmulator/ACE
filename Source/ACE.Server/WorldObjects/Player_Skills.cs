83c6625744c826c5dcfbf458bc1091df900904b0
gmriggs <gmriggs@gmail.com>
2019-07-23 03:05:30 -0400

    adding universal masteries

--- Source/ACE.Server/WorldObjects/Player_Skills.cs
+++ Source/ACE.Server/WorldObjects/Player_Skills.cs
@@ -6,6 +6,7 @@ using ACE.DatLoader;
 using ACE.Entity.Enum;
 using ACE.Entity.Enum.Properties;
 using ACE.Server.Entity.Actions;
+using ACE.Server.Managers;
 using ACE.Server.Network.GameMessages.Messages;
 using ACE.Server.WorldObjects.Entity;
 
@@ -551,7 +552,14 @@ namespace ACE.Server.WorldObjects
 
         public override bool GetHeritageBonus(WorldObject weapon)
         {
-            return GetHeritageBonus(GetWeaponType(weapon));
+            if (PropertyManager.GetBool("universal_masteries").Item)
+            {
+                // https://asheron.fandom.com/wiki/Spring_2014_Update
+                // end of retail - universal masteries
+                return weapon != null && weapon.IsMasterable;
+            }
+            else
+                return GetHeritageBonus(GetWeaponType(weapon));
         }
 
         public bool GetHeritageBonus(WeaponType weaponType)
