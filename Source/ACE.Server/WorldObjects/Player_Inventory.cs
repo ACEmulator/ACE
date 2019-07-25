062d975767f1613fc111947d8d5fcc29502f8468
gmriggs <gmriggs@gmail.com>
2019-07-25 11:17:21 -0400

    adding prevention/debug code for spellbook probability 0

--- Source/ACE.Server/WorldObjects/Player_Inventory.cs
+++ Source/ACE.Server/WorldObjects/Player_Inventory.cs
@@ -266,6 +266,13 @@ namespace ACE.Server.WorldObjects
                     if (item.HasProcSpell((uint)spell.Spell))
                         continue;
 
+                    if (spell.Probability == 0.0f)
+                    {
+                        var _spell = new Spell(spell.Spell, false);
+                        log.Warn($"{Name}.TryEquipObjectWithNetworking({item.Name} ({item.Guid}, wcid {item.WeenieClassId})) - spellbook contains {_spell.Name} probability 0, ignoring");
+                        continue;
+                    }
+
                     var enchantmentStatus = CreateItemSpell(item, (uint)spell.Spell);
                     if (enchantmentStatus.Success)
                         item.IsAffecting = true;
