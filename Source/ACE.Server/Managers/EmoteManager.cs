5c466f00c41a406fb0f262b093f559e42a6a33a5
Ty Conner <tyconner@itcproductions.com>
2019-08-20 02:01:04 -0400

    Update TakeItems msg to match retail messaging

--- Source/ACE.Server/Managers/EmoteManager.cs
+++ Source/ACE.Server/Managers/EmoteManager.cs
@@ -1066,7 +1066,7 @@ namespace ACE.Server.Managers
                             var itemTaken = WorldObjectFactory.CreateWorldObject(DatabaseManager.World.GetCachedWeenie(emote.WeenieClassId ?? 0), ObjectGuid.Invalid);
                             if (itemTaken != null)
                             {
-                                var msg = $"You hand over {emote.StackSize ?? 1} of your {itemTaken.GetPluralName()}.";
+                                var msg = $"You hand over {(player.GetNumInventoryItemsOfWCID(emote.WeenieClassId ?? 0) == 0 ? "all" : (emote.StackSize ?? 1).ToString())} of your {itemTaken.GetPluralName()}.";
                                 player.Session.Network.EnqueueSend(new GameMessageSystemChat(msg, ChatMessageType.Broadcast));
                             }
                         }
