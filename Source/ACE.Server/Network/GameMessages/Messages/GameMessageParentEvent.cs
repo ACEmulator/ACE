079f6d61e9d7dd5a268cbc362e86a3167b5f1244
Ty Conner <tyconner@itcproductions.com>
2019-08-24 00:04:10 -0400

    Update GameMessageParentEvent.cs

--- Source/ACE.Server/Network/GameMessages/Messages/GameMessageParentEvent.cs
+++ Source/ACE.Server/Network/GameMessages/Messages/GameMessageParentEvent.cs
@@ -6,15 +6,15 @@ namespace ACE.Server.Network.GameMessages.Messages
 {
     public class GameMessageParentEvent : GameMessage
     {
-        public GameMessageParentEvent(WorldObject parent, WorldObject child, ParentLocation? parentLocation = null, Placement? placement = null)
+        public GameMessageParentEvent(WorldObject creature, WorldObject wieldedSelectableItem, ParentLocation? overriddenParentLocation = null, Placement? overriddenPlacement = null)
             : base(GameMessageOpcode.ParentEvent, GameMessageGroup.SmartboxQueue)
         {
-            Writer.WriteGuid(parent.Guid);
-            Writer.WriteGuid(child.Guid);
-            Writer.Write(parentLocation != null ? (int)parentLocation : (int?)child.ParentLocation ?? 0);
-            Writer.Write(placement != null ? (int)placement : (int?)child.Placement ?? 0);
-            Writer.Write(parent.Sequences.GetCurrentSequence(SequenceType.ObjectInstance));
-            Writer.Write(child.Sequences.GetNextSequence(SequenceType.ObjectPosition));
+            Writer.WriteGuid(creature.Guid);
+            Writer.WriteGuid(wieldedSelectableItem.Guid);
+            Writer.Write((int)(overriddenParentLocation ?? wieldedSelectableItem.ParentLocation ?? ParentLocation.None));
+            Writer.Write((int)(overriddenPlacement ?? wieldedSelectableItem.Placement ?? Placement.Default));
+            Writer.Write(creature.Sequences.GetCurrentSequence(SequenceType.ObjectInstance));
+            Writer.Write(wieldedSelectableItem.Sequences.GetNextSequence(SequenceType.ObjectPosition));
         }
     }
 }
