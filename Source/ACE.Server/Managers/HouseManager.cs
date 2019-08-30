35d3eb68ecb206bce6df0b9bc1a6a44ae04db85d
Mag-nus <Mag-nus@users.noreply.github.com>
2019-08-30 18:22:49 -0500

    more tags

--- Source/ACE.Server/Managers/HouseManager.cs
+++ Source/ACE.Server/Managers/HouseManager.cs
@@ -446,7 +446,7 @@ namespace ACE.Server.Managers
                 var nextRentTime = house.GetRentDue(purchaseTime);
                 player.HouseRentTimestamp = (int)nextRentTime;
 
-                log.Debug($"HouseManager.HandleRentPaid({player.Name}): house rent disabled via config");
+                log.Debug($"[HOUSE] HouseManager.HandleRentPaid({player.Name}): house rent disabled via config");
 
                 // re-add item to queue
                 AddRentQueue(player, house);
@@ -546,7 +546,7 @@ namespace ACE.Server.Managers
             {
                 if (rentItem.Paid < rentItem.Num)
                 {
-                    log.Debug($"{playerHouse.PlayerName}.IsRentPaid() - required wcid {rentItem.WeenieID} amount {rentItem.Num:N0}, found {rentItem.Paid:N0}");
+                    log.Debug($"[HOUSE] {playerHouse.PlayerName}.IsRentPaid() - required wcid {rentItem.WeenieID} amount {rentItem.Num:N0}, found {rentItem.Paid:N0}");
                     return false;
                 }
             }
@@ -573,7 +573,7 @@ namespace ACE.Server.Managers
 
             if (player == null)
             {
-                log.Warn($"{playerHouse.PlayerName}.HasRequirements() - couldn't find player");
+                log.Warn($"[HOUSE] {playerHouse.PlayerName}.HasRequirements() - couldn't find player");
                 return false;
             }
 
@@ -588,7 +588,7 @@ namespace ACE.Server.Managers
 
             if (allegiance == null || rank < allegianceMinLevel)
             {
-                log.Debug($"{playerHouse.PlayerName}.HasRequirements() - allegiance rank {rank} < {allegianceMinLevel}");
+                log.Debug($"[HOUSE] {playerHouse.PlayerName}.HasRequirements() - allegiance rank {rank} < {allegianceMinLevel}");
                 return false;
             }
             return true;
