0198eccb1eefb197030a82fda7d0470193839c84
gmriggs <gmriggs@gmail.com>
2019-10-22 06:34:17 -0400

    improved /teledungeon name search (#2347)

--- Source/ACE.Server/Command/Handlers/DeveloperCommands.cs
+++ Source/ACE.Server/Command/Handlers/DeveloperCommands.cs
@@ -2155,7 +2155,7 @@ namespace ACE.Server.Command.Handlers
 
                 var results = query.ToList();
 
-                var dest = results.Where(i => i.Name.Value.Equals(searchName, StringComparison.OrdinalIgnoreCase)).Select(i => i.Dest).FirstOrDefault();
+                var dest = results.Where(i => i.Name.Value.Contains(searchName, StringComparison.OrdinalIgnoreCase)).Select(i => i.Dest).FirstOrDefault();
 
                 if (dest == null)
                 {
