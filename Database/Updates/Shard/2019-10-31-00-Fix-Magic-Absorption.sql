576584a0fdd3f6c3a2a01572ad52d82f1a508065
gmriggs <gmriggs@gmail.com>
2019-11-04 10:48:24 -0500

    fixing dark idols items created before sept. 27

--- Database/Updates/Shard/2019-10-31-00-Fix-Magic-Absorption.sql
+++ Database/Updates/Shard/2019-10-31-00-Fix-Magic-Absorption.sql
@@ -1,1 +1,4 @@
-update biota_properties_float set value=0.25 where `type`=159 and value=1;
+INSERT INTO biota_properties_float (object_Id, `type`, value)
+SELECT did.object_Id, 159, 0.25 FROM biota_properties_d_i_d did
+WHERE did.`type`=50 and did.value=0x60030AD
+ON DUPLICATE KEY UPDATE biota_properties_float.value=0.25;
