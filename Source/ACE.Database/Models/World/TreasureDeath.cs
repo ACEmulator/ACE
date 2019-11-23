cefb5ecd3d68017d142e18837fb217baf453ae5f
Harli <curiouser69@gmail.com>
2019-11-22 22:55:18 -0500

    Changes requested by Ripley and gmriggs

--- Source/ACE.Database/Models/World/TreasureDeath.cs
+++ Source/ACE.Database/Models/World/TreasureDeath.cs
@@ -23,29 +23,5 @@ namespace ACE.Database.Models.World
         public int MundaneItemMaxAmount { get; set; }
         public int MundaneItemTypeSelectionChances { get; set; }
         public DateTime LastModified { get; set; }
-
-        public TreasureDeath() { }
-
-        public TreasureDeath(TreasureDeath profile)
-        {
-            Id = profile.Id;
-            TreasureType = profile.TreasureType;
-            Tier = profile.Tier;
-            LootQualityMod = profile.LootQualityMod;
-            UnknownChances = profile.UnknownChances;
-            ItemChance = profile.ItemChance;
-            ItemMinAmount = profile.ItemMinAmount;
-            ItemMaxAmount = profile.ItemMaxAmount;
-            ItemTreasureTypeSelectionChances = profile.ItemTreasureTypeSelectionChances;
-            MagicItemChance = profile.MagicItemChance;
-            MagicItemMinAmount = profile.MagicItemMinAmount;
-            MagicItemMaxAmount = profile.MagicItemMaxAmount;
-            MagicItemTreasureTypeSelectionChances = profile.MagicItemTreasureTypeSelectionChances;
-            MundaneItemChance = profile.MundaneItemChance;
-            MundaneItemMinAmount = profile.MundaneItemMinAmount;
-            MundaneItemMaxAmount = profile.MundaneItemMaxAmount;
-            MundaneItemTypeSelectionChances = profile.MundaneItemTypeSelectionChances;
-            LastModified = profile.LastModified;
-        }
     }
 }
