b2b8e9267f4e645889a871a571db87bf600350b4
Ty Conner <tyconner@itcproductions.com>
2019-12-04 19:49:50 -0500

    Update WorldObject_Generators.cs

--- Source/ACE.Server/WorldObjects/WorldObject_Generators.cs
+++ Source/ACE.Server/WorldObjects/WorldObject_Generators.cs
@@ -701,7 +701,7 @@ namespace ACE.Server.WorldObjects
                     removeQueueTotal += generator.RemoveQueue.Count;
                 }
 
-                if (Generator.GeneratorId.HasValue && Generator.GeneratorId > 0) // Generator is controlled by another generator.
+                if (Generator.GeneratorId > 0) // Generator is controlled by another generator.
                 {
                     if (!(Generator is Creature) && Generator.InitCreate > 0 && (Generator.CurrentCreate - removeQueueTotal) == 0) // Parent generator is non-creature generator
                         Generator.Destroy(); // Generator's complete spawn count has been wiped out
