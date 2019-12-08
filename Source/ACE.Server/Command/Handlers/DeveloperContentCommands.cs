63d24bfd32122a1449d747596c81cf577c48b64e
gmriggs <gmriggs@gmail.com>
2019-12-07 23:09:05 -0500

    /addenc command for content creators to create encounters

--- Source/ACE.Server/Command/Handlers/DeveloperContentCommands.cs
+++ Source/ACE.Server/Command/Handlers/DeveloperContentCommands.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Text.RegularExpressions;
 
 using Microsoft.EntityFrameworkCore;
@@ -17,6 +18,7 @@ using ACE.Server.Factories;
 using ACE.Server.Managers;
 using ACE.Server.Network;
 using ACE.Server.Network.GameMessages.Messages;
+using ACE.Server.WorldObjects;
 
 namespace ACE.Server.Command.Handlers.Processors
 {
@@ -363,6 +365,113 @@ namespace ACE.Server.Command.Handlers.Processors
                     return highestLandblockInst.Guid + 1;
             }
         }
+
+        [CommandHandler("addenc", AccessLevel.Developer, CommandHandlerFlag.RequiresWorld, 1, "Spawns a new wcid or classname in the current outdoor cell as an encounter", "<wcid or classname>")]
+        public static void HandleAddEncounter(Session session, params string[] parameters)
+        {
+            var param = parameters[0];
+
+            Weenie weenie = null;
+
+            if (uint.TryParse(param, out var wcid))
+                weenie = DatabaseManager.World.GetCachedWeenie(wcid);   // wcid
+            else
+                weenie = DatabaseManager.World.GetCachedWeenie(param);  // classname
+
+            if (weenie == null)
+            {
+                session.Network.EnqueueSend(new GameMessageSystemChat($"Couldn't find weenie {param}", ChatMessageType.Broadcast));
+                return;
+            }
+
+            var pos = session.Player.Location;
+
+            if ((pos.Cell & 0xFFFF) >= 0x100)
+            {
+                session.Network.EnqueueSend(new GameMessageSystemChat("You must be outdoors to create an encounter!", ChatMessageType.Broadcast));
+                return;
+            }
+
+            var cellX = (int)pos.PositionX / 24;
+            var cellY = (int)pos.PositionY / 24;
+
+            // spawn encounter
+            var wo = SpawnEncounter(weenie, cellX, cellY, pos, session);
+
+            if (wo == null) return;
+
+            session.Network.EnqueueSend(new GameMessageSystemChat($"Creating new encounter @ landblock {pos.Landblock:X4}, cellX={cellX}, cellY={cellY}\n{wo.WeenieClassId} - {wo.Name}", ChatMessageType.Broadcast));
+
+            var timestamp = DateTime.Now.ToString("MM-dd-yyyy HH:mm:ss");
+
+            var sql = $"INSERT INTO encounter set landblock=0x{pos.Landblock:X4}, weenie_Class_Id={weenie.ClassId}, cell_X={cellX}, cell_Y={cellY}, last_Modified='{timestamp}';";
+
+            Console.WriteLine(sql);
+
+            // serialize to .sql file
+            var contentFolder = VerifyContentFolder(session, false);
+
+            var sep = Path.DirectorySeparatorChar;
+            var folder = new DirectoryInfo($"{contentFolder.FullName}{sep}sql{sep}6 LandblockExtendedData{sep}");
+
+            if (!folder.Exists)
+                folder.Create();
+
+            var sql_filename = $"{pos.Landblock:X4}.sql";
+
+            using (var file = File.Open($"{folder.FullName}{sep}{sql_filename}", FileMode.OpenOrCreate))
+            {
+                file.Seek(0, SeekOrigin.End);
+
+                using (var stream = new StreamWriter(file))
+                    stream.WriteLine(sql);
+            }
+        }
+
+        public static WorldObject SpawnEncounter(Weenie weenie, int cellX, int cellY, Position pos, Session session)
+        {
+            var wo = WorldObjectFactory.CreateNewWorldObject(weenie.ClassId);
+
+            if (wo == null)
+            {
+                Console.WriteLine($"Failed to create encounter weenie");
+                return null;
+            }
+
+            var xPos = Math.Clamp(cellX * 24.0f, 0.5f, 191.5f);
+            var yPos = Math.Clamp(cellY * 24.0f, 0.5f, 191.5f);
+
+            var newPos = new Physics.Common.Position();
+            newPos.ObjCellID = pos.Cell;
+            newPos.Frame = new Physics.Animation.AFrame(new Vector3(xPos, yPos, 0), Quaternion.Identity);
+            newPos.adjust_to_outside();
+
+            newPos.Frame.Origin.Z = session.Player.CurrentLandblock.PhysicsLandblock.GetZ(newPos.Frame.Origin);
+
+            wo.Location = new Position(newPos.ObjCellID, newPos.Frame.Origin, newPos.Frame.Orientation);
+
+            var sortCell = Physics.Common.LScape.get_landcell(newPos.ObjCellID) as Physics.Common.SortCell;
+            if (sortCell != null && sortCell.has_building())
+            {
+                Console.WriteLine($"Failed to create encounter near building cell");
+                return null;
+            }
+
+            if (PropertyManager.GetBool("override_encounter_spawn_rates").Item)
+            {
+                wo.RegenerationInterval = PropertyManager.GetDouble("encounter_regen_interval").Item;
+
+                wo.ReinitializeHeartbeats();
+
+                foreach (var profile in wo.Biota.BiotaPropertiesGenerator)
+                    profile.Delay = (float)PropertyManager.GetDouble("encounter_delay").Item;
+            }
+
+            session.Player.CurrentLandblock.AddWorldObject(wo);
+
+            return wo;
+        }
+
 
         [CommandHandler("export-json", AccessLevel.Developer, CommandHandlerFlag.None, 1, "Exports a weenie from database to JSON file", "<wcid>")]
         public static void HandleExportJson(Session session, params string[] parameters)
