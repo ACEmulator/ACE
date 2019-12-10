f6fc41f82f303a53b93fd267c017eb9f2d51a740
Ty Conner <tyconner@itcproductions.com>
2019-12-10 00:51:11 -0500

    Update WorldObject_Generators.cs

--- Source/ACE.Server/WorldObjects/WorldObject_Generators.cs
+++ Source/ACE.Server/WorldObjects/WorldObject_Generators.cs
@@ -703,7 +703,7 @@ namespace ACE.Server.WorldObjects
 
                 if (Generator.GeneratorId > 0) // Generator is controlled by another generator.
                 {
-                    if (!(Generator is Container) && Generator.InitCreate > 0 && (Generator.CurrentCreate - removeQueueTotal) == 0) // Parent generator is non-creature generator
+                    if (!(Generator is Container) && Generator.InitCreate > 0 && (Generator.CurrentCreate - removeQueueTotal) == 0) // Parent generator is non-container (Container, Corpse, Chest, Slumlord, Storage, Hook, Creature) generator
                         Generator.Destroy(); // Generator's complete spawn count has been wiped out
                 }
             }
