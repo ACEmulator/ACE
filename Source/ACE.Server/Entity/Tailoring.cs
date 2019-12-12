aca3f3891140ba126321dc04a084b04734e74b38
gmriggs <gmriggs@gmail.com>
2019-12-12 12:05:21 -0500

    adding server option for intermediate

--- Source/ACE.Server/Entity/Tailoring.cs
+++ Source/ACE.Server/Entity/Tailoring.cs
@@ -4,6 +4,7 @@ using ACE.Entity.Enum;
 using ACE.Entity.Enum.Properties;
 using ACE.Server.Entity.Actions;
 using ACE.Server.Factories;
+using ACE.Server.Managers;
 using ACE.Server.Network.GameMessages.Messages;
 using ACE.Server.WorldObjects;
 
@@ -191,7 +192,8 @@ namespace ACE.Server.Entity
             // here a lot is done directly in code..
 
             target.PaletteTemplate = source.PaletteTemplate;
-            target.UiEffects = source.UiEffects;
+            if (PropertyManager.GetBool("tailoring_intermediate_uieffects").Item)
+                target.UiEffects = source.UiEffects;
             target.MaterialType = source.MaterialType;
             target.TargetType = source.ItemType;
 
