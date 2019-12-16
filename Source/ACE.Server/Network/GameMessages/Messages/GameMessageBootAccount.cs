e57df568b58a013758e597fa58444ec41a405588
Ty Conner <tyconner@itcproductions.com>
2019-12-16 02:40:40 -0500

    Alert player to incomplete DATs
    Cheap alert, triggered only by clients attempt to download

--- Source/ACE.Server/Network/GameMessages/Messages/GameMessageBootAccount.cs
+++ Source/ACE.Server/Network/GameMessages/Messages/GameMessageBootAccount.cs
@@ -5,10 +5,7 @@ namespace ACE.Server.Network.GameMessages.Messages
         public GameMessageBootAccount(Session session, string reason = null)
             : base(GameMessageOpcode.AccountBoot, GameMessageGroup.UIQueue)
         {
-            if (reason != null)
-            {
-                Writer.WriteString16L($" - {reason}");
-            }
+            Writer.WriteString16L($"{(reason != null ? "\n" : "")}{reason}");
         }
     }
 }
--- Source/ACE.Server/Network/Handlers/DDDHandler.cs
+++ Source/ACE.Server/Network/Handlers/DDDHandler.cs
@@ -1,4 +1,7 @@
-﻿using ACE.Server.Network.Enum;
+using ACE.Entity.Enum;
+using ACE.Server.Entity.Actions;
+using ACE.Server.Network.Enum;
+using ACE.Server.Network.GameEvent.Events;
 using ACE.Server.Network.GameMessages;
 using ACE.Server.Network.GameMessages.Messages;
 
@@ -17,6 +20,30 @@ namespace ACE.Server.Network.Handlers
         public static void DDD_EndDDD(ClientMessage message, Session session)
         {
             // We don't need to reply to this message.
+        }
+
+        [GameMessage(GameMessageOpcode.DDD_RequestDataMessage, SessionState.WorldConnected)]
+        public static void DDD_RequestDataMessage(ClientMessage message, Session session)
+        {
+            var msg = "Your DAT files are incomplete.\nACEmulator does not support dynamic DAT updating at this time.\nPlease visit https://emulator.ac/how-to-play to download the complete DAT files.";
+            var popupMsg = new GameEventPopupString(session, msg);
+            var chatMsg = new GameMessageSystemChat(msg, ChatMessageType.WorldBroadcast);
+            var transientMsg = new GameEventCommunicationTransientString(session, msg);
+
+            var resourceType = message.Payload.ReadUInt32();
+            var dataId = message.Payload.ReadUInt32();
+            var errorType = 1u; // unknown enum... this seems to trigger reattempt request by client.
+
+            var dddErrorMsg = new GameMessageDDDErrorMessage(resourceType, dataId, errorType);
+
+            if (session.Player.FirstEnterWorldDone) // Boot client with msg
+            {
+                session.Network.EnqueueSend(new GameMessageBootAccount(session, msg), dddErrorMsg);
+                session.LogOffPlayer(true);
+            }
+            else // cannot cleanly boot player (client crashes) that hasn't completed first login, spam msgs until server auto boots them or they disconnect.
+                session.Network.EnqueueSend(popupMsg, chatMsg, transientMsg, dddErrorMsg);
+
         }
     }
 }
