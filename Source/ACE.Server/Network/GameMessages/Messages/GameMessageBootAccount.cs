8b5c2d37666b01ba8e49d5d3ba711c5d6fce3e28
Ty Conner <tyconner@itcproductions.com>
2019-12-31 02:21:42 -0500

    Update GameMessageBootAccount.cs

--- Source/ACE.Server/Network/GameMessages/Messages/GameMessageBootAccount.cs
+++ Source/ACE.Server/Network/GameMessages/Messages/GameMessageBootAccount.cs
@@ -2,6 +2,11 @@ namespace ACE.Server.Network.GameMessages.Messages
 {
     public class GameMessageBootAccount : GameMessage
     {
+        /// <summary>
+        /// Tells the client the player has been booted from the server.
+        /// </summary>
+        /// <param name="session"></param>
+        /// <param name="reason">message to tell player why they were booted, typically you will want to start with a space because client does not automatically add one<para>If null client will see " for Code of Conduct Violations"</para></param>
         public GameMessageBootAccount(Session session, string reason = null)
             : base(GameMessageOpcode.AccountBoot, GameMessageGroup.UIQueue)
         {
