ba93521098ec8eaf27e180f757b4b8ec67484bff
Ty Conner <tyconner@itcproductions.com>
2020-02-06 13:47:31 -0500

    Add support for minimums in AwardLevelProportional*XP

--- Source/ACE.Server/WorldObjects/Managers/EmoteManager.cs
+++ Source/ACE.Server/WorldObjects/Managers/EmoteManager.cs
@@ -116,16 +116,21 @@ namespace ACE.Server.WorldObjects.Managers
 
                 case EmoteType.AwardLevelProportionalSkillXP:
 
+                    var max = emote.Max64 ?? emote.Max ?? 0;
+                    var min = emote.Min64 ?? emote.Min ?? 0;
+
                     if (player != null)
-                        player.GrantLevelProportionalSkillXP((Skill)emote.Stat, emote.Percent ?? 0, emote.Max64 ?? 0);
+                        player.GrantLevelProportionalSkillXP((Skill)emote.Stat, emote.Percent ?? 0, max, min);
                     break;
 
                 case EmoteType.AwardLevelProportionalXP:
 
                     bool shareXP = emote.Display ?? false;
+                    max = emote.Max64 ?? emote.Max ?? 0;
+                    min = emote.Min64 ?? emote.Min ?? 0;
 
                     if (player != null)
-                        player.GrantLevelProportionalXp(emote.Percent ?? 0, emote.Max64 ?? 0, shareXP);
+                        player.GrantLevelProportionalXp(emote.Percent ?? 0, max, min, shareXP);
                     break;
 
                 case EmoteType.AwardLuminance:
@@ -138,7 +143,7 @@ namespace ACE.Server.WorldObjects.Managers
                 case EmoteType.AwardNoShareXP:
 
                     if (player != null)
-                        player.EarnXP(emote.Amount64 ?? 0, XpType.Quest, ShareType.None);
+                        player.EarnXP(emote.Amount64 ?? emote.Amount ?? 0, XpType.Quest, ShareType.None);
 
                     break;
 
@@ -164,7 +169,7 @@ namespace ACE.Server.WorldObjects.Managers
 
                     if (player != null)
                     {
-                        var amt = emote.Amount64 ?? 0;
+                        var amt = emote.Amount64 ?? emote.Amount ?? 0;
                         if (amt > 0)
                         {
                             player.EarnXP(amt, XpType.Quest, ShareType.All);
--- Source/ACE.Server/WorldObjects/Player_Skills.cs
+++ Source/ACE.Server/WorldObjects/Player_Skills.cs
@@ -339,7 +339,7 @@ namespace ACE.Server.WorldObjects
         /// <summary>
         /// Grants skill XP proportional to the player's skill level
         /// </summary>
-        public void GrantLevelProportionalSkillXP(Skill skill, double percent, long max)
+        public void GrantLevelProportionalSkillXP(Skill skill, double percent, long max, long min)
         {
             var creatureSkill = GetCreatureSkill(skill, false);
             if (creatureSkill == null || creatureSkill.IsMaxRank)
@@ -355,6 +355,9 @@ namespace ACE.Server.WorldObjects
                 amount = Math.Min(amount, (uint)max);
 
             amount = Math.Min(amount, creatureSkill.ExperienceLeft);
+
+            if (min > 0 && min <= uint.MinValue)
+                amount = Math.Max((uint)min, amount);
 
             //Console.WriteLine($"{Name}.GrantLevelProportionalSkillXP({skill}, {percent}, {max:N0})");
             //Console.WriteLine($"Amount: {amount:N0}");
