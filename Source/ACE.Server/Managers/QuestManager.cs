d9322405653fc97c0b9d758798504e82e715b342
Ty Conner <tyconner@itcproductions.com>
2020-02-10 20:07:22 -0500

    depreciated clean up

--- Source/ACE.Server/Managers/QuestManager.cs
+++ Source/ACE.Server/Managers/QuestManager.cs
@@ -249,12 +249,6 @@ namespace ACE.Server.Managers
         {
             var questName = GetQuestName(questFormat);
 
-            // Always return false for Kill Task count quests,
-            // as their emote logic is backwards from standard quests, and
-            // they should always be solvable, once flagged with main kill task quest
-            if (questName.EndsWith("count", StringComparison.Ordinal) || questName.EndsWith("counter", StringComparison.Ordinal))
-                return false;
-
             // verify max solves / quest timer
             var nextSolveTime = GetNextSolveTime(questName);
 
--- Source/ACE.Server/WorldObjects/Managers/EmoteManager.cs
+++ Source/ACE.Server/WorldObjects/Managers/EmoteManager.cs
@@ -1109,6 +1109,7 @@ namespace ACE.Server.WorldObjects.Managers
                     break;
 
                 case EmoteType.SpendLuminance:
+
                     if (player != null)
                         player.SpendLuminance(emote.HeroXP64 ?? 0);
                     break;
@@ -1121,13 +1122,7 @@ namespace ACE.Server.WorldObjects.Managers
                         {
                             var questName = emote.Message;
 
-                            // are there fellowship only kill tasks?
-                            //if (questName.EndsWith("@#kt", StringComparison.Ordinal))
-                            //{
-                            //    player.Fellowship.QuestManager.HandleKillTask(questName, WorldObject);
-                            //}
-                            //else
-                                player.Fellowship.QuestManager.Stamp(emote.Message);
+                            player.Fellowship.QuestManager.Stamp(emote.Message);
                         }
                     }
                     break;
@@ -1142,11 +1137,9 @@ namespace ACE.Server.WorldObjects.Managers
                         var questName = emote.Message;
 
                         if (questName.EndsWith("@#kt", StringComparison.Ordinal))
-                        {
-                            questTarget.QuestManager.HandleKillTask(questName, WorldObject);
-                        }
-                        else
-                            questTarget.QuestManager.Stamp(emote.Message);
+                            log.Warn($"0x{WorldObject.Guid}:{WorldObject.Name} ({WorldObject.WeenieClassId}).EmoteManager.ExecuteEmote: EmoteType.StampQuest({questName}) is depreciated.");
+
+                        questTarget.QuestManager.Stamp(emote.Message);
                     }
                     break;
 
