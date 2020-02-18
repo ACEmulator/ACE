1b5938241a4a49511ff1df9ab8b1b563299e6b16
gmriggs <gmriggs@gmail.com>
2020-02-18 15:59:43 -0500

    fixing untargeted spell projectiles hitting scrum drudge (#2718)
    * fixing untargeted spell projectiles hitting scrum drudge
    
    * fixing more gaps

--- Source/ACE.Server/WorldObjects/WorldObject_Magic.cs
+++ Source/ACE.Server/WorldObjects/WorldObject_Magic.cs
@@ -204,6 +204,9 @@ namespace ACE.Server.WorldObjects
             if (caster == target && spell.IsNegativeRedirectable)
                 return true;
 
+            if (targetCreature != null && spell.NonComponentTargetType == ItemType.Creature && !caster.CanDamage(targetCreature))
+                return true;
+
             // Cannot cast Weapon Aura spells on targets that are not players or creatures
             if ((spell.MetaSpellType == SpellType.Enchantment) && (spell.School == MagicSchool.ItemEnchantment))
             {
