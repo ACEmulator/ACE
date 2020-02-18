e680059fab69be2bbeb3d6a7574e2293127233fb
gmriggs <gmriggs@gmail.com>
2020-02-18 02:23:43 -0500

    fix TryConsumeFromInventoryWithNetworking when player has 0 items

--- Source/ACE.Server/WorldObjects/Player_Inventory.cs
+++ Source/ACE.Server/WorldObjects/Player_Inventory.cs
@@ -159,7 +159,7 @@ namespace ACE.Server.WorldObjects
                 if (leftReq <= 0)
                     break;
             }
-            return true;
+            return leftReq <= 0;
         }
 
         public enum RemoveFromInventoryAction
