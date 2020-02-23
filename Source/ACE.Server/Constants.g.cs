0c5b73b6446eac824ab0b036b35cc1b69f9670d8
Ty Conner <tyconner@itcproductions.com>
2020-02-23 16:18:59 -0500

    test 2

---
+++ Source/ACE.Server/Constants.g.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ACE.Server
+{
+    public static partial class Constants
+    {
+        public static DateTime CompilationTimestampUtc { get { return new DateTime(637180887292239824L, DateTimeKind.Utc); } }
+    }
+}
