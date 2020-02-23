2317e0cbf758d93b88d89e9b5f477f0229055e47
gmriggs <gmriggs@gmail.com>
2020-02-23 15:44:22 -0500

    adding support for cloak proc spells (#2734)
    * adding support for cloak proc spells
    
    * tweaking CloakMod

--- Source/ACE.Server/WorldObjects/WorldObject_Magic.cs
+++ Source/ACE.Server/WorldObjects/WorldObject_Magic.cs
@@ -86,7 +86,7 @@ namespace ACE.Server.WorldObjects
             if (player != null && status.Message != null && !status.Broadcast && showMsg)
                 player.Session.Network.EnqueueSend(status.Message);
             else if (player != null && status.Message != null && status.Broadcast && showMsg)
-                player.EnqueueBroadcast(status.Message, LocalBroadcastRange);
+                player.EnqueueBroadcast(status.Message, LocalBroadcastRange, ChatMessageType.Magic);
 
             // for invisible spell traps,
             // their effects won't be seen if they broadcast from themselves
@@ -439,6 +439,19 @@ namespace ACE.Server.WorldObjects
                     if (player != null && srcVital != null && srcVital.Equals("health"))
                         player.Session.Network.EnqueueSend(new GameEventUpdateHealth(player.Session, target.Guid.Full, (float)spellTarget.Health.Current / spellTarget.Health.MaxValue));
 
+                    // handle cloaks
+                    if (spellTarget != this && spellTarget.IsAlive && srcVital != null && srcVital.Equals("health") && boost < 0 && spellTarget.HasCloakEquipped)
+                    {
+                        // ensure message is sent after enchantment.Message
+                        var actionChain = new ActionChain();
+                        actionChain.AddDelayForOneTick();
+                        actionChain.AddAction(this, () =>
+                        {
+                            var pct = (float)-boost / spellTarget.Health.MaxValue;
+                            Cloak.TryProcSpell(spellTarget, this, pct);
+                        });
+                        actionChain.EnqueueChain();
+                    }
                     break;
 
                 case SpellType.Transfer:
@@ -572,6 +585,19 @@ namespace ACE.Server.WorldObjects
                     if (player != null && srcVital != null && srcVital.Equals("health"))
                         player.Session.Network.EnqueueSend(new GameEventUpdateHealth(player.Session, target.Guid.Full, (float)spellTarget.Health.Current / spellTarget.Health.MaxValue));
 
+                    // handle cloaks
+                    if (spellTarget != this && spellTarget.IsAlive && srcVital != null && srcVital.Equals("health") && spellTarget.HasCloakEquipped)
+                    {
+                        // ensure message is sent after enchantment.Message
+                        var actionChain = new ActionChain();
+                        actionChain.AddDelayForOneTick();
+                        actionChain.AddAction(this, () =>
+                        {
+                            var pct = (float)srcVitalChange / spellTarget.Health.MaxValue;
+                            Cloak.TryProcSpell(spellTarget, this, pct);
+                        });
+                        actionChain.EnqueueChain();
+                    }
                     break;
 
                 case SpellType.LifeProjectile:
@@ -1510,15 +1536,22 @@ namespace ACE.Server.WorldObjects
             // create enchantment
             AddEnchantmentResult addResult;
             var aetheriaProc = false;
+            var cloakProc = false;
 
-            if (caster is Gem && Aetheria.IsAetheria(caster.WeenieClassId) && caster.ProcSpell == spell.Id)
+            if (caster.ProcSpell == spell.Id)
             {
-                caster = this;
-                addResult = target.EnchantmentManager.Add(spell, caster, equip);
-                aetheriaProc = true;
+                if (caster is Gem && Aetheria.IsAetheria(caster.WeenieClassId))
+                {
+                    caster = this;
+                    aetheriaProc = true;
+                }
+                else if (Cloak.IsCloak(caster))
+                {
+                    caster = this;
+                    cloakProc = true;
+                }
             }
-            else
-                addResult = target.EnchantmentManager.Add(spell, caster, equip);
+            addResult = target.EnchantmentManager.Add(spell, caster, equip);
 
             // build message
             var suffix = "";
@@ -1572,7 +1605,7 @@ namespace ACE.Server.WorldObjects
             if (playerTarget == null && target.Wielder is Player wielder)
                 playerTarget = wielder;
 
-            if (playerTarget != null && playerTarget != this && !playerTarget.SquelchManager.Squelches.Contains(this, ChatMessageType.Magic))
+            if (playerTarget != null && playerTarget != this && !playerTarget.SquelchManager.Squelches.Contains(this, ChatMessageType.Magic) && !cloakProc)
             {
                 var targetName = target == playerTarget ? "you" : target.Name;
 
