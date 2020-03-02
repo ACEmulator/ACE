65cb8663abd6eb6231b2e963bc4882564fc42140
Ty Conner <tyconner@itcproductions.com>
2020-03-02 13:01:31 -0500

    adjustments to appveyor

--- Source/AppVeyor/AppVeyorAfterDeploy.bat
+++ Source/AppVeyor/AppVeyorAfterDeploy.bat
@@ -5,6 +5,5 @@ del *.txt
 @echo off
 docker login -u="%DOCKER_USER%" -p="%DOCKER_PASS%"
 @echo on
-docker build -t acemulator/ace:latest -t acemulator/ace:%APPVEYOR_BUILD_VERSION% .
 
 docker push acemulator/ace
