3e947f5ba481ce16686494f8f4932d3263b910ff
Ty Conner <tyconner@itcproductions.com>
2020-03-07 22:00:55 -0500

    Update DeveloperContentCommands.cs

--- Source/ACE.Server/Command/Handlers/DeveloperContentCommands.cs
+++ Source/ACE.Server/Command/Handlers/DeveloperContentCommands.cs
@@ -1247,6 +1247,11 @@ namespace ACE.Server.Command.Handlers.Processors
             {
                 LandblockInstanceWriter = new LandblockInstanceWriter();
                 LandblockInstanceWriter.WeenieNames = DatabaseManager.World.GetAllWeenieNames();
+                foreach (var value in instances)
+                {
+                    if (LandblockInstanceWriter.WeenieNames.TryGetValue(value.WeenieClassId, out var name))
+                        LandblockInstanceWriter.InstanceNames[value.Guid] = name;
+                }
             }
 
             LandblockInstanceWriter.CreateSQLDELETEStatement(instances, fileWriter);
