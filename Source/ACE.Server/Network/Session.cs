12bc935860a8f3edd21097c385b173858b96cc0c
Ty Conner <tyconner@itcproductions.com>
2020-03-09 00:51:04 -0400

    Update Session.cs

--- Source/ACE.Server/Network/Session.cs
+++ Source/ACE.Server/Network/Session.cs
@@ -201,16 +201,19 @@ namespace ACE.Server.Network
 
             Player = null;
 
-            Network.EnqueueSend(new GameMessageCharacterLogOff());
+            if (!ServerManager.ShutdownInProgress)
+            {
+                Network.EnqueueSend(new GameMessageCharacterLogOff());
 
-            CheckCharactersForDeletion();
+                CheckCharactersForDeletion();
 
-            Network.EnqueueSend(new GameMessageCharacterList(Characters, this));
+                Network.EnqueueSend(new GameMessageCharacterList(Characters, this));
 
-            GameMessageServerName serverNameMessage = new GameMessageServerName(ConfigManager.Config.Server.WorldName, PlayerManager.GetOnlineCount(), (int)ConfigManager.Config.Server.Network.MaximumAllowedSessions);
-            Network.EnqueueSend(serverNameMessage);
+                GameMessageServerName serverNameMessage = new GameMessageServerName(ConfigManager.Config.Server.WorldName, PlayerManager.GetOnlineCount(), (int)ConfigManager.Config.Server.Network.MaximumAllowedSessions);
+                Network.EnqueueSend(serverNameMessage);
 
-            State = SessionState.AuthConnected;
+                State = SessionState.AuthConnected;
+            }        
         }
 
         public void Terminate(SessionTerminationReason reason, GameMessage message = null, ServerPacket packet = null, string extraReason = "")
