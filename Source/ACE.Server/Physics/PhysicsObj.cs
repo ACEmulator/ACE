4ac8b6a4e6367268db56cd9f5ee906a875ab9781
gmriggs <gmriggs@gmail.com>
2020-03-10 22:47:35 -0400

    fixing players sometimes invisible after teleport

--- Source/ACE.Server/Physics/PhysicsObj.cs
+++ Source/ACE.Server/Physics/PhysicsObj.cs
@@ -2261,7 +2261,19 @@ namespace ACE.Server.Physics
                 foreach (var obj in newlyVisible)
                 {
                     var wo = obj.WeenieObj.WorldObject;
-                    if (wo != null)
+
+                    if (wo == null)
+                        continue;
+
+                    if (wo.Teleporting)
+                    {
+                        // ensure post-teleport position is sent
+                        var actionChain = new ActionChain();
+                        actionChain.AddDelayForOneTick();
+                        actionChain.AddAction(player, () => player.TrackObject(wo));
+                        actionChain.EnqueueChain();
+                    }
+                    else
                         player.TrackObject(wo);
                 }
             }
@@ -2285,7 +2297,16 @@ namespace ACE.Server.Physics
             }
             else
             {
-                player.TrackObject(wo);
+                if (wo.Teleporting)
+                {
+                    // ensure post-teleport position is sent
+                    var actionChain = new ActionChain();
+                    actionChain.AddDelayForOneTick();
+                    actionChain.AddAction(player, () => player.TrackObject(wo));
+                    actionChain.EnqueueChain();
+                }
+                else
+                    player.TrackObject(wo);
             }
         }
 
