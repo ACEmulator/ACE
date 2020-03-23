8e8b0c82fde32f0278ade465d3a8cc10635e6939
gmriggs <gmriggs@gmail.com>
2020-03-23 16:33:52 -0400

    remove comment

--- Source/ACE.Server/WorldObjects/WorldObject_Properties.cs
+++ Source/ACE.Server/WorldObjects/WorldObject_Properties.cs
@@ -636,7 +636,6 @@ namespace ACE.Server.WorldObjects
             set { if (!value.HasValue) RemoveProperty(PropertyFloat.Translucency); else SetProperty(PropertyFloat.Translucency, value.Value); }
         }
 
-        // TODO: sync with physics -- these shouldn't be separate fields
         public Vector3 Velocity => PhysicsObj?.Velocity ?? Vector3.Zero;
 
         public Vector3 Acceleration => PhysicsObj?.Acceleration ?? Vector3.Zero;
