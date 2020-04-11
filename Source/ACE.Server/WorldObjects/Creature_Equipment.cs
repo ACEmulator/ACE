3a7883bf64bfafe8007572383962538efb889797
gmriggs <gmriggs@gmail.com>
2020-04-11 13:36:30 -0400

    fixing antius blackmoor statue

--- Source/ACE.Server/WorldObjects/Creature_Equipment.cs
+++ Source/ACE.Server/WorldObjects/Creature_Equipment.cs
@@ -530,17 +530,12 @@ namespace ACE.Server.WorldObjects
             {
                 var wo = WorldObjectFactory.CreateNewWorldObject(item);
 
-                if (wo != null)
+                if (wo == null) continue;
+
+                if (wo.ValidLocations == null || (ItemCapacity ?? 0) > 0)
                     TryAddToInventory(wo);
-
-                // handled in EquipInventoryItems()
-                /*var equipped = false;
-
-                if (wo.ValidLocations != null)
-                    equipped = TryWieldObject(wo, (EquipMask)wo.ValidLocations);
-
-                if (!equipped)*/
-                    
+                else
+                    TryWieldObject(wo, (EquipMask)wo.ValidLocations);
             }
         }
 
@@ -664,16 +659,10 @@ namespace ACE.Server.WorldObjects
 
             foreach (var item in wieldedTreasure)
             {
-                TryAddToInventory(item);
-
-                // handled in EquipInventoryItems()
-
-                /*var equipped = false;
-
-                if (item.ValidLocations != null)
-                    equipped = TryWieldObject(item, (EquipMask)item.ValidLocations);
-
-                if (!equipped)*/
+                if (item.ValidLocations == null || (ItemCapacity ?? 0) > 0)
+                    TryAddToInventory(item);
+                else
+                    TryWieldObject(item, (EquipMask)item.ValidLocations);
             }
         }
     }
