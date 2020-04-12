ca10b1b020bd154b4fe9f63e0660a27e8ded85f9
Ty Conner <tyconner@itcproductions.com>
2020-04-12 05:25:50 -0400

    Update 2020-04-11-00-Update-Character-SpellBars.sql

--- Database/Updates/Shard/2020-04-11-00-Update-Character-SpellBars.sql
+++ Database/Updates/Shard/2020-04-11-00-Update-Character-SpellBars.sql
@@ -1,17 +1,19 @@
 USE ace_shard;
 
+ALTER TABLE `character_properties_spell_bar`
+DROP FOREIGN KEY `wcid_spellbar`;
+ALTER TABLE `character_properties_spell_bar`
+DROP INDEX `wcid_spellbar_barId_spellId_uidx`;
+
 UPDATE character_properties_spell_bar
 SET spell_Bar_Number = spell_Bar_Number + 1,
     spell_Bar_Index = spell_Bar_Index + 1
 WHERE id > 0;
 
 ALTER TABLE `character_properties_spell_bar`
-DROP FOREIGN KEY `wcid_spellbar`;
-ALTER TABLE `character_properties_spell_bar`
 DROP COLUMN `id`,
 DROP PRIMARY KEY,
-ADD PRIMARY KEY (`character_Id`, `spell_Bar_Number`, `spell_Id`),
-DROP INDEX `wcid_spellbar_barId_spellId_uidx` ;
+ADD PRIMARY KEY (`character_Id`, `spell_Bar_Number`, `spell_Id`);
 
 ALTER TABLE `character_properties_spell_bar`
 ADD CONSTRAINT `wcid_spellbar`
