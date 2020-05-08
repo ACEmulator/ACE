eb5703f862210c5db35fc03de6195cabec02e790
Ty Conner <tyconner@itcproductions.com>
2020-05-08 01:44:21 -0400

    Wire up automatic database updates (#2965)
    * Wire up automatic database updates
    
    - Support automatic database patching
    - Support automatic updating of world database to latest release
    - Support automatic re-application of world database customization
    
    * Update Program_DbUpdates.cs
    
    * Update Program_DbUpdates.cs
    
    * Update Program_Setup.cs
    
    * Update Program_DbUpdates.cs
    
    * Update Program_DbUpdates.cs
    
    * Update Program_DbUpdates.cs

--- Source/ACE.Server/Program.cs
+++ Source/ACE.Server/Program.cs
@@ -163,6 +163,21 @@ namespace ACE.Server
                 log.Info($"Purged {numberOfBiotasPurged:N0} biotas.");
             }
 
+            if (ConfigManager.Config.Offline.AutoUpdateWorldDatabase)
+            {
+                CheckForWorldDatabaseUpdate();
+
+                if (ConfigManager.Config.Offline.AutoApplyWorldCustomizations)
+                    AutoApplyWorldCustomizations();
+            }
+            else
+                log.Info($"AutoUpdateWorldDatabase is disabled...");
+
+            if (ConfigManager.Config.Offline.AutoApplyDatabaseUpdates)
+                AutoApplyDatabaseUpdates();
+            else
+                log.Info($"AutoApplyDatabaseUpdates is disabled...");
+
             // This is temporary and can be removed in the near future, 2020-04-05 Mag-nus
             ShardDatabaseOfflineTools.FixAnimPartAndTextureMapFromPR2731(out var numberOfRecordsFixed);
             log.Info($"Fixed {numberOfRecordsFixed:N0} AnimPart and TextureMap records.");
