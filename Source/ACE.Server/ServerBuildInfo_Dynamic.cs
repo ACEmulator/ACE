e59949ee478989c0c06df81e6b55cc35be39d0e9
Ty Conner <tyconner@itcproductions.com>
2020-05-08 16:25:23 +0000

    [ci skip] Updating ServerBuildInfo_Dynamic with latest build and version information

--- Source/ACE.Server/ServerBuildInfo_Dynamic.cs
+++ Source/ACE.Server/ServerBuildInfo_Dynamic.cs
@@ -4,17 +4,17 @@ namespace ACE.Server
     public static partial class ServerBuildInfo 
     { 
         public static string Branch = "master"; 
-        public static string Commit = "a0285c77e63afe239a2e523382845f892bf5b398"; 
+        public static string Commit = "8aa419d9f333a4bfc6c6bc8966d60cadfad34de6"; 
  
         public static string Version = "1.3"; 
-        public static string Build   = "3373"; 
+        public static string Build   = "3374"; 
  
         public static int BuildYear   = 2020; 
         public static int BuildMonth  = 05; 
         public static int BuildDay    = 08; 
-        public static int BuildHour   = 05; 
-        public static int BuildMinute = 59; 
-        public static int BuildSecond = 36; 
+        public static int BuildHour   = 16; 
+        public static int BuildMinute = 23; 
+        public static int BuildSecond = 18; 
     } 
 } 
  
