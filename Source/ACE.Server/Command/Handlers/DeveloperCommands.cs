6fcc72804986bd342bcca62b080d2b698f7b0c60
harliq <30803300+harliq@users.noreply.github.com>
2020-06-15 22:12:21 -0400

    Added Guid to /getinfo

--- Source/ACE.Server/Command/Handlers/DeveloperCommands.cs
+++ Source/ACE.Server/Command/Handlers/DeveloperCommands.cs
@@ -2901,7 +2901,7 @@ namespace ACE.Server.Command.Handlers
             var wo = CommandHandlerHelper.GetLastAppraisedObject(session);
 
             if (wo != null)
-                session.Network.EnqueueSend(new GameMessageSystemChat($"WeenieClassId: {wo.WeenieClassId}\nWeenieClassName: {wo.WeenieClassName}", ChatMessageType.Broadcast));
+                session.Network.EnqueueSend(new GameMessageSystemChat($"GUID: {wo.Guid}\nWeenieClassId: {wo.WeenieClassId}\nWeenieClassName: {wo.WeenieClassName}", ChatMessageType.Broadcast));
         }
 
         public static WorldObject LastTestAim;
