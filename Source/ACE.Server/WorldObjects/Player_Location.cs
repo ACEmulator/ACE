86901e62bd0019bb19130b3483b72e53e1a151f4
Ty Conner <tyconner@itcproductions.com>
2020-07-17 21:26:31 -0400

    Fix issue with SendMotionAsCommands and stance

--- Source/ACE.Server/WorldObjects/Player_Location.cs
+++ Source/ACE.Server/WorldObjects/Player_Location.cs
@@ -578,12 +578,14 @@ namespace ACE.Server.WorldObjects
             if (FastTick)
             {
                 var actionChain = new ActionChain();
-                EnqueueMotionAction(actionChain, new List<MotionCommand>() { motionCommand }, 1.0f, MotionStance.NonCombat);
+                //EnqueueMotionAction(actionChain, new List<MotionCommand>() { motionCommand }, 1.0f, MotionStance.NonCombat);
+                EnqueueMotionAction(actionChain, new List<MotionCommand>() { motionCommand }, 1.0f, CurrentMotionState.Stance);
                 actionChain.EnqueueChain();
             }
             else
             {
-                var motion = new Motion(MotionStance.NonCombat, MotionCommand.Ready);
+                //var motion = new Motion(MotionStance.NonCombat, MotionCommand.Ready);
+                var motion = new Motion(CurrentMotionState.Stance, MotionCommand.Ready);
                 motion.MotionState.AddCommand(this, motionCommand);
                 EnqueueBroadcastMotion(motion);
             }
