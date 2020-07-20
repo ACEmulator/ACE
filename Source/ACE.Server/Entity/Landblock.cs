035c16b59d2ea5278397f96d961c42f8412fd2bb
Mag-nus <Mag-nus@users.noreply.github.com>
2020-07-19 21:33:47 -0500

    Log landblock group thread collisions that could throw exceptions

--- Source/ACE.Server/Entity/Landblock.cs
+++ Source/ACE.Server/Entity/Landblock.cs
@@ -620,8 +620,15 @@ namespace ACE.Server.Entity
             }
         }
 
+        private long processPendingWorldObjectAdditionsAndRemovalsCounter = 0;
+
         private void ProcessPendingWorldObjectAdditionsAndRemovals()
         {
+            var counterVal = Interlocked.Increment(ref processPendingWorldObjectAdditionsAndRemovalsCounter);
+
+            if (counterVal != 1)
+                log.Error($"Landblock 0x{Id} entered ProcessPendingWorldObjectAdditionsAndRemovals but counterVal: {counterVal} is not 1");
+
             if (pendingAdditions.Count > 0)
             {
                 foreach (var kvp in pendingAdditions)
@@ -671,6 +678,11 @@ namespace ACE.Server.Entity
 
                 pendingRemovals.Clear();
             }
+
+            counterVal = Interlocked.Decrement(ref processPendingWorldObjectAdditionsAndRemovalsCounter);
+
+            if (counterVal != 0)
+                log.Error($"Landblock 0x{Id} exited ProcessPendingWorldObjectAdditionsAndRemovals but counterVal: {counterVal} is not 0");
         }
 
         private void InsertWorldObjectIntoSortedHeartbeatList(WorldObject worldObject)
@@ -810,6 +822,15 @@ namespace ACE.Server.Entity
 
         private bool AddWorldObjectInternal(WorldObject wo)
         {
+            var counterVal = Interlocked.Read(ref processPendingWorldObjectAdditionsAndRemovalsCounter);
+
+            if (counterVal != 0)
+            {
+                log.Error($"Landblock 0x{Id} entered AddWorldObjectInternal but counterVal: {counterVal} is not 0");
+                log.Error(System.Environment.StackTrace);
+                log.Error("PLEASE REPORT THIS TO THE ACE DEV TEAM !!!");
+            }
+
             wo.CurrentLandblock = this;
 
             if (wo.PhysicsObj == null)
@@ -835,6 +856,13 @@ namespace ACE.Server.Entity
 
                     return false;
                 }
+            }
+
+            if (counterVal != 0)
+            {
+                log.Error($"Landblock 0x{Id} middle AddWorldObjectInternal but counterVal: {counterVal} is not 0");
+                log.Error(System.Environment.StackTrace);
+                log.Error("PLEASE REPORT THIS TO THE ACE DEV TEAM !!!");
             }
 
             if (!worldObjects.ContainsKey(wo.Guid))
@@ -868,6 +896,15 @@ namespace ACE.Server.Entity
 
         private void RemoveWorldObjectInternal(ObjectGuid objectId, bool adjacencyMove = false, bool fromPickup = false, bool showError = true)
         {
+            var counterVal = Interlocked.Read(ref processPendingWorldObjectAdditionsAndRemovalsCounter);
+
+            if (counterVal != 0)
+            {
+                log.Error($"Landblock 0x{Id} entered RemoveWorldObjectInternal but counterVal: {counterVal} is not 0");
+                log.Error(System.Environment.StackTrace);
+                log.Error("PLEASE REPORT THIS TO THE ACE DEV TEAM !!!");
+            }
+
             if (worldObjects.TryGetValue(objectId, out var wo))
                 pendingRemovals.Add(objectId);
             else if (!pendingAdditions.Remove(objectId, out wo))
