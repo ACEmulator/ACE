929c307dbe3b4ca6997bf8a7303f57382da4a39c
Ty Conner <tyconner@itcproductions.com>
2020-07-29 00:46:55 -0400

    Add support for Enlightenment (#3094)
    * Add support for Enlightenment
    
    * Update Enlightenment.cs
    
    * Update Enlightenment.cs
    
    * comments
    
    * Update Enlightenment.cs
    
    * Update Enlightenment.cs
    
    * Update Specialization rules
    
    If skill is specialized via augmentation gem and is tinkering skill, it can be removed/refunded. If retrained, it will go back to specialized.
    
    If skill is Salvaging, it will refund xp spent only, and remain specialized.
    
    * Update Enlightenment.cs
    
    * Update Enlightenment.cs
    
    * Update Player_Skills.cs
    
    * Update appveyor.yml

--- Source/ACE.Server/WorldObjects/Managers/EmoteManager.cs
+++ Source/ACE.Server/WorldObjects/Managers/EmoteManager.cs
@@ -302,6 +302,15 @@ namespace ACE.Server.WorldObjects.Managers
 
                     break;
 
+                case EmoteType.Enlightenment:
+
+                    if (player != null)
+                    {
+                        Enlightenment.HandleEnlightenment(WorldObject, player);
+                    }
+
+                    break;
+
                 case EmoteType.EraseMyQuest:
                 case EmoteType.EraseQuest:
 
@@ -530,11 +539,26 @@ namespace ACE.Server.WorldObjects.Managers
 
                 case EmoteType.InqPackSpace:
 
-                    //if (player != null)
-                    //{
-                    //    var freeSpace = player.ContainerCapacity > player.ItemCapacity;
-                    //    InqCategory(freeSpace ? EmoteCategory.TestSuccess : EmoteCategory.TestFailure, emote);
-                    //}
+                    if (player != null)
+                    {
+                        var numRequired = emote.Amount ?? 1;
+
+                        success = false;
+                        if (numRequired > 10000) // Since emote was not in 16py and we have just the two fields to go on, I will assume you could "mask" the value to pick between free Item Capacity space or free Container Capacity space
+                        {
+                            var freeSpace = player.GetFreeContainerSlots();
+
+                            success = freeSpace >= (numRequired - 10000);
+                        }
+                        else
+                        {
+                            var freeSpace = player.GetFreeInventorySlots(false); // assuming this was only for main pack. makes things easier at this point.
+
+                            success = freeSpace >= numRequired;
+                        }
+
+                        ExecuteEmoteSet(success ? EmoteCategory.TestSuccess : EmoteCategory.TestFailure, emote.Message, targetObject, true);
+                    }
                     break;
 
                 case EmoteType.InqMyQuest:
