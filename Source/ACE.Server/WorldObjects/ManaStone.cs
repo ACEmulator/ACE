e4df8cc366af6bb3f648c3da53cc8d8ab11fad87
gmriggs <gmriggs@gmail.com>
2020-08-26 10:44:01 -0400

    fix exception when giving mana to an item w/ ItemMaxMana, but not ItemCurMana

--- Source/ACE.Server/WorldObjects/ManaStone.cs
+++ Source/ACE.Server/WorldObjects/ManaStone.cs
@@ -176,7 +176,9 @@ namespace ACE.Server.WorldObjects
                 }
                 else if (target.ItemMaxMana.HasValue && target.ItemMaxMana.Value > 0)
                 {
-                    if (target.ItemCurMana.Value >= target.ItemMaxMana.Value)
+                    var targetItemCurMana = target.ItemCurMana ?? 0;
+
+                    if (targetItemCurMana >= target.ItemMaxMana)
                     {
                         player.Session.Network.EnqueueSend(new GameMessageSystemChat($"The {target.Name} is already full of mana.", ChatMessageType.Broadcast));
                     }
@@ -186,9 +188,9 @@ namespace ACE.Server.WorldObjects
 
                         // The Mana Stone gives 3,267 points of mana to the Protective Drudge Charm.
 
-                        var targetManaNeeded = target.ItemCurMana.HasValue ? (target.ItemMaxMana.Value - target.ItemCurMana.Value) : target.ItemMaxMana.Value;
+                        var targetManaNeeded = target.ItemMaxMana.Value - targetItemCurMana;
                         var manaToPour = Math.Min(targetManaNeeded, ItemCurMana.Value);
-                        target.ItemCurMana += manaToPour;
+                        target.ItemCurMana = targetItemCurMana + manaToPour;
                         var msg = $"The Mana Stone gives {manaToPour:N0} points of mana to the {target.Name}.";
                         player.Session.Network.EnqueueSend(new GameMessageSystemChat(msg, ChatMessageType.Broadcast));
 
