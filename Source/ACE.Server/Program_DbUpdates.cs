25ecc881f5bc179ec4847bafe77937cff6666b9d
Ty Conner <tyconner@itcproductions.com>
2020-08-27 16:41:03 -0400

    On error, exit CheckForWorldDatabaseUpdate gracefully

--- Source/ACE.Server/Program_DbUpdates.cs
+++ Source/ACE.Server/Program_DbUpdates.cs
@@ -19,38 +19,45 @@ namespace ACE.Server
         private static void CheckForWorldDatabaseUpdate()
         {
             log.Info($"Automatic World Database Update started...");
-            var worldDb = new Database.WorldDatabase();
-            var currentVersion = worldDb.GetVersion();
-            log.Info($"Current World Database version: Base - {currentVersion.BaseVersion} | Patch - {currentVersion.PatchVersion}");
-
-            var url = "https://api.github.com/repos/ACEmulator/ACE-World-16PY-Patches/releases";
-            var request = (HttpWebRequest)WebRequest.Create(url);
-            request.UserAgent = "ACE.Server";
-
-            var response = request.GetResponse();
-            var reader = new StreamReader(response.GetResponseStream(), System.Text.Encoding.UTF8);
-            var html = reader.ReadToEnd();
-            reader.Close();
-            response.Close();
-
-            dynamic json = JsonConvert.DeserializeObject(html);
-            string tag = json[0].tag_name;
-            string dbURL = json[0].assets[0].browser_download_url;
-            string dbFileName = json[0].assets[0].name;
-
-            if (currentVersion.PatchVersion != tag)
+            try
             {
-                log.Info($"Latest patch version is {tag} -- Update Required!");
-                UpdateToLatestWorldDatabase(dbURL, dbFileName);
-                var newVersion = worldDb.GetVersion();
-                log.Info($"Updated World Database version: Base - {newVersion.BaseVersion} | Patch - {newVersion.PatchVersion}");
-            }
-            else
-            {
-                log.Info($"Latest patch version is {tag} -- No Update Required!");
-            }
+                var worldDb = new Database.WorldDatabase();
+                var currentVersion = worldDb.GetVersion();
+                log.Info($"Current World Database version: Base - {currentVersion.BaseVersion} | Patch - {currentVersion.PatchVersion}");
 
+                var url = "https://api.github.com/repos/ACEmulator/ACE-World-16PY-Patches/releases";
+                var request = (HttpWebRequest)WebRequest.Create(url);
+                request.UserAgent = "ACE.Server";
+
+                var response = request.GetResponse();
+                var reader = new StreamReader(response.GetResponseStream(), System.Text.Encoding.UTF8);
+                var html = reader.ReadToEnd();
+                reader.Close();
+                response.Close();
+
+                dynamic json = JsonConvert.DeserializeObject(html);
+                string tag = json[0].tag_name;
+                string dbURL = json[0].assets[0].browser_download_url;
+                string dbFileName = json[0].assets[0].name;
+
+                if (currentVersion.PatchVersion != tag)
+                {
+                    log.Info($"Latest patch version is {tag} -- Update Required!");
+                    UpdateToLatestWorldDatabase(dbURL, dbFileName);
+                    var newVersion = worldDb.GetVersion();
+                    log.Info($"Updated World Database version: Base - {newVersion.BaseVersion} | Patch - {newVersion.PatchVersion}");
+                }
+                else
+                {
+                    log.Info($"Latest patch version is {tag} -- No Update Required!");
+                }
+            }
+            catch (Exception ex)
+            {
+                log.Info($"Unable to continue with Automatic World Database Update due to error: {ex}");
+            }
             log.Info($"Automatic World Database Update complete.");
+
         }
 
         private static void UpdateToLatestWorldDatabase(string dbURL, string dbFileName)
