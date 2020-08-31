1477eb49a7748a1d856713896f80394352425390
Ty Conner <tyconner@itcproductions.com>
2020-08-31 17:08:47 +0000

    [ci skip] Updating ServerBuildInfo_Dynamic.cs
    1.11.3564.20200831170641-master-0e9009e

--- Source/ACE.Server/ServerBuildInfo_Dynamic.cs
+++ Source/ACE.Server/ServerBuildInfo_Dynamic.cs
@@ -4,17 +4,17 @@ namespace ACE.Server
     public static partial class ServerBuildInfo 
     { 
         public static string Branch = "master"; 
-        public static string Commit = "d4d85a6917a00acf05c7ce3cff023d80de5d48fc"; 
+        public static string Commit = "0e9009ec88a165238a3e7d6bfe09c60f0dc6fe7c"; 
  
         public static string Version = "1.11"; 
-        public static string Build   = "3563"; 
+        public static string Build   = "3564"; 
  
         public static int BuildYear   = 2020; 
         public static int BuildMonth  = 08; 
         public static int BuildDay    = 31; 
-        public static int BuildHour   = 16; 
-        public static int BuildMinute = 39; 
-        public static int BuildSecond = 06; 
+        public static int BuildHour   = 17; 
+        public static int BuildMinute = 06; 
+        public static int BuildSecond = 41; 
     } 
 } 
  
