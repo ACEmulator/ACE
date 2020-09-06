58cf9a418f3d175d52c2c745c85a5a467d3e11d0
gmriggs <gmriggs@gmail.com>
2020-09-05 21:06:16 -0400

    adjusting format

--- Source/ACE.Server/Factories/Tables/TreasureItemTypeChances_Orig.cs
+++ Source/ACE.Server/Factories/Tables/TreasureItemTypeChances_Orig.cs
@@ -7,131 +7,131 @@ namespace ACE.Server.Factories.Tables
 {
     public static class TreasureItemTypeChances_Orig
     {
-        private static readonly List<TreasureItemTypeChance_Orig> profile1 = new List<TreasureItemTypeChance_Orig>()
+        private static readonly ChanceTable<TreasureItemType_Orig> profile1 = new ChanceTable<TreasureItemType_Orig>()
         {
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Weapon, 1),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Armor, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Scroll, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Clothing, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Jewelry, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Gem, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.ArtObject, 0),
+            ( TreasureItemType_Orig.Weapon,    1.0f ),
+            ( TreasureItemType_Orig.Armor,     0.0f ),
+            ( TreasureItemType_Orig.Scroll,    0.0f ),
+            ( TreasureItemType_Orig.Clothing,  0.0f ),
+            ( TreasureItemType_Orig.Jewelry,   0.0f ),
+            ( TreasureItemType_Orig.Gem,       0.0f ),
+            ( TreasureItemType_Orig.ArtObject, 0.0f ),
         };
 
-        private static readonly List<TreasureItemTypeChance_Orig> profile2 = new List<TreasureItemTypeChance_Orig>()
+        private static readonly ChanceTable<TreasureItemType_Orig> profile2 = new ChanceTable<TreasureItemType_Orig>()
         {
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Weapon, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Armor, 1),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Scroll, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Clothing, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Jewelry, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Gem, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.ArtObject, 0),
+            ( TreasureItemType_Orig.Weapon,    0.0f ),
+            ( TreasureItemType_Orig.Armor,     1.0f ),
+            ( TreasureItemType_Orig.Scroll,    0.0f ),
+            ( TreasureItemType_Orig.Clothing,  0.0f ),
+            ( TreasureItemType_Orig.Jewelry,   0.0f ),
+            ( TreasureItemType_Orig.Gem,       0.0f ),
+            ( TreasureItemType_Orig.ArtObject, 0.0f ),
         };
 
-        private static readonly List<TreasureItemTypeChance_Orig> profile3 = new List<TreasureItemTypeChance_Orig>()
+        private static readonly ChanceTable<TreasureItemType_Orig> profile3 = new ChanceTable<TreasureItemType_Orig>()
         {
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Weapon, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Armor, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Scroll, 1),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Clothing, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Jewelry, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Gem, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.ArtObject, 0),
+            ( TreasureItemType_Orig.Weapon,    0.0f ),
+            ( TreasureItemType_Orig.Armor,     0.0f ),
+            ( TreasureItemType_Orig.Scroll,    1.0f ),
+            ( TreasureItemType_Orig.Clothing,  0.0f ),
+            ( TreasureItemType_Orig.Jewelry,   0.0f ),
+            ( TreasureItemType_Orig.Gem,       0.0f ),
+            ( TreasureItemType_Orig.ArtObject, 0.0f ),
         };
 
-        private static readonly List<TreasureItemTypeChance_Orig> profile4 = new List<TreasureItemTypeChance_Orig>()
+        private static readonly ChanceTable<TreasureItemType_Orig> profile4 = new ChanceTable<TreasureItemType_Orig>()
         {
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Weapon, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Armor, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Scroll, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Clothing, 1),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Jewelry, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Gem, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.ArtObject, 0),
+            ( TreasureItemType_Orig.Weapon,    0.0f ),
+            ( TreasureItemType_Orig.Armor,     0.0f ),
+            ( TreasureItemType_Orig.Scroll,    0.0f ),
+            ( TreasureItemType_Orig.Clothing,  1.0f ),
+            ( TreasureItemType_Orig.Jewelry,   0.0f ),
+            ( TreasureItemType_Orig.Gem,       0.0f ),
+            ( TreasureItemType_Orig.ArtObject, 0.0f ),
         };
 
-        private static readonly List<TreasureItemTypeChance_Orig> profile5 = new List<TreasureItemTypeChance_Orig>()
+        private static readonly ChanceTable<TreasureItemType_Orig> profile5 = new ChanceTable<TreasureItemType_Orig>()
         {
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Weapon, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Armor, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Scroll, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Clothing, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Jewelry, 1),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Gem, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.ArtObject, 0),
+            ( TreasureItemType_Orig.Weapon,    0.0f ),
+            ( TreasureItemType_Orig.Armor,     0.0f ),
+            ( TreasureItemType_Orig.Scroll,    0.0f ),
+            ( TreasureItemType_Orig.Clothing,  0.0f ),
+            ( TreasureItemType_Orig.Jewelry,   1.0f ),
+            ( TreasureItemType_Orig.Gem,       0.0f ),
+            ( TreasureItemType_Orig.ArtObject, 0.0f ),
         };
 
-        private static readonly List<TreasureItemTypeChance_Orig> profile6 = new List<TreasureItemTypeChance_Orig>()
+        private static readonly ChanceTable<TreasureItemType_Orig> profile6 = new ChanceTable<TreasureItemType_Orig>()
         {
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Weapon, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Armor, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Scroll, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Clothing, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Jewelry, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Gem, 1),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.ArtObject, 0),
+            ( TreasureItemType_Orig.Weapon,    0.0f ),
+            ( TreasureItemType_Orig.Armor,     0.0f ),
+            ( TreasureItemType_Orig.Scroll,    0.0f ),
+            ( TreasureItemType_Orig.Clothing,  0.0f ),
+            ( TreasureItemType_Orig.Jewelry,   0.0f ),
+            ( TreasureItemType_Orig.Gem,       1.0f ),
+            ( TreasureItemType_Orig.ArtObject, 0.0f ),
         };
 
-        private static readonly List<TreasureItemTypeChance_Orig> profile7 = new List<TreasureItemTypeChance_Orig>()
+        private static readonly ChanceTable<TreasureItemType_Orig> profile7 = new ChanceTable<TreasureItemType_Orig>()
         {
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Weapon, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Armor, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Scroll, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Clothing, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Jewelry, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Gem, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.ArtObject, 1),
+            ( TreasureItemType_Orig.Weapon,    0.0f ),
+            ( TreasureItemType_Orig.Armor,     0.0f ),
+            ( TreasureItemType_Orig.Scroll,    0.0f ),
+            ( TreasureItemType_Orig.Clothing,  0.0f ),
+            ( TreasureItemType_Orig.Jewelry,   0.0f ),
+            ( TreasureItemType_Orig.Gem,       0.0f ),
+            ( TreasureItemType_Orig.ArtObject, 1.0f ),
         };
 
-        private static readonly List<TreasureItemTypeChance_Orig> profile8 = new List<TreasureItemTypeChance_Orig>()
+        private static readonly ChanceTable<TreasureItemType_Orig> profile8 = new ChanceTable<TreasureItemType_Orig>()
         {
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Weapon, 0.15f),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Armor, 0.15f),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Scroll, 0.14f),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Clothing, 0.14f),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Jewelry, 0.14f),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Gem, 0.14f),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.ArtObject, 0.14f),
+            ( TreasureItemType_Orig.Weapon,    0.15f ),
+            ( TreasureItemType_Orig.Armor,     0.15f ),
+            ( TreasureItemType_Orig.Scroll,    0.14f ),
+            ( TreasureItemType_Orig.Clothing,  0.14f ),
+            ( TreasureItemType_Orig.Jewelry,   0.14f ),
+            ( TreasureItemType_Orig.Gem,       0.14f ),
+            ( TreasureItemType_Orig.ArtObject, 0.14f ),
         };
 
-        private static readonly List<TreasureItemTypeChance_Orig> profile9 = new List<TreasureItemTypeChance_Orig>()
+        private static readonly ChanceTable<TreasureItemType_Orig> profile9 = new ChanceTable<TreasureItemType_Orig>()
         {
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Weapon, 0.3f),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Armor, 0.3f),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Scroll, 0.2f),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Clothing, 0.05f),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Jewelry, 0.05f),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Gem, 0.05f),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.ArtObject, 0.05f),
+            ( TreasureItemType_Orig.Weapon,    0.30f ),
+            ( TreasureItemType_Orig.Armor,     0.30f ),
+            ( TreasureItemType_Orig.Scroll,    0.20f ),
+            ( TreasureItemType_Orig.Clothing,  0.05f ),
+            ( TreasureItemType_Orig.Jewelry,   0.05f ),
+            ( TreasureItemType_Orig.Gem,       0.05f ),
+            ( TreasureItemType_Orig.ArtObject, 0.05f ),
         };
 
-        private static readonly List<TreasureItemTypeChance_Orig> profile10 = new List<TreasureItemTypeChance_Orig>()
+        private static readonly ChanceTable<TreasureItemType_Orig> profile10 = new ChanceTable<TreasureItemType_Orig>()
         {
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Weapon, 0.4f),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Armor, 0.4f),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Scroll, 0.2f),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Clothing, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Jewelry, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Gem, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.ArtObject, 0),
+            ( TreasureItemType_Orig.Weapon,    0.40f ),
+            ( TreasureItemType_Orig.Armor,     0.40f ),
+            ( TreasureItemType_Orig.Scroll,    0.20f ),
+            ( TreasureItemType_Orig.Clothing,  0.0f ),
+            ( TreasureItemType_Orig.Jewelry,   0.0f ),
+            ( TreasureItemType_Orig.Gem,       0.0f ),
+            ( TreasureItemType_Orig.ArtObject, 0.0f ),
         };
 
-        private static readonly List<TreasureItemTypeChance_Orig> profile11 = new List<TreasureItemTypeChance_Orig>()
+        private static readonly ChanceTable<TreasureItemType_Orig> profile11 = new ChanceTable<TreasureItemType_Orig>()
         {
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Weapon, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Armor, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Scroll, 0),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Clothing, 0.25f),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Jewelry, 0.25f),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.Gem, 0.25f),
-            new TreasureItemTypeChance_Orig(TreasureItemType_Orig.ArtObject, 0.25f),
+            ( TreasureItemType_Orig.Weapon,    0.0f ),
+            ( TreasureItemType_Orig.Armor,     0.0f ),
+            ( TreasureItemType_Orig.Scroll,    0.0f ),
+            ( TreasureItemType_Orig.Clothing,  0.25f ),
+            ( TreasureItemType_Orig.Jewelry,   0.25f ),
+            ( TreasureItemType_Orig.Gem,       0.25f ),
+            ( TreasureItemType_Orig.ArtObject, 0.25f ),
         };
 
         /// <summary>
         /// Index lookup would be treasure_death table, *_Selection_Chances columns
         /// </summary>
-        public static Dictionary<int, List<TreasureItemTypeChance_Orig>> Profiles = new Dictionary<int, List<TreasureItemTypeChance_Orig>>()
+        public static Dictionary<int, ChanceTable<TreasureItemType_Orig>> Profiles = new Dictionary<int, ChanceTable<TreasureItemType_Orig>>()
         {
             { 1, profile1 },
             { 2, profile2 },
