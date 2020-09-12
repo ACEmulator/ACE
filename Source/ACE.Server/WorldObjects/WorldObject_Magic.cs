03d3a136b81e5f40094d262df369d6349a661201
gmriggs <gmriggs@gmail.com>
2020-09-12 12:28:40 -0400

    fixing fellowship spell messages (#3225)

--- Source/ACE.Server/WorldObjects/WorldObject_Magic.cs
+++ Source/ACE.Server/WorldObjects/WorldObject_Magic.cs
@@ -380,7 +380,7 @@ namespace ACE.Server.WorldObjects
             var player = this as Player;
             var creature = this as Creature;
 
-            var spellTarget = !spell.IsSelfTargeted ? target as Creature : creature;
+            var spellTarget = !spell.IsSelfTargeted || spell.IsFellowshipSpell ? target as Creature : creature;
 
             if (this is Gem || this is Food || this is Hook)
                 spellTarget = target as Creature;
