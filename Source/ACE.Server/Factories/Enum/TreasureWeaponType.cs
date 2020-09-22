fb71e497ac793836772062ec84275c6e28cdd7ec
gmriggs <gmriggs@gmail.com>
2020-09-22 18:43:31 -0400

    adding post-tod melee weapon chance tables (#3240)
    * adding post-tod melee weapon chance tables
    
    * upgrade
    
    * finalizing
    
    * removing unused function

--- Source/ACE.Server/Factories/Enum/TreasureWeaponType.cs
+++ Source/ACE.Server/Factories/Enum/TreasureWeaponType.cs
@@ -2,6 +2,8 @@ namespace ACE.Server.Factories.Enum
 {
     public enum TreasureWeaponType
     {
+        // even after MoA, retail appeared to retain the initial roll for melee weapons based on WeaponType
+        MeleeWeapon,
         Sword,
         Mace,
         Axe,
@@ -9,9 +11,22 @@ namespace ACE.Server.Factories.Enum
         Unarmed,
         Staff,
         Dagger,
+
+        MissileWeapon,
         Bow,
         Crossbow,
         Atlatl,
+
         Caster,
+
+        // this could also be placed in WeaponWcids, however the roll for TwoHandedWeapons appears to have worked differently from Heavy/Light/Finesse
+        // for Heavy/Light/Finesse, there did not appear to be any kind of pre-roll that divied up weapons evenly between these types
+        // overall, the chances appeared to be about 36/30/34, however for a particular weapon type (which are definitely even in retail logs), these ratios vary,
+        // with little discernable pattern (ie. not by total # of weapons for that type), other than simply entering different values in the inner chance tables
+        // two-handed however, is much more consistent: each of the different two-handed weapons appeared to have an even chance
+        // because of the differences between heavy/light/finesse and two-handed, placing this here makes more sense currently,
+        // as the weapon type would essentially still get discarded, even if it was in WeaponWcids
+
+        TwoHandedWeapon,
     }
 }
--- Source/ACE.Server/Factories/Tables/Wcids/WeaponWcids.cs
+++ Source/ACE.Server/Factories/Tables/Wcids/WeaponWcids.cs
@@ -1,3 +1,4 @@
+using ACE.Common;
 using ACE.Database.Models.World;
 using ACE.Entity.Enum;
 using ACE.Server.Factories.Enum;
@@ -12,7 +13,7 @@ namespace ACE.Server.Factories.Tables.Wcids
         {
             switch (weaponType)
             {
-                case TreasureWeaponType.Sword:
+                /*case TreasureWeaponType.Sword:
                     return RollSwordWcid(treasureDeath);
 
                 case TreasureWeaponType.Mace:
@@ -31,7 +32,16 @@ namespace ACE.Server.Factories.Tables.Wcids
                     return RollStaffWcid(treasureDeath);
 
                 case TreasureWeaponType.Dagger:
-                    return RollDaggerWcid(treasureDeath);
+                    return RollDaggerWcid(treasureDeath);*/
+
+                case TreasureWeaponType.Sword:
+                case TreasureWeaponType.Mace:
+                case TreasureWeaponType.Axe:
+                case TreasureWeaponType.Spear:
+                case TreasureWeaponType.Unarmed:
+                case TreasureWeaponType.Staff:
+                case TreasureWeaponType.Dagger:
+                    return RollMeleeWeapon();
 
                 case TreasureWeaponType.Bow:
                     return RollBowWcid(treasureDeath);
@@ -44,6 +54,44 @@ namespace ACE.Server.Factories.Tables.Wcids
 
                 case TreasureWeaponType.Caster:
                     return RollCaster(treasureDeath);
+
+                case TreasureWeaponType.TwoHandedWeapon:
+                    return RollTwoHandedWeaponWcid();
+            }
+            return WeenieClassName.undef;
+        }
+
+        public static WeenieClassName RollMeleeWeapon()
+        {
+            // retail did something silly here --
+            // instead of having an even chance to roll between heavy/light/finesse,
+            // they kept everything divied up by weaponType
+
+            // doing something slightly less silly here...
+
+            // basically throwing out the weaponType here,
+            // rolling for an even chance between heavy/light/finesse,
+            // and then an even chance into each weapon for that skill
+
+            // if you wish to maintain a profile closer to retail overall,
+            // heavy 36% | light 30% | finesse 34%
+
+            // however, it still wouldn't match up exactly,
+            // as each weaponType still had slightly different chances,
+            // which were most likely engrained deeply in the per-weaponType chance tables
+
+            var weaponSkill = (MeleeWeaponSkill)ThreadSafeRandom.Next(1, 3);
+
+            switch (weaponSkill)
+            {
+                case MeleeWeaponSkill.HeavyWeapons:
+                    return HeavyWeaponWcids.Roll();
+
+                case MeleeWeaponSkill.LightWeapons:
+                    return LightWeaponWcids.Roll();
+
+                case MeleeWeaponSkill.FinesseWeapons:
+                    return FinesseWeaponWcids.Roll();
             }
             return WeenieClassName.undef;
         }
@@ -153,6 +201,11 @@ namespace ACE.Server.Factories.Tables.Wcids
         public static WeenieClassName RollCaster(TreasureDeath treasureDeath)
         {
             return CasterWcids.Roll(treasureDeath.Tier);
+        }
+
+        public static WeenieClassName RollTwoHandedWeaponWcid()
+        {
+            return TwoHandedWeaponWcids.Roll();
         }
     }
 }
