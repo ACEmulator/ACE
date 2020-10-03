af461c22686eda9577ce3156a6ff6f26405a7de8
gmriggs <gmriggs@gmail.com>
2020-10-03 01:17:44 -0400

    porting *_cantrip_drop_rate server config options to updated cantrips system (#3260)
    * adding end of retail mutation filters for melee weapon damage, wield difficulty, damage variance, weapondefense, and weaponoffense
    
    * .
    
    * adding 4eyebiped credit
    
    * adding end of retail mutation filters for missile weapon DamageMod, ElementalDamageBonus, WieldRequirements, and WeaponDefense
    
    * adding end of retail mutation filters for caster weapon ManaConversionMod, ElementalDamageMod, WieldRequirements, and WeaponDefense
    
    * update melee scripts
    
    * resolve merge
    
    * .
    
    * add missing lines
    
    * adding weapon speed mutations
    
    * appending LongDesc with ' of <first spell name w/ descriptor>'
    
    * .
    
    * adding some missing spell descs from magloot logs
    
    * adding SpellDIDs to isMagical casters
    
    * wip
    
    * finalizing
    
    * updating qualityMod
    
    * modifying qualityMod again, to align with data + magloot logs for legendary chests
    
    * ConcurrentDictionary.TryAdd
    
    * updating Clothing spells w/ latest data
    
    * porting *_cantrip_drop_rate server config options to updated cantrips system

--- Source/ACE.Server/Managers/PropertyManager.cs
+++ Source/ACE.Server/Managers/PropertyManager.cs
@@ -219,7 +219,7 @@ namespace ACE.Server.Managers
         /// </summary>
         /// <param name="key">The string key for the property</param>
         /// <param name="newVal">The value to replace the old value with</param>
-        public static bool ModifyDouble(string key, double newVal)
+        public static bool ModifyDouble(string key, double newVal, bool init = false)
         {
             if (!DefaultPropertyManager.DefaultDoubleProperties.ContainsKey(key))
                 return false;
@@ -227,6 +227,22 @@ namespace ACE.Server.Managers
                 CachedDoubleSettings[key].Modify(newVal);
             else
                 CachedDoubleSettings[key] = new ConfigurationEntry<double>(true, newVal, DefaultPropertyManager.DefaultDoubleProperties[key].Description);
+
+            if (!init)
+            {
+                switch (key)
+                {
+                    case "cantrip_drop_rate":
+                        Factories.Tables.CantripChance.ApplyNumCantripsMod();
+                        break;
+                    case "minor_cantrip_drop_rate":
+                    case "major_cantrip_drop_rate":
+                    case "epic_cantrip_drop_rate":
+                    case "legendary_cantrip_drop_rate":
+                        Factories.Tables.CantripChance.ApplyCantripLevelsMod();
+                        break;
+                }
+            }
             return true;
         }
 
@@ -464,7 +480,7 @@ namespace ACE.Server.Managers
 
             //float
             foreach (var item in DefaultDoubleProperties)
-                PropertyManager.ModifyDouble(item.Key, item.Value.Item);
+                PropertyManager.ModifyDouble(item.Key, item.Value.Item, true);
 
             //int
             foreach (var item in DefaultLongProperties)
@@ -561,10 +577,12 @@ namespace ACE.Server.Managers
         public static readonly ReadOnlyDictionary<string, Property<double>> DefaultDoubleProperties =
             DictOf(
 
-                ("minor_cantrip_drop_rate", new Property<double>(1.0, "Modifier for minor cantrip drop rate, 1 being normal")),
-                ("major_cantrip_drop_rate", new Property<double>(1.0, "Modifier for major cantrip drop rate, 1 being normal")),
-                ("epic_cantrip_drop_rate", new Property<double>(1.0, "Modifier for epic cantrip drop rate, 1 being normal")),
-                ("legendary_cantrip_drop_rate", new Property<double>(1.0, "Modifier for legendary cantrip drop rate, 1 being normal")),
+                ("cantrip_drop_rate", new Property<double>(1.0, "Scales the chance for cantrips to drop in each tier. Defaults to 1.0, as per end of retail")),
+
+                ("minor_cantrip_drop_rate", new Property<double>(1.0, "Scales the chance for minor cantrips to drop, relative to other cantrip levels in the tier. Defaults to 1.0, as per end of retail")),
+                ("major_cantrip_drop_rate", new Property<double>(1.0, "Scales the chance for major cantrips to drop, relative to other cantrip levels in the tier. Defaults to 1.0, as per end of retail")),
+                ("epic_cantrip_drop_rate", new Property<double>(1.0, "Scales the chance for epic cantrips to drop, relative to other cantrip levels in the tier. Defaults to 1.0, as per end of retail")),
+                ("legendary_cantrip_drop_rate", new Property<double>(1.0, "Scales the chance for legendary cantrips to drop, relative to other cantrip levels in the tier. Defaults to 1.0, as per end of retail")),
 
                 ("advocate_fane_auto_bestow_level", new Property<double>(1, "the level that advocates are automatically bestowed by Advocate Fane if advocate_fane_auto_bestow is true")),
                 ("aetheria_drop_rate", new Property<double>(1.0, "Modifier for Aetheria drop rate, 1 being normal")),
