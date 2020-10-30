eaf3428eb39eefd31d9c98d6dbecee94aa2dec0c
gmriggs <gmriggs@gmail.com>
2020-10-30 13:19:43 -0400

    fix vulnerability in RemoveFellowshipMember()

--- Source/ACE.Server/WorldObjects/Player_Fellowship.cs
+++ Source/ACE.Server/WorldObjects/Player_Fellowship.cs
@@ -66,7 +66,7 @@ namespace ACE.Server.WorldObjects
             if (fellowToDismiss == null)
                 return;
 
-            Fellowship.RemoveFellowshipMember(fellowToDismiss);
+            Fellowship.RemoveFellowshipMember(fellowToDismiss, this);
         }
 
         public void FellowshipRecruit(Player newPlayer)
