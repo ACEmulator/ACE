e46e917782c043367e69efb225ae1ec8cd1b5bbd
gmriggs <gmriggs@gmail.com>
2020-11-22 02:57:36 -0500

    fix narnia arrows (#3365)
    * fix narnia arrows
    
    * .

--- Source/ACE.Server/Physics/PhysicsObj.cs
+++ Source/ACE.Server/Physics/PhysicsObj.cs
@@ -3468,6 +3468,8 @@ namespace ACE.Server.Physics
                 }
                 change_cell_server(newCell);
             }
+
+            CachedVelocity = requestCachedVelocity;
         }
 
         /// <summary>
@@ -3863,6 +3865,8 @@ namespace ACE.Server.Physics
             return true;
         }
 
+        private Vector3 requestCachedVelocity;
+
         /// <summary>
         /// Sets the requested position to the AutonomousPosition
         /// received from the client
@@ -3883,6 +3887,8 @@ namespace ACE.Server.Physics
                 RequestPos.ObjCellID = RequestPos.GetCell(CurCell.ID);
             else
                 RequestPos.ObjCellID = cell.ID;
+
+            requestCachedVelocity = CachedVelocity;
         }
 
         public void set_sequence_animation(int animID, bool interrupt, int startFrame, float framerate)
