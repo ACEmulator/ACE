c39bd19dc10a001bb45cf0e07602b87b3625f631
gmriggs <gmriggs@gmail.com>
2020-12-13 19:22:56 -0500

    .

--- Source/ACE.DatLoader/FileTypes/SpellTable.cs
+++ Source/ACE.DatLoader/FileTypes/SpellTable.cs
@@ -1,11 +1,8 @@
-using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 
 using ACE.DatLoader.Entity;
-using ACE.Entity.Enum;
 
 namespace ACE.DatLoader.FileTypes
 {
