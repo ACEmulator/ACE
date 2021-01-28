cbadfc086e254cf107a767eb384ef83e20c274a1
Ty Conner <tyconner@itcproductions.com>
2021-01-27 23:44:52 -0500

    Update ability to log chat via log4net (#3438)

--- Source/ACE.Database/ShardDatabaseOfflineTools.cs
+++ Source/ACE.Database/ShardDatabaseOfflineTools.cs
@@ -996,7 +996,7 @@ namespace ACE.Database
         /// </summary>
         public static void CheckForBiotaPropertiesPaletteOrderColumnInShard()
         {
-            log.Info($"Checking for order column in biota_properties_palette table in shard database...");
+            //log.Info($"Checking for order column in biota_properties_palette table in shard database...");
 
             using (var context = new ShardDbContext())
             {
@@ -1022,7 +1022,7 @@ namespace ACE.Database
                 }
             }
 
-            log.Info($"Successfully verified order column in biota_properties_palette table in shard database!");
+            //log.Info($"Successfully verified order column in biota_properties_palette table in shard database!");
         }
     }
 }
