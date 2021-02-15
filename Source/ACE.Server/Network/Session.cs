85fd5a571d372b86aa034d0177b36722ec0ce894
Mag-nus <Mag-nus@users.noreply.github.com>
2021-02-15 11:58:15 -0600

    Improve Character Save Process
    This fixes a couple of race conditions

--- Source/ACE.Server/Network/Session.cs
+++ Source/ACE.Server/Network/Session.cs
@@ -196,6 +196,11 @@ namespace ACE.Server.Network
         {
             if (Player == null) return;
 
+            // Character database objects are not cached. Each session gets a new character entity and dbContext from ShardDatabase.
+            // To ensure the latest version of the character is saved before any new logins pull these records again, we queue a save here if necessary, at the instant logoff is requested.
+            if (Player.CharacterChangesDetected)
+                Player.SaveCharacterToDatabase();
+
             if (logOffRequestTime == DateTime.MinValue)
             {
                 var result = Player.LogOut(false, forceImmediate);
