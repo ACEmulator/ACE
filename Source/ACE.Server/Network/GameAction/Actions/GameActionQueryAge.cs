89c7656cb9ce0e145f964d0f218507e6baa3dc3f
Ty Conner <tyconner@itcproductions.com>
2021-02-26 14:41:38 -0500

    Match /age output with client character panel

--- Source/ACE.Server/Network/GameAction/Actions/GameActionQueryAge.cs
+++ Source/ACE.Server/Network/GameAction/Actions/GameActionQueryAge.cs
@@ -9,57 +9,41 @@ namespace ACE.Server.Network.GameAction.Actions
         [GameAction(GameActionType.QueryAge)]
         public static void Handle(ClientMessage message, Session session)
         {
-            var target = message.Payload.ReadString16L();
-            DateTime playerDOB = new DateTime();
-            playerDOB = playerDOB.AddSeconds(session.Player.Age.Value);
-            TimeSpan tsAge = playerDOB - new DateTime();
+            var target = message.Payload.ReadString16L(); // unused?
 
-            string age = "";
+            string ageMsg = "";
 
-            if (tsAge.ToString("%d") != "0")
-            {
-                if (Convert.ToInt16(tsAge.ToString("%d")) > 0 && Convert.ToInt16(tsAge.ToString("%d")) <= 7)
-                    age = age + tsAge.ToString("%d") + "d ";
-                if (Convert.ToInt16(tsAge.ToString("%d")) > 7)
-                {
-                    int months  = 0;
-                    int weeks   = 0;
-                    int days    = 0;
+            var ageSeconds = session.Player.Age.Value;
+            var years = ageSeconds / 31536000;
+            var months = ageSeconds % 31536000 / 2592000;
+            var _hours = ageSeconds % 31536000 % 2592000;
+            var weeks = _hours / 604800;
+            _hours %= 604800;
+            var days = _hours / 86400;
+            _hours %= 86400;
+            var hours = _hours / 3600;
+            _hours %= 3600;
+            var seconds = _hours % 60;
+            var minutes = _hours / 60;
 
-                    for (int i = 0; i < tsAge.Days; i++)
-                    {
-                        days++;
-                        if (days > 7)
-                        {
-                            weeks++;
-                            days = 0;
-                        }
-                        if (weeks > 3)
-                        {
-                            months++;
-                            weeks = 0;
-                        }
-                    }
+            if (years > 0)
+                ageMsg += $"{years}y ";
+            if (months > 0)
+                ageMsg += $"{months}mo ";
+            if (weeks > 0)
+                ageMsg += $"{weeks}w ";
+            if (days > 0)
+                ageMsg += $"{days}d ";
+            if (hours > 0)
+                ageMsg += $"{hours}h ";
+            if (minutes > 0)
+                ageMsg += $"{minutes}m ";
+            if (seconds > 0)
+                ageMsg += $"{seconds}s";
 
-                    if (months > 0)
-                        age = age + months + "mo ";
-                    if (weeks > 0)
-                        age = age + weeks + "w ";
-                    if (days > 0)
-                        age = age + days + "d ";
-                }
-            }
+            ageMsg = ageMsg.TrimEnd();
 
-            if (tsAge.ToString("%h") != "0")
-                age = age + tsAge.ToString("%h") + "h ";
-
-            if (tsAge.ToString("%m") != "0")
-                age = age + tsAge.ToString("%m") + "m ";
-
-            if (tsAge.ToString("%s") != "0")
-                age = age + tsAge.ToString("%s") + "s";
-
-            var ageEvent = new GameEvent.Events.GameEventQueryAgeResponse(session, "", age);
+            var ageEvent = new GameEvent.Events.GameEventQueryAgeResponse(session, "", ageMsg);
 
             session.Network.EnqueueSend(ageEvent);
         }
