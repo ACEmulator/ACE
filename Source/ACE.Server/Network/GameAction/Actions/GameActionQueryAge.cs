289d08f4cd18815594afda5d79e1bf77c81956d4
Ty Conner <tyconner@itcproductions.com>
2021-02-27 23:07:41 -0500

    Match /age output with client character panel (#3486)
    * Match /age output with client character panel
    
    * Update GameActionQueryAge.cs
    
    Co-Authored-By: gmriggs <8909245+gmriggs@users.noreply.github.com>
    
    Co-authored-by: gmriggs <8909245+gmriggs@users.noreply.github.com>

--- Source/ACE.Server/Network/GameAction/Actions/GameActionQueryAge.cs
+++ Source/ACE.Server/Network/GameAction/Actions/GameActionQueryAge.cs
@@ -1,6 +1,7 @@
-using System;
+using System.Collections.Generic;
 
 using ACE.Common.Extensions;
+using ACE.Server.Network.GameEvent.Events;
 
 namespace ACE.Server.Network.GameAction.Actions
 {
@@ -9,59 +10,95 @@ namespace ACE.Server.Network.GameAction.Actions
         [GameAction(GameActionType.QueryAge)]
         public static void Handle(ClientMessage message, Session session)
         {
-            var target = message.Payload.ReadString16L();
-            DateTime playerDOB = new DateTime();
-            playerDOB = playerDOB.AddSeconds(session.Player.Age.Value);
-            TimeSpan tsAge = playerDOB - new DateTime();
+            var target = message.Payload.ReadString16L();  // unused?
 
-            string age = "";
+            var ageMsg = CalculateAgeMessage(session.Player.Age ?? 0);
 
-            if (tsAge.ToString("%d") != "0")
+            session.Network.EnqueueSend(new GameEventQueryAgeResponse(session, string.Empty, ageMsg));
+        }
+
+        private static readonly int SecondsPerMinute = 60;
+        private static readonly int MinutesPerHour = 60;
+        private static readonly int HoursPerDay = 24;
+
+        private static readonly int DaysPerWeek = 7;
+        private static readonly int DaysPerMonth = 30;
+        private static readonly int DaysPerYear = 365;
+
+        private static readonly int SecondsPerHour = SecondsPerMinute * MinutesPerHour;     // 3600
+        private static readonly int SecondsPerDay = SecondsPerHour * HoursPerDay;           // 86400
+        private static readonly int SecondsPerWeek = SecondsPerDay * DaysPerWeek;           // 604800
+        private static readonly int SecondsPerMonth = SecondsPerDay * DaysPerMonth;         // 2592000
+        private static readonly int SecondsPerYear = SecondsPerDay * DaysPerYear;           // 31536000
+
+        public static string CalculateAgeMessage(int ageSeconds)
+        {
+            var years = 0;
+            var months = 0;
+            var weeks = 0;
+            var hours = 0;
+            var days = 0;
+            var minutes = 0;
+            var seconds = 0;
+
+            var remaining = ageSeconds;
+
+            if (remaining >= SecondsPerYear)
             {
-                if (Convert.ToInt16(tsAge.ToString("%d")) > 0 && Convert.ToInt16(tsAge.ToString("%d")) <= 7)
-                    age = age + tsAge.ToString("%d") + "d ";
-                if (Convert.ToInt16(tsAge.ToString("%d")) > 7)
-                {
-                    int months  = 0;
-                    int weeks   = 0;
-                    int days    = 0;
-
-                    for (int i = 0; i < tsAge.Days; i++)
-                    {
-                        days++;
-                        if (days > 7)
-                        {
-                            weeks++;
-                            days = 0;
-                        }
-                        if (weeks > 3)
-                        {
-                            months++;
-                            weeks = 0;
-                        }
-                    }
-
-                    if (months > 0)
-                        age = age + months + "mo ";
-                    if (weeks > 0)
-                        age = age + weeks + "w ";
-                    if (days > 0)
-                        age = age + days + "d ";
-                }
+                years = remaining / SecondsPerYear;
+                remaining -= years * SecondsPerYear;
             }
 
-            if (tsAge.ToString("%h") != "0")
-                age = age + tsAge.ToString("%h") + "h ";
+            if (remaining >= SecondsPerMonth)
+            {
+                months = remaining / SecondsPerMonth;
+                remaining -= months * SecondsPerMonth;
+            }
 
-            if (tsAge.ToString("%m") != "0")
-                age = age + tsAge.ToString("%m") + "m ";
+            if (remaining >= SecondsPerWeek)
+            {
+                weeks = remaining / SecondsPerWeek;
+                remaining -= weeks * SecondsPerWeek;
+            }
 
-            if (tsAge.ToString("%s") != "0")
-                age = age + tsAge.ToString("%s") + "s";
+            if (remaining >= SecondsPerDay)
+            {
+                days = remaining / SecondsPerDay;
+                remaining -= days * SecondsPerDay;
+            }
 
-            var ageEvent = new GameEvent.Events.GameEventQueryAgeResponse(session, "", age);
+            if (remaining >= SecondsPerHour)
+            {
+                hours = remaining / SecondsPerHour;
+                remaining -= hours * SecondsPerHour;
+            }
 
-            session.Network.EnqueueSend(ageEvent);
+            if (remaining >= SecondsPerMinute)
+            {
+                minutes = remaining / SecondsPerMinute;
+                remaining -= minutes * SecondsPerMinute;
+            }
+
+            seconds = remaining;
+
+            var pieces = new List<string>();
+
+            if (years > 0)
+                pieces.Add($"{years}y");
+            if (months > 0)
+                pieces.Add($"{months}mo");
+            if (weeks > 0)
+                pieces.Add($"{weeks}w");
+            if (days > 0)
+                pieces.Add($"{days}d");
+            if (hours > 0)
+                pieces.Add($"{hours}h");
+            if (minutes > 0)
+                pieces.Add($"{minutes}m");
+            if (seconds > 0)
+                pieces.Add($"{seconds}s");
+
+            return string.Join(" ", pieces);
         }
     }
 }
