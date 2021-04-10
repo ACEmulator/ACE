2470d7e57afc0129119de2555fb6f5d1ca889b78
Ty Conner <tyconner@itcproductions.com>
2021-04-10 17:40:24 -0400

    Update AdminCommands.cs

--- Source/ACE.Server/Command/Handlers/AdminCommands.cs
+++ Source/ACE.Server/Command/Handlers/AdminCommands.cs
@@ -1476,7 +1476,7 @@ namespace ACE.Server.Command.Handlers
                 msg += $"RestrictionEffect: {(PlayScript)house.GetProperty(PropertyDataId.RestrictionEffect)} ({house.GetProperty(PropertyDataId.RestrictionEffect)})\n";
                 msg += $"HouseMaxHooksUsable: {house.HouseMaxHooksUsable}\n";
                 msg += $"HouseCurrentHooksUsable: {house.HouseCurrentHooksUsable}\n";
-                msg += $"HouseHooksVisible: {house.HouseHooksVisible}\n";
+                msg += $"HouseHooksVisible: {house.HouseHooksVisible ?? false}\n";
                 msg += $"OpenToEveryone: {house.OpenToEveryone}\n";
                 session.Player.SendMessage(msg, ChatMessageType.System);
 
