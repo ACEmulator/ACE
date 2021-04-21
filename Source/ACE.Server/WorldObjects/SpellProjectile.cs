7038595f89437571d98f763f9021c8c9d6186a77
Randall Flagg <russellfannin0@gmail.com>
2021-04-20 17:59:28 -0700

    Apply 2014 Aegis PvP Nerf

--- Source/ACE.Server/WorldObjects/SpellProjectile.cs
+++ Source/ACE.Server/WorldObjects/SpellProjectile.cs
@@ -437,6 +437,14 @@ namespace ACE.Server.WorldObjects
 
             bool isPVP = sourcePlayer != null && targetPlayer != null;
 
+            //http://acpedia.org/wiki/Announcements_-_2014/01_-_Forces_of_Nature - Aegis is 72% effective in PvP
+            if (isPVP)
+            {
+                absorbMod = 1 - absorbMod;
+                absorbMod *= 0.72f;
+                absorbMod = 1 - absorbMod;
+            }
+
             if (isPVP && Spell.IsHarmful)
                 Player.UpdatePKTimers(sourcePlayer, targetPlayer);
 
