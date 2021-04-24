52e52fa315d73daa4efc69226d21a540ace155a2
Mag-nus <Mag-nus@users.noreply.github.com>
2021-04-24 09:06:27 -0500

    formatting

--- Source/ACE.Server/Command/Handlers/AdminStatCommands.cs
+++ Source/ACE.Server/Command/Handlers/AdminStatCommands.cs
@@ -235,7 +235,7 @@ namespace ACE.Server.Command.Handlers
             // https://docs.microsoft.com/en-us/dotnet/api/system.gcmemoryinfo?view=net-5.0
             var gcmi = GC.GetGCMemoryInfo();
 
-            sb.Append($"GCMI Index {gcmi.Index}, Generation {gcmi.Generation}, Compacted: {gcmi.Compacted}, Concurrent {gcmi.Concurrent}, PauseTimePercentage {gcmi.PauseTimePercentage}{'\n'}");
+            sb.Append($"GCMI Index {gcmi.Index:N0}, Generation {gcmi.Generation}, Compacted: {gcmi.Compacted}, Concurrent {gcmi.Concurrent}, PauseTimePercentage {gcmi.PauseTimePercentage}{'\n'}");
             for (int i = 0 ; i < gcmi.GenerationInfo.Length ; i++)
                 sb.Append($"GCMI.GenerationInfo[{i}] FragmentationBeforeBytes {(gcmi.GenerationInfo[i].FragmentationBeforeBytes >> 20):N0} MB, FragmentationAfterBytes {(gcmi.GenerationInfo[i].FragmentationAfterBytes >> 20):N0} MB, SizeBeforeBytes {(gcmi.GenerationInfo[i].SizeBeforeBytes >> 20):N0} MB, SizeAfterBytes {(gcmi.GenerationInfo[i].SizeAfterBytes >> 20):N0} MB{'\n'}");
             for (int i = 0; i < gcmi.PauseDurations.Length; i++)
