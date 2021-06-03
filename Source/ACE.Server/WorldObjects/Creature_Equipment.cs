2d434187179f7f3fac6d5a87746fa33e72e4796d
gmriggs <gmriggs@gmail.com>
2021-06-03 11:43:58 -0400

    fix magic absorbing shields in ThrownShield stance

--- Source/ACE.Server/WorldObjects/Creature_Equipment.cs
+++ Source/ACE.Server/WorldObjects/Creature_Equipment.cs
@@ -145,10 +145,20 @@ namespace ACE.Server.WorldObjects
 
         /// <summary>
         /// Returns the currently equipped missile weapon
+        /// This can be either a missile launcher (bow, crossbow, atlatl) or stackable thrown weapons directly in the main hand slot
         /// </summary>
         public WorldObject GetEquippedMissileWeapon()
         {
             return EquippedObjects.Values.FirstOrDefault(e => e.CurrentWieldedLocation == EquipMask.MissileWeapon);
+        }
+
+        /// <summary>
+        /// Returns the currently equipped missile launcher
+        /// </summary>
+        /// <returns></returns>
+        public WorldObject GetEquippedMissileLauncher()
+        {
+            return EquippedObjects.Values.FirstOrDefault(e => e.CurrentWieldedLocation == EquipMask.MissileWeapon && e is MissileLauncher);
         }
 
         /// <summary>
