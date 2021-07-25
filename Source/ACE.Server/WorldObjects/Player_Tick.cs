3d577ff71175503c0dcc7c5e5466bbdcb8732d3e
Ty Conner <tyconner@itcproductions.com>
2021-07-25 12:38:34 -0400

    Update house auto warning to exclude inactive houses (#3628)

--- Source/ACE.Server/WorldObjects/Player_Tick.cs
+++ Source/ACE.Server/WorldObjects/Player_Tick.cs
@@ -65,7 +65,7 @@ namespace ACE.Server.WorldObjects
                 {
                     HouseManager.GetHouse(House.Guid.Full, (house) =>
                     {
-                        if (house != null && !house.SlumLord.IsRentPaid())
+                        if (house != null && house.HouseStatus == HouseStatus.Active && !house.SlumLord.IsRentPaid())
                             Session.Network.EnqueueSend(new GameMessageSystemChat($"Warning!  You have not paid your maintenance costs for the last {(house.IsApartment ? "90" : "30")} day maintenance period.  Please pay these costs by this deadline or you will lose your house, and all your items within it.", ChatMessageType.Broadcast));
                     });
 
