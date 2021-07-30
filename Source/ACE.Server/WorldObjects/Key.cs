7060eb3efcc57c9467be537b200bd886c1fa097b
Ty Conner <tyconner@itcproductions.com>
2021-07-30 13:43:11 -0400

    Add UseRequiresLevel check to keys

--- Source/ACE.Server/WorldObjects/Key.cs
+++ Source/ACE.Server/WorldObjects/Key.cs
@@ -45,6 +45,22 @@ namespace ACE.Server.WorldObjects
 
         public override void HandleActionUseOnTarget(Player player, WorldObject target)
         {
+            // check activation requirements
+
+            // verify player level
+            if (UseRequiresLevel != null)
+            {
+                var playerLevel = player.Level ?? 1;
+                if (playerLevel < UseRequiresLevel.Value)
+                //return new ActivationResult(new GameEventWeenieErrorWithString(player.Session, WeenieErrorWithString.YouMustBe_ToUseItemMagic, $"level {UseRequiresLevel.Value}"));
+                {
+                    //player.SendWeenieError(ACE.Entity.Enum.WeenieError.LevelTooLow);
+                    //player.SendUseDoneEvent(ACE.Entity.Enum.WeenieError.LevelTooLow);
+                    player.SendTransientError($"You are not high enough level to use {Name} on {target.Name}!"); // not retail message almost certainly, but unable to locate specific instance in pcaps of using a key on chest below the level required at this time
+                    return;
+                }
+            }
+
             UnlockerHelper.UseUnlocker(player, this, target);
         }
     }
