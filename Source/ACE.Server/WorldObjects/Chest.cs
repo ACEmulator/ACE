b5b00091bae462fa5541016aaf3632a3ef51943d
Ty Conner <tyconner@itcproductions.com>
2021-08-15 16:09:05 -0400

    Update Chest.cs

--- Source/ACE.Server/WorldObjects/Chest.cs
+++ Source/ACE.Server/WorldObjects/Chest.cs
@@ -288,6 +288,7 @@ namespace ACE.Server.WorldObjects
                     generator.Spawned.Clear();
                     generator.SpawnQueue.Clear();
                     generator.RemoveQueue.Clear();
+                    generator.Removed.Clear();
                 }
             }
 
