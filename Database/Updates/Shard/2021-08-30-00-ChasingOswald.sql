a4d5a28a0ecd1ed0ad348571378d7c8ddd958bc2
Ross Elliott <ross@yotesfan.com>
2021-08-30 10:18:24 -0700

    Fix for ChasingOswald stamp

--- Database/Updates/Shard/2021-08-30-00-ChasingOswald.sql
+++ Database/Updates/Shard/2021-08-30-00-ChasingOswald.sql
@@ -1,7 +1,8 @@
 /* Changes the completion quest stamp for "Chasing Oswald" to match the Contract stamp */
 UPDATE
+IGNORE
     `character_properties_quest_registry`
 SET
     `quest_Name` = 'OswaldManualCompleted'
 WHERE
-    `character_properties_quest_registry`.`quest_Name` = 'ChasingOswald';
+    `quest_Name` = 'ChasingOswaldDone';
