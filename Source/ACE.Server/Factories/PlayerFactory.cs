0df0a375a3d7599870b6532609c64aaf239b7d38
ziang4891 <56269610+ziang4891@users.noreply.github.com>
2021-10-11 23:43:34 -0700

    Couple more adjustments

--- Source/ACE.Server/Factories/PlayerFactory.cs
+++ Source/ACE.Server/Factories/PlayerFactory.cs
@@ -391,20 +391,22 @@ namespace ACE.Server.Factories
 
             player.Instantiation = new Position(instantiation);
 
-            player.Sanctuary = new Position(player.Location);
+            if (player.IsOlthoiPlayer)
+                player.Sanctuary = new Position(0xE6D3000E, 37.881401f, 134.464005f, 217.996094f,  0.0f, 0.0f, -0.5f, -0.866025f);
+            else
+                player.Sanctuary = new Position(player.Location);
 
             /* Turn off RecallsDisabled and requiring and burning spell components for Olthoi play */
             if (player.IsOlthoiPlayer)
             {
                 player.SetProperty(PropertyBool.RecallsDisabled, false);
+                player.SetProperty(PropertyBool.LoginAtLifestone, true);
                 player.SetProperty(PropertyBool.SpellComponentsRequired, false);
-                player.SetProperty(PropertyBool.SafeSpellComponents, true);
             }
             else
             {
                 player.SetProperty(PropertyBool.RecallsDisabled, true);
                 player.SetProperty(PropertyBool.SpellComponentsRequired, true);
-                player.SetProperty(PropertyBool.SafeSpellComponents, false);
             }
 
             if (PropertyManager.GetBool("pk_server").Item)
