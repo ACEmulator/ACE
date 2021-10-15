61d86680b0a8f95590438bdc56f226059d1e0c84
ziang4891 <56269610+ziang4891@users.noreply.github.com>
2021-10-14 21:08:36 -0700

    LoginAtLifestone support (#3698)
    * LoginAtLifestone support
    
    * Update WorldManager.cs
    
    * Update WorldManager.cs
    
    Co-authored-by: Ty Conner <tyconner@itcproductions.com>

--- Source/ACE.Server/Managers/WorldManager.cs
+++ Source/ACE.Server/Managers/WorldManager.cs
@@ -213,8 +213,8 @@ namespace ACE.Server.Managers
                     session.Player.Location = new Position(0xA9B40019, 84, 7.1f, 94, 0, 0, -0.0784591f, 0.996917f);  // ultimate fallback
             }
 
-            if (session.Player.IsOlthoiPlayer)
-                session.Player.Location = session.Player.Sanctuary;
+            if (session.Player.LoginAtLifestone && character.TotalLogins >= 1)
+                session.Player.Location = new Position(session.Player.Sanctuary);
 
             session.Player.PlayerEnterWorld();
 
