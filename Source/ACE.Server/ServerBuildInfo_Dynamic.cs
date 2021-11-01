729af2b5039f9b54683b7e187f918579fcbda8e2
Ty Conner <tyconner@itcproductions.com>
2021-11-01 16:27:16 +0000

    [ci skip] Updating ServerBuildInfo_Dynamic.cs
    1.32.4101.20211101162351-master-f06ee93

--- Source/ACE.Server/ServerBuildInfo_Dynamic.cs
+++ Source/ACE.Server/ServerBuildInfo_Dynamic.cs
@@ -4,17 +4,17 @@ namespace ACE.Server
     public static partial class ServerBuildInfo 
     { 
         public static string Branch = "master"; 
-        public static string Commit = "1d436d8a40237989f9e90cc487e4d40e363fffea"; 
+        public static string Commit = "f06ee93884109eb5e4e7e5bcc39ddc0453c358c6"; 
  
         public static string Version = "1.32"; 
-        public static string Build   = "4097"; 
+        public static string Build   = "4101"; 
  
         public static int BuildYear   = 2021; 
-        public static int BuildMonth  = 10; 
-        public static int BuildDay    = 31; 
-        public static int BuildHour   = 20; 
-        public static int BuildMinute = 28; 
-        public static int BuildSecond = 36; 
+        public static int BuildMonth  = 11; 
+        public static int BuildDay    = 01; 
+        public static int BuildHour   = 16; 
+        public static int BuildMinute = 23; 
+        public static int BuildSecond = 51; 
     } 
 } 
  
