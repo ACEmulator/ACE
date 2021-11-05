8bf3bb49db8172a40200d78a6ef16b9993a375d4
gmriggs <gmriggs@gmail.com>
2021-11-04 20:03:20 -0400

    add vortex thorn

--- Database/Updates/Shard/2021-11-04-00-Update-Critical-Frequency.sql
+++ Database/Updates/Shard/2021-11-04-00-Update-Critical-Frequency.sql
@@ -46,3 +46,10 @@ INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.we
 SET bfloat.value=wfloat.value
 WHERE biota.weenie_Class_Id=40088 and bfloat.`type`=147 and wfloat.`type`=147;
 
+/* PropertyFloat.CriticalFrequency - 52733 Vortex Thorn */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=52733 and bfloat.`type`=147 and wfloat.`type`=147;
