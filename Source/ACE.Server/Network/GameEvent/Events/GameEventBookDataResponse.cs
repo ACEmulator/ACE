3e3bb13b698cf3c69bace4f139973a7540a8371a
Ty Conner <tyconner@itcproductions.com>
2021-12-21 14:50:48 -0500

    Adjust GameEventBookDataResponse (#3761)
    * Adjust GameEventBookDataResponse
    
    Fixes #3654
    
    * Update GameEventBookDataResponse.cs

--- Source/ACE.Server/Network/GameEvent/Events/GameEventBookDataResponse.cs
+++ Source/ACE.Server/Network/GameEvent/Events/GameEventBookDataResponse.cs
@@ -52,7 +52,7 @@ namespace ACE.Server.Network.GameEvent.Events
             }
 
             Writer.WriteString16L(inscription);
-            Writer.Write(authorId);
+            Writer.Write(authorId != 0xFFFFFFFF ? authorId : 0x0u);
             Writer.WriteString16L(authorName);
         }
     }
