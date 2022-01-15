7d40aec3932e87a880938e11dd8a971d6e69be3a
Acpvpserver <Acpvpserver@gmail.com>
2022-01-14 23:29:11 -0500

    Unlimited use consumables no longer get consumed

--- Source/ACE.Server/WorldObjects/Food.cs
+++ Source/ACE.Server/WorldObjects/Food.cs
@@ -92,6 +92,9 @@ namespace ACE.Server.WorldObjects
             var soundEvent = new GameMessageSound(player.Guid, GetUseSound(), 1.0f);
             player.EnqueueBroadcast(soundEvent);
 
+            if ((bool)this.GetProperty(PropertyBool.UnlimitedUse))
+                return;
+
             player.TryConsumeFromInventoryWithNetworking(this, 1);
         }
 
