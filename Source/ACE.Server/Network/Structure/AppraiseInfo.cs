cb8fa2f50be128436cd8090c277492d5783b4cd1
Ty Conner <tyconner@itcproductions.com>
2022-01-19 12:09:18 -0500

    Adjust Appraisal info for Hooks (#3793)

--- Source/ACE.Server/Network/Structure/AppraiseInfo.cs
+++ Source/ACE.Server/Network/Structure/AppraiseInfo.cs
@@ -255,28 +255,35 @@ namespace ACE.Server.Network.Structure
 
                     containsString = "It contains: \n";
 
-                    if (PropertiesString.ContainsKey(PropertyString.LongDesc) && PropertiesString[PropertyString.LongDesc] != null)
+                    if (!string.IsNullOrWhiteSpace(hookedItem.LongDesc))
                     {
-                        containsString += PropertiesString[PropertyString.LongDesc];
+                        containsString += hookedItem.LongDesc;
                     }
-                    else if (PropertiesString.ContainsKey(PropertyString.ShortDesc) && PropertiesString[PropertyString.ShortDesc] != null)
-                    {
-                        containsString += PropertiesString[PropertyString.ShortDesc];
-                    }
+                    //else if (PropertiesString.ContainsKey(PropertyString.ShortDesc) && PropertiesString[PropertyString.ShortDesc] != null)
+                    //{
+                    //    containsString += PropertiesString[PropertyString.ShortDesc];
+                    //}
                     else
                     {
-                        containsString += PropertiesString[PropertyString.Name];
+                        containsString += hookedItem.Name;
                     }
 
                     BuildHookProfile(hookedItem);
                 }
 
-                if (PropertiesString.ContainsKey(PropertyString.LongDesc) && PropertiesString[PropertyString.LongDesc] != null)
-                    PropertiesString[PropertyString.LongDesc] = baseDescString + containsString;
-                else if (PropertiesString.ContainsKey(PropertyString.ShortDesc) && PropertiesString[PropertyString.ShortDesc] != null)
-                    PropertiesString[PropertyString.LongDesc] = baseDescString + containsString;
-                else
-                    PropertiesString[PropertyString.LongDesc] = baseDescString + containsString;
+                //if (PropertiesString.ContainsKey(PropertyString.LongDesc) && PropertiesString[PropertyString.LongDesc] != null)
+                //    PropertiesString[PropertyString.LongDesc] = baseDescString + containsString;
+                ////else if (PropertiesString.ContainsKey(PropertyString.ShortDesc) && PropertiesString[PropertyString.ShortDesc] != null)
+                ////    PropertiesString[PropertyString.LongDesc] = baseDescString + containsString;
+                //else
+                //    PropertiesString[PropertyString.LongDesc] = baseDescString + containsString;
+
+                PropertiesString[PropertyString.LongDesc] = baseDescString + containsString;
+
+                PropertiesInt.Remove(PropertyInt.Structure);
+
+                // retail should have removed this property and then server side built the same result for the hook longdesc replacement but didn't and ends up with some odd looking appraisals as seen on video/pcaps
+                //PropertiesInt.Remove(PropertyInt.AppraisalLongDescDecoration);
             }
 
             if (wo is ManaStone)
