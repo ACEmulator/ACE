a267abaca36156f0ed4d32cadb7f5e702f6d275b
Ty Conner <tyconner@itcproductions.com>
2022-04-16 09:12:10 -0400

    Update WindupParams.cs

--- Source/ACE.Server/Entity/WindupParams.cs
+++ Source/ACE.Server/Entity/WindupParams.cs
@@ -1,21 +1,25 @@
+using ACE.Server.WorldObjects;
+
 namespace ACE.Server.Entity
 {
     public class WindupParams
     {
         public uint TargetGuid;
         public uint SpellId;
-        public bool BuiltInSpell;
+        //public bool BuiltInSpell;
+        public WorldObject CasterItem;
 
-        public WindupParams(uint targetGuid, uint spellId, bool builtInSpell)
+        public WindupParams(uint targetGuid, uint spellId, WorldObject casterItem)
         {
             TargetGuid = targetGuid;
             SpellId = spellId;
-            BuiltInSpell = builtInSpell;
+            //BuiltInSpell = builtInSpell;
+            CasterItem = casterItem;
         }
 
         public override string ToString()
         {
-            return $"TargetGuid: {TargetGuid:X8}, SpellID: {SpellId}, BuiltInSpell: {BuiltInSpell}";
+            return $"TargetGuid: {TargetGuid:X8}, SpellID: {SpellId}, CasterItem: {CasterItem?.Name}";
         }
     }
 }
