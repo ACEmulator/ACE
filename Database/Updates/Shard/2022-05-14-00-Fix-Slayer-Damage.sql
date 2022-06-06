9fce5cd7ff7fa53d369685226b7bd9eb8ad3729b
Ty Conner <tyconner@itcproductions.com>
2022-06-05 20:56:24 -0400

    Several Retroactive to Shard Item Fixes (#3853)
    * Create 2022-05-14-00-Fix-Slayer-Damage.sql
    
    Co-Authored-By: Fire-Chimp <49045665+Fire-Chimp@users.noreply.github.com>
    
    * Create 2022-05-14-01-Fix-Black-Cloud-Bow.sql
    
    * Create 2022-05-14-02-Fix-Exquisite-Lense.sql
    
    * Create 2022-05-14-03-Fix-Nether-Society-Orb.sql
    
    * Create 2022-05-14-04-Fix-Aegis-of-the-Golden-Flame.sql
    
    * Create 2022-05-14-05-Fix-Nekodes.sql
    
    * Create 2022-05-14-06-Fix-Gauntlet-Backpacks.sql
    
    Co-authored-by: Fire-Chimp <49045665+Fire-Chimp@users.noreply.github.com>

---
+++ Database/Updates/Shard/2022-05-14-00-Fix-Slayer-Damage.sql
@@ -0,0 +1,376 @@
+/* PropertyFloat.SlayerDamageBonus - 51965 Rynthid Tentacle Dagger */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=51965 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 51966 Rynthid Tentacle Mace */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=51966 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 51967 Rynthid Tentacle Spear */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=51967 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 51968 Rynthid Tentacle Greatspear */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=51968 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 51988 Rynthid Tentacle Bow */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=51988 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 51989 Rynthid Tentacle Wand */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=51989 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 51990 Life-attuned Rynthid Tentacle Wand */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=51990 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 51991 Nether-attuned Rynthid Tentacle Wand */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=51991 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 37574 Soul Bound Atlatl */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=37574 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 37577 Soul Bound Bow */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=37577 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 37579 Soul Bound Crossbow */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=37579 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 37585 Soul Bound Staff */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=37585 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 41927 Soul Bound Greatsword */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=41927 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 45523 Soul Bound Axe */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=45523 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 45524 Soul Bound Claw */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=45524 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 45525 Soul Bound Dagger */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=45525 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 45526 Soul Bound Mace */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=45526 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 45527 Soul Bound Spear */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=45527 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 45528 Soul Bound Staff */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=45528 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 45529 Soul Bound Sword */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=45529 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 41881 Ultimate Singularity Crossbow */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=41881 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 70970 Ultimate Singularity Atlatl */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=70970 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 41880 Ultimate Singularity Bow */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=41880 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 41890 Ultimate Singularity Greatsword */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=41890 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 41889 Ultimate Singularity Sword */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=41889 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 41888 Ultimate Singularity Staff */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=41888 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 41887 Ultimate Singularity Spear */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=41887 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 41884 Ultimate Singularity Mace */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=41884 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 41883 Ultimate Singularity Katar */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=41883 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 41882 Ultimate Singularity Dagger */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=41882 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 41879 Ultimate Singularity Axe */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=41879 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 41886 Ultimate Singularity Scepter of War Magic */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=41886 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 41885 Ultimate Singularity Scepter of Life Magic */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=41885 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 27837 Bound Singularity Scepter of Life Magic */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=27837 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 27841 Bound Singularity Scepter of War Magic */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=27841 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 70734 Spectral Atlatl */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=70734 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 70733 Spectral Crossbow */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=70733 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 37498 Spectral Bow */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=37498 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 70735 Spectral Axe */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=70735 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 70732 Spectral Sword */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=70732 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 70730 Spectral Spear */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=70730 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 70731 Spectral Staff */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=70731 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 70729 Spectral Mace */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=70729 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 70727 Spectral Dagger */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=70727 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 70728 Spectral Greatsword */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=70728 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 70726 Spectral Claw */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=70726 and bfloat.`type`=138 and wfloat.`type`=138;
+
+/* PropertyFloat.SlayerDamageBonus - 37478 Spectral Staff */
+
+UPDATE biota_properties_float bfloat
+INNER JOIN biota ON bfloat.object_Id=biota.Id
+INNER JOIN ace_world.weenie_properties_float wfloat ON wfloat.object_Id=biota.weenie_Class_Id
+SET bfloat.value=wfloat.value
+WHERE biota.weenie_Class_Id=37478 and bfloat.`type`=138 and wfloat.`type`=138;
+
