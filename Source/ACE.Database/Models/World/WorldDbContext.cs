b34007faac72da70432b22aae12416ed381361a6
Ty Conner <tyconner@itcproductions.com>
2022-06-21 14:16:44 -0400

    Update WorldDbContext.cs

--- Source/ACE.Database/Models/World/WorldDbContext.cs
+++ Source/ACE.Database/Models/World/WorldDbContext.cs
@@ -78,7 +78,7 @@ namespace ACE.Database.Models.World
             {
                 var config = Common.ConfigManager.Config.MySql.World;
 
-                var connectionString = $"server={config.Host};port={config.Port};user={config.Username};password={config.Password};database={config.Database};TreatTinyAsBoolean=False";
+                var connectionString = $"server={config.Host};port={config.Port};user={config.Username};password={config.Password};database={config.Database};TreatTinyAsBoolean=False;SslMode=None;ApplicationName=ACEmulator";
 
                 optionsBuilder.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString), builder =>
                 {
