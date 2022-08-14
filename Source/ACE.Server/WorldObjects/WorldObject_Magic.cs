8ce34e89df1a58c0aea5f83423e6f0cf3d6594ae
Ty Conner <tyconner@itcproductions.com>
2022-08-14 17:22:55 -0400

    Proxy OriginalPortal's Emotes to Summoned Portal

--- Source/ACE.Server/WorldObjects/WorldObject_Magic.cs
+++ Source/ACE.Server/WorldObjects/WorldObject_Magic.cs
@@ -1288,6 +1288,8 @@ namespace ACE.Server.WorldObjects
             gateway.Quest = portal.Quest;
             gateway.QuestRestriction = portal.QuestRestriction;
 
+            gateway.EmoteManager.SetProxy(portal);
+
             gateway.PortalRestrictions |= PortalBitmask.NoSummon; // all gateways are marked NoSummon but by default ruleset, the OriginalPortal is the one that is checked against
 
             gateway.EnterWorld();
