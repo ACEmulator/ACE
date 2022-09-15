e5a83cb979a9acaf5ffcce081707eeb6fa898024
Ty Conner <tyconner@itcproductions.com>
2022-09-15 01:23:48 -0400

    Create GameMessageDDDDataMessage.cs
    Co-Authored-By: OptimShi <OptimShi@users.noreply.github.com>

---
+++ Source/ACE.Server/Network/GameMessages/Messages/GameMessageDDDDataMessage.cs
@@ -0,0 +1,100 @@
+using System;
+using System.Linq;
+
+using ACE.DatLoader;
+using ACE.Server.Managers;
+
+namespace ACE.Server.Network.GameMessages.Messages
+{
+    public class GameMessageDDDDataMessage : GameMessage
+    {
+        public GameMessageDDDDataMessage(uint datFileId, DatDatabaseType datDatabaseType)
+            : base(GameMessageOpcode.DDD_DataMessage, GameMessageGroup.DatabaseQueue)
+        {
+            var datFileFound = false;
+
+            var datFileType = 0;
+            var datFileID = 0;
+
+            var datFile = new DatFile();
+            var datFileContents = Array.Empty<byte>();
+
+            switch (datDatabaseType)
+            {
+                case DatDatabaseType.Portal:
+
+                    datFileType = 0;
+                    datFileID = 1;
+
+                    datFileFound = DatManager.PortalDat.AllFiles.TryGetValue(datFileId, out datFile);
+
+                    datFileContents = DatManager.PortalDat.GetReaderForFile(datFileId).Buffer;
+
+                    break;
+
+                case DatDatabaseType.Cell:
+
+                    datFileType = 1;
+                    datFileID = 2;
+
+                    datFileFound = DatManager.CellDat.AllFiles.TryGetValue(datFileId, out datFile);
+
+                    datFileContents = DatManager.CellDat.GetReaderForFile(datFileId).Buffer;
+
+                    break;
+
+                case DatDatabaseType.Language:
+
+                    datFileType = 1;
+                    datFileID = 3;
+
+                    datFileFound = DatManager.LanguageDat.AllFiles.TryGetValue(datFileId, out datFile);
+
+                    datFileContents = DatManager.LanguageDat.GetReaderForFile(datFileId).Buffer;
+
+                    break;
+            }
+
+            if (!datFileFound)
+            {
+
+                return;
+            }
+
+            var fileId = datFile.ObjectId;
+            var fileType = datFile.GetFileType(datDatabaseType);
+
+            Writer.Write((uint)datFileType); // DatFileType
+            Writer.Write((uint)datFileID); // DatFileID
+
+            Writer.Write((uint)fileType);
+            Writer.Write(fileId);
+
+            Writer.Write(datFile.Iteration);
+
+            var compressDatFile = DDDManager.DatFileSizes[datDatabaseType][datFileId].CompressedFileSize > 0;
+
+            if (compressDatFile)
+            {
+                //var compressedDatFile = Ionic.Zlib.ZlibStream.CompressBuffer(datFileContents);
+                var compressedDatFile = DDDManager.Compress(datFileContents);
+
+                Writer.Write(true);
+
+                Writer.Write(3); // version
+                var a = BitConverter.GetBytes(datFile.FileSize);
+                var b = a.Concat(compressedDatFile).ToArray();
+                Writer.Write(b.Length + 4); // length + size of this message
+                Writer.Write(b);
+            }
+            else
+            {
+                Writer.Write(false);
+
+                Writer.Write(3); // version
+                Writer.Write(datFileContents.Length + 4); // length + size of this message
+                Writer.Write(datFileContents);
+            }
+        }
+    }
+}
