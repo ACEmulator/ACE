f1d6aa41c6ba88f9c82fe8196b85be474d5a555e
Ty Conner <tyconner@itcproductions.com>
2022-09-17 19:42:00 -0400

    Update WorldManager.cs

--- Source/ACE.Server/Managers/WorldManager.cs
+++ Source/ACE.Server/Managers/WorldManager.cs
@@ -412,8 +412,6 @@ namespace ACE.Server.Managers
 
             HouseManager.Tick();
 
-            DDDManager.Tick();
-
             ServerPerformanceMonitor.RegisterEventEnd(ServerPerformanceMonitor.MonitorType.UpdateGameWorld_Entire);
             ServerPerformanceMonitor.RegisterCumulativeEvents();
 
