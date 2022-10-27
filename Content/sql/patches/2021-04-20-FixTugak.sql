33304d120ec7299c9a6f5db690ae60d3c83185a7
AcPvP <89650671+AcPvP@users.noreply.github.com>
2022-10-26 21:21:35 -0400

    Create 2021-04-20-FixTugak.sql

---
+++ Content/sql/patches/2021-04-20-FixTugak.sql
@@ -0,0 +1,4 @@
+DELETE FROM `spell` WHERE `id` = 3818;
+
+INSERT INTO `spell` (`id`, `name`, `e_Type`, `base_Intensity`, `variance`, `wcid`, `num_Projectiles`, `num_Projectiles_Variance`, `spread_Angle`, `vertical_Angle`, `default_Launch_Angle`, `non_Tracking`, `create_Offset_Origin_X`, `create_Offset_Origin_Y`, `create_Offset_Origin_Z`, `padding_Origin_X`, `padding_Origin_Y`, `padding_Origin_Z`, `dims_Origin_X`, `dims_Origin_Y`, `dims_Origin_Z`, `peturbation_Origin_X`, `peturbation_Origin_Y`, `peturbation_Origin_Z`, `imbued_Effect`, `slayer_Creature_Type`, `slayer_Damage_Bonus`, `crit_Freq`, `crit_Multiplier`, `ignore_Magic_Resist`, `elemental_Modifier`, `drain_Percentage`, `damage_Ratio`, `last_Modified`)
+VALUES (3818, 'Curse of Raven Fury', 128 /* Health */, 42, 0, 31662 /* Health Ring Bolt */, 9, 0, 360, 0, 0, False, 0, 0, 0, 1, 1, 1, 9, 1, 1, 0, 0, 0, 0 /* Undef */, 0 /* Invalid */, 1, 0, 0, 0, 1, 0.50, 2.00, '2021-01-24 14:11:00');
