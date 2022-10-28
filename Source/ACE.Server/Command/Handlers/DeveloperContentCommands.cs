ea11513633fd1895257cfdea191ef7e55a0aaab5
AcPvP <89650671+AcPvP@users.noreply.github.com>
2022-10-27 22:49:40 -0400

    Dev command to import sql weenie subfolders

--- Source/ACE.Server/Command/Handlers/DeveloperContentCommands.cs
+++ Source/ACE.Server/Command/Handlers/DeveloperContentCommands.cs
@@ -251,7 +251,25 @@ namespace ACE.Server.Command.Handlers.Processors
                         break;
 
                     case FileType.Weenie:
-                        ImportSQLWeenie(session, param);
+                        if (param.Equals("all", StringComparison.OrdinalIgnoreCase))
+                        {
+                            ImportSQLWeenieWrapped(session, param, parameters.Length >= 3 ? parameters[2] : "");
+                        }
+                        else if (param.Equals("folder", StringComparison.OrdinalIgnoreCase))
+                        {
+                            string folderName = "";
+
+                            for (int i = 2; i < parameters.Length; i++)
+                            {
+                                folderName += parameters[i] + (i == parameters.Length - 1 ? "" : " ");
+                            }
+
+                            ImportSQLWeenieWrapped(session, param, folderName);
+                        }
+                        else
+                        {
+                            ImportSQLWeenie(session, param);
+                        }
                         break;
                 }
             }
@@ -2997,6 +3015,51 @@ namespace ACE.Server.Command.Handlers.Processors
             {
                 CommandHandlerHelper.WriteOutputInfo(session, $"Invalid Landblock ID: {parameters[0]}\nLandblock ID should be in the hex format such as this: @vloc2loc 0xAB94");
             }
+        }
+
+        public static void ImportSQLWeenieWrapped(Session session, string param, string param2)
+        {
+            DirectoryInfo di = VerifyContentFolder(session);
+            if (!di.Exists) return;
+
+            var sep = Path.DirectorySeparatorChar;
+
+            var prefix = param + " ";
+
+            var sql_folder = $"{di.FullName}{sep}sql{sep}weenies{sep}";
+
+            if (param.Equals("folder", StringComparison.OrdinalIgnoreCase) && !string.IsNullOrWhiteSpace(param2))
+            {
+                if (param2.Contains(".."))
+                {
+                    CommandHandlerHelper.WriteOutputInfo(session, $"Path may not contain the sequence '..'");
+                    return;
+                }
+                sql_folder = $"{sql_folder}{param2}{sep}";
+                prefix = "";
+            }
+            else if (param.Equals("all", StringComparison.OrdinalIgnoreCase))
+            {
+                prefix = "";
+            }
+
+            di = new DirectoryInfo(sql_folder);
+            if (!di.Exists)
+            {
+                CommandHandlerHelper.WriteOutputInfo(session, $"Couldn't find folder: {di.FullName}");
+                return;
+            }
+
+            var files = di.Exists ? di.GetFiles($"{prefix}*.sql", SearchOption.AllDirectories) : null;
+
+            if (files == null || files.Length == 0)
+            {
+                CommandHandlerHelper.WriteOutputInfo(session, $"Couldn't find {sql_folder}{prefix}*.sql");
+                return;
+            }
+
+            foreach (var file in files)
+                ImportSQLWeenie(session, Path.GetDirectoryName(file.FullName) + Path.DirectorySeparatorChar, file.Name);
         }
     }
 }
