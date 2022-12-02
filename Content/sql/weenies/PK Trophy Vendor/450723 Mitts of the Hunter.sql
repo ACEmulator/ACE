3e94a63fe8602a9abc3bb411cbaa7822ab86305e
ZaiusAC <cameron.spence@alumni.utoronto.ca>
2022-12-02 17:55:06 -0500

    Updated vendor items

--- Content/sql/weenies/PK Trophy Vendor/450723 Mitts of the Hunter.sql
+++ Content/sql/weenies/PK Trophy Vendor/450723 Mitts of the Hunter.sql
@@ -12,7 +12,7 @@ VALUES (450723,   1,          2) /* ItemType - Armor */
      , (450723,  16,          1) /* ItemUseable - No */
      , (450723,  19,       20) /* Value */
      , (450723,  28,        0) /* ArmorLevel */
-     , (450723,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity *//;
+     , (450723,  93,       1044) /* PhysicsState - Ethereal, IgnoreCollisions, Gravity */;
 
 INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
 VALUES (450723,  11, True ) /* IgnoreCollisions */
