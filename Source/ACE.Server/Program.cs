ac9be0bba78a4fade78aaed491c0b415896918a1
Ty Conner <tyconner@itcproductions.com>
2022-12-03 12:41:15 -0500

    Update Program.cs

--- Source/ACE.Server/Program.cs
+++ Source/ACE.Server/Program.cs
@@ -232,11 +232,13 @@ namespace ACE.Server
             log.Info("Initializing DatManager...");
             DatManager.Initialize(ConfigManager.Config.Server.DatFilesDirectory, true);
 
-            //if (ConfigManager.Config.Server.AllowDatPatching)
+            if (ConfigManager.Config.DDD.EnableDATPatching)
             {
                 log.Info("Initializing DDDManager...");
                 DDDManager.Initialize();
             }
+            else
+                log.Info("DAT Patching Disabled...");
 
             log.Info("Initializing DatabaseManager...");
             DatabaseManager.Initialize();
