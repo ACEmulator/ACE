9f117e15a225616f72ebc00e880fcc9eee000a42
ZaiusAC <cameron.spence@alumni.utoronto.ca>
2023-04-13 16:03:31 -0400

    Add Gauntlet Portals back into game

--- Content/sql/weenies/Gauntlet Portals/53445 Radiant Blood Gauntlet.sql
+++ Content/sql/weenies/Gauntlet Portals/53445 Radiant Blood Gauntlet.sql
@@ -1,1 +1,30 @@
 DELETE FROM `weenie` WHERE `class_Id` = 53445;
+
+INSERT INTO `weenie` (`class_Id`, `class_Name`, `type`, `last_Modified`)
+VALUES (53445, 'ace53445-radiantbloodgauntlet', 7, '2021-11-01 00:00:00') /* Portal */;
+
+INSERT INTO `weenie_properties_int` (`object_Id`, `type`, `value`)
+VALUES (53445,   1,      65536) /* ItemType - Portal */
+     , (53445,  16,         32) /* ItemUseable - Remote */
+     , (53445,  86,        180) /* MinLevel */
+     , (53445,  93,       3084) /* PhysicsState - Ethereal, ReportCollisions, Gravity, LightingOn */
+     , (53445, 111,         49) /* PortalBitmask - Unrestricted, NoSummon, NoRecall */
+     , (53445, 133,          4) /* ShowableOnRadar - ShowAlways */;
+
+INSERT INTO `weenie_properties_bool` (`object_Id`, `type`, `value`)
+VALUES (53445,   1, True ) /* Stuck */;
+
+INSERT INTO `weenie_properties_float` (`object_Id`, `type`, `value`)
+VALUES (53445,  54,    -0.1) /* UseRadius */;
+
+INSERT INTO `weenie_properties_string` (`object_Id`, `type`, `value`)
+VALUES (53445,   1, 'Radiant Blood Gauntlet') /* Name */;
+
+INSERT INTO `weenie_properties_d_i_d` (`object_Id`, `type`, `value`)
+VALUES (53445,   1, 0x020005D5) /* Setup */
+     , (53445,   2, 0x09000003) /* MotionTable */
+     , (53445,   8, 0x0600106B) /* Icon */;
+
+INSERT INTO `weenie_properties_position` (`object_Id`, `position_Type`, `obj_Cell_Id`, `origin_X`, `origin_Y`, `origin_Z`, `angles_W`, `angles_X`, `angles_Y`, `angles_Z`)
+VALUES (53445, 2, 0x596B010F, 120, -10, 0.005, -0.707107, 0, 0, -0.707107) /* Destination */
+/* @teleloc 0x596B010F [120.000000 -10.000000 0.005000] -0.707107 0.000000 0.000000 -0.707107 */;
