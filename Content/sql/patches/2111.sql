39b69773d2e1c701c5fe2b1502b93add06f0e469
ZaiusAC <cameron.spence@alumni.utoronto.ca>
2023-04-16 14:51:30 -0400

    Change 150 loot to T7 for legendary release

--- Content/sql/patches/2111.sql
+++ Content/sql/patches/2111.sql
@@ -3,7 +3,7 @@ DELETE FROM `treasure_death` WHERE `id` = 2111;
 INSERT INTO `treasure_death` (`id`, `treasure_Type`, `tier`, `loot_Quality_Mod`, `unknown_Chances`, `item_Chance`, `item_Min_Amount`, `item_Max_Amount`, `item_Treasure_Type_Selection_Chances`, `magic_Item_Chance`, `magic_Item_Min_Amount`, `magic_Item_Max_Amount`, `magic_Item_Treasure_Type_Selection_Chances`, `mundane_Item_Chance`, `mundane_Item_Min_Amount`, `mundane_Item_Max_Amount`, `mundane_Item_Type_Selection_Chances`, `last_Modified`) 
 VALUES (2111 	/* id */
 	 , 2111		/* treasure_Type */
-	 , 8		/* tier */
+	 , 7		/* tier */
 	 , 0.5		/* loot_Quality_Mod */
 	 , 19		/* unknown_Chances */
 	 , 100		/* item_Chance */
