8404857f7f7dff7c66ff75cfd9185d32f752726b
Ty Conner <tyconner@itcproductions.com>
2023-04-26 12:54:49 -0400

    Update NetworkSession.cs

--- Source/ACE.Server/Network/NetworkSession.cs
+++ Source/ACE.Server/Network/NetworkSession.cs
@@ -725,7 +725,6 @@ namespace ACE.Server.Network
             try
             {
                 // On connection to server, client expects response on the connection it initiated, once that occurs, the client connects to the +1 port and then the server transmits on that connection, while the client continues to transmit on the initial port.
-                //var socket = packet.Header.HasFlag(PacketHeaderFlags.ConnectRequest) ? connectionC2S.Socket: connectionS2C.Socket;
                 var socket = (session.State < SessionState.AuthConnected || session.State == SessionState.TerminationStarted) ? connectionC2S.Socket : connectionS2C.Socket;
                 var endPoint = (session.State < SessionState.AuthConnected || session.State == SessionState.TerminationStarted) ? session.EndPointC2S : session.EndPointS2C;
 
