5f4313cee54d47ce0fe04e22bee901946d921a3d
Ty Conner <tyconner@itcproductions.com>
2023-04-26 12:58:18 -0400

    Update BasePropertyDesc.cs (#3975)
    * Update BasePropertyDesc.cs
    
    * Update BasePropertyDesc.cs

--- Source/ACE.DatLoader/Entity/BasePropertyDesc.cs
+++ Source/ACE.DatLoader/Entity/BasePropertyDesc.cs
@@ -36,7 +36,7 @@ namespace ACE.DatLoader.Entity
         PropertyInheritanceType m_inheritanceType;
         PropertyDatFileType m_datFileType;
         PropertyPropagationType m_propagationType;
-        PropertyCachingType m_cachingType;
+        //PropertyCachingType m_cachingType;
 
         bool m_bRequired;
         bool m_bReadOnly;
@@ -93,7 +93,6 @@ namespace ACE.DatLoader.Entity
                     default:
                         // Should never hit this!
                         throw new NotImplementedException();
-                        break;
                 }
             }
             hasMax = reader.ReadBoolean();
@@ -125,10 +124,6 @@ namespace ACE.DatLoader.Entity
 
             /* Confident Var Names */
             m_fPredictionTimeout = reader.ReadSingle(); // const float Const_DefaultPredictedExpirationTime =  1.5; // idb
-            if(m_fPredictionTimeout != 1.5)
-            {
-                var test = false;
-            }
             m_inheritanceType = (PropertyInheritanceType)reader.ReadByte();
             m_datFileType = (PropertyDatFileType)reader.ReadByte();
             m_propagationType = (PropertyPropagationType)reader.ReadByte();
