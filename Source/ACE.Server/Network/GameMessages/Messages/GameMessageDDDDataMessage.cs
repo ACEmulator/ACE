cc018c529b52ccbc95f18115edfb17d9803b8757
Ty Conner <tyconner@itcproductions.com>
2023-05-01 21:49:08 -0400

    Update GameMessageDDDDataMessage.cs

--- Source/ACE.Server/Network/GameMessages/Messages/GameMessageDDDDataMessage.cs
+++ Source/ACE.Server/Network/GameMessages/Messages/GameMessageDDDDataMessage.cs
@@ -11,13 +11,8 @@ namespace ACE.Server.Network.GameMessages.Messages
         public GameMessageDDDDataMessage(uint datFileId, DatDatabaseType datDatabaseType)
             : base(GameMessageOpcode.DDD_DataMessage, GameMessageGroup.DatabaseQueue)
         {
-            //var datFileFound = false;
-
             var datFileType = 0;
             var datFileID = 0;
-
-            //var datFile = new DatFile();
-            //var datFileContents = Array.Empty<byte>();
 
             switch (datDatabaseType)
             {
@@ -26,20 +21,12 @@ namespace ACE.Server.Network.GameMessages.Messages
                     datFileType = 0;
                     datFileID = 1;
 
-                    //datFileFound = DatManager.PortalDat.AllFiles.TryGetValue(datFileId, out datFile);
-
-                    //datFileContents = DatManager.PortalDat.GetReaderForFile(datFileId).Buffer;
-
                     break;
 
                 case DatDatabaseType.Cell:
 
                     datFileType = 1;
                     datFileID = 2;
-
-                    //datFileFound = DatManager.CellDat.AllFiles.TryGetValue(datFileId, out datFile);
-
-                    //datFileContents = DatManager.CellDat.GetReaderForFile(datFileId).Buffer;
 
                     break;
 
@@ -48,24 +35,14 @@ namespace ACE.Server.Network.GameMessages.Messages
                     datFileType = 1;
                     datFileID = 3;
 
-                    //datFileFound = DatManager.LanguageDat.AllFiles.TryGetValue(datFileId, out datFile);
-
-                    //datFileContents = DatManager.LanguageDat.GetReaderForFile(datFileId).Buffer;
-
                     break;
             }
 
             var datFileContents = DDDManager.TryGetDatFileContentsForTransmission(datFileId, datDatabaseType, out var datFile, out var isCompressed);
 
-            //if (!datFileFound)
-            //{
-
-            //    return;
-            //}
-
             if (datFileContents == null)
             {
-
+                // do something here?
                 return;
             }
 
@@ -79,30 +56,6 @@ namespace ACE.Server.Network.GameMessages.Messages
             Writer.Write(fileId);
 
             Writer.Write(datFile.Iteration);
-
-            //var compressDatFile = DDDManager.DatFileSizes[datDatabaseType][datFileId].CompressedFileSize > 0;
-
-            //if (compressDatFile)
-            //{
-            //    //var compressedDatFile = Ionic.Zlib.ZlibStream.CompressBuffer(datFileContents);
-            //    var compressedDatFile = DDDManager.Compress(datFileContents);
-
-            //    Writer.Write(true);
-
-            //    Writer.Write(3); // version
-            //    var a = BitConverter.GetBytes(datFile.FileSize);
-            //    var b = a.Concat(compressedDatFile).ToArray();
-            //    Writer.Write(b.Length + 4); // length + size of this message
-            //    Writer.Write(b);
-            //}
-            //else
-            //{
-            //    Writer.Write(false);
-
-            //    Writer.Write(3); // version
-            //    Writer.Write(datFileContents.Length + 4); // length + size of this message
-            //    Writer.Write(datFileContents);
-            //}
 
             Writer.Write(isCompressed);
 
