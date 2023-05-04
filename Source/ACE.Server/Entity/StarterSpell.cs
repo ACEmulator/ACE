83754f25a43145466a42af9dd245eeed313a66b4
Ty Conner <tyconner@itcproductions.com>
2023-05-04 19:35:19 -0400

    Update StarterSpell.cs

--- Source/ACE.Server/Entity/StarterSpell.cs
+++ Source/ACE.Server/Entity/StarterSpell.cs
@@ -1,5 +1,6 @@
+using System;
+using System.Text.Json;
 using System.Text.Json.Serialization;
-using static ACE.Server.Factories.StarterGearFactory;
 
 namespace ACE.Server.Entity
 {
@@ -16,5 +17,34 @@ namespace ACE.Server.Entity
 
         [JsonPropertyName("specializedOnly"), JsonConverter(typeof(StringToBoolConverter))]
         public bool SpecializedOnly { get; set; }
+    }
+
+    public class StringToBoolConverter : JsonConverter<bool>
+    {
+        public override bool Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.String)
+            {
+                var boolString = reader.GetString();
+
+                return Convert.ToBoolean(boolString);
+            }
+            else if (reader.TokenType == JsonTokenType.True)
+            {
+                return true;
+            }
+            else if (reader.TokenType == JsonTokenType.False)
+            {
+                return false;
+            }
+
+            throw new JsonException();
+        }
+
+        public override void Write(Utf8JsonWriter writer, bool value, JsonSerializerOptions options)
+        {
+            writer.WriteBooleanValue(value);
+        }
+
     }
 }
