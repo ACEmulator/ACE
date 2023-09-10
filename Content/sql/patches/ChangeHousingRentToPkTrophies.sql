5683f2b2f59996e960fe3bed18c651e4005418b6
AcPvP <89650671+AcPvP@users.noreply.github.com>
2023-09-10 01:01:44 -0400

    Update ChangeHousingRentToPkTrophies.sql

--- Content/sql/patches/ChangeHousingRentToPkTrophies.sql
+++ Content/sql/patches/ChangeHousingRentToPkTrophies.sql
@@ -19,3 +19,63 @@ JOIN ace_world.weenie weenie
 ON biota.weenie_class_id = weenie.class_id
 WHERE weenie.`type` = 55
 AND class_Name LIKE '%mansion%';
+
+/* Buy item list for villa */
+INSERT INTO ace_shard.biota_properties_create_list
+(object_Id, destination_type, weenie_class_Id, stack_Size, palette, shade, try_to_Bond)
+SELECT biota.id, 16, 1000002, 100, 0, 0, 0
+FROM ace_shard.biota biota
+JOIN ace_world.weenie weenie
+ON biota.weenie_class_id = weenie.class_id
+WHERE weenie.`type` = 55
+AND class_Name LIKE '%villa%';
+
+/* Rent item list for villa */
+INSERT INTO ace_shard.biota_properties_create_list
+(object_Id, destination_type, weenie_class_Id, stack_Size, palette, shade, try_to_Bond)
+SELECT biota.id, 32, 1000002, 40, 0, 0, 0
+FROM ace_shard.biota biota
+JOIN ace_world.weenie weenie
+ON biota.weenie_class_id = weenie.class_id
+WHERE weenie.`type` = 55
+AND class_Name LIKE '%villa%';
+
+/* Buy item list for cottage */
+INSERT INTO ace_shard.biota_properties_create_list
+(object_Id, destination_type, weenie_class_Id, stack_Size, palette, shade, try_to_Bond)
+SELECT biota.id, 16, 1000002, 50, 0, 0, 0
+FROM ace_shard.biota biota
+JOIN ace_world.weenie weenie
+ON biota.weenie_class_id = weenie.class_id
+WHERE weenie.`type` = 55
+AND class_Name LIKE '%cottage%';
+
+/* Rent item list for villa */
+INSERT INTO ace_shard.biota_properties_create_list
+(object_Id, destination_type, weenie_class_Id, stack_Size, palette, shade, try_to_Bond)
+SELECT biota.id, 32, 1000002, 25, 0, 0, 0
+FROM ace_shard.biota biota
+JOIN ace_world.weenie weenie
+ON biota.weenie_class_id = weenie.class_id
+WHERE weenie.`type` = 55
+AND class_Name LIKE '%cottage%';
+
+/* Buy item list for apartment */
+INSERT INTO ace_shard.biota_properties_create_list
+(object_Id, destination_type, weenie_class_Id, stack_Size, palette, shade, try_to_Bond)
+SELECT biota.id, 16, 1000002, 25, 0, 0, 0
+FROM ace_shard.biota biota
+JOIN ace_world.weenie weenie
+ON biota.weenie_class_id = weenie.class_id
+WHERE weenie.`type` = 55
+AND class_id IN (9621,15608);
+
+/* Rent item list for apartment */
+INSERT INTO ace_shard.biota_properties_create_list
+(object_Id, destination_type, weenie_class_Id, stack_Size, palette, shade, try_to_Bond)
+SELECT biota.id, 32, 1000002, 25, 0, 0, 0
+FROM ace_shard.biota biota
+JOIN ace_world.weenie weenie
+ON biota.weenie_class_id = weenie.class_id
+WHERE weenie.`type` = 55
+AND class_id IN (9621,15608);
