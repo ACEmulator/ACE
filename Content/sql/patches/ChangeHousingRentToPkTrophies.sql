5c687bae6f6e476c7efcc3d5f21e1f3e5c968b3f
AcPvP <89650671+AcPvP@users.noreply.github.com>
2023-09-09 22:36:46 -0400

    Update ChangeHousingRentToPkTrophies.sql

--- Content/sql/patches/ChangeHousingRentToPkTrophies.sql
+++ Content/sql/patches/ChangeHousingRentToPkTrophies.sql
@@ -1,61 +1,21 @@
-DELETE FROM ace_world.weenie_properties_create_list WHERE destination_type IN (16,32);
-
-/* Buy item list for apartments */
-INSERT INTO ace_world.weenie_properties_create_list
-(object_Id, destination_type, weenie_class_Id, stack_Size, palette, shade, try_to_Bond)
-VALUES
-(9621, 16, 1000002, 25, 0, 0, 0);
-
-/* Rent item list for apartments */
-INSERT INTO ace_world.weenie_properties_create_list
-(object_Id, destination_type, weenie_class_Id, stack_Size, palette, shade, try_to_Bond)
-VALUES
-(9621, 32, 1000002, 25, 0, 0, 0);
-
-/* Buy item list for cottages */
-INSERT INTO ace_world.weenie_properties_create_list
-(object_Id, destination_type, weenie_class_Id, stack_Size, palette, shade, try_to_Bond)
-SELECT class_id, 16, 1000002, 50, 0, 0, 0
-FROM ace_world.weenie
-WHERE `type` = 55
-AND class_Name LIKE '%cottage%';
-
-/* Rent item list for cottages */
-INSERT INTO ace_world.weenie_properties_create_list
-(object_Id, destination_type, weenie_class_Id, stack_Size, palette, shade, try_to_Bond)
-SELECT class_id, 32, 1000002, 25, 0, 0, 0
-FROM ace_world.weenie
-WHERE `type` = 55
-AND class_Name LIKE '%cottage%';
-
-/* Buy item list for villas */
-INSERT INTO ace_world.weenie_properties_create_list
-(object_Id, destination_type, weenie_class_Id, stack_Size, palette, shade, try_to_Bond)
-SELECT class_id, 16, 1000002, 100, 0, 0, 0
-FROM ace_world.weenie
-WHERE `type` = 55
-AND class_Name LIKE '%villa%';
-
-/* Rent item list for villas */
-INSERT INTO ace_world.weenie_properties_create_list
-(object_Id, destination_type, weenie_class_Id, stack_Size, palette, shade, try_to_Bond)
-SELECT class_id, 32, 1000002, 50, 0, 0, 0
-FROM ace_world.weenie
-WHERE `type` = 55
-AND class_Name LIKE '%villa%';
+DELETE FROM ace_shard.biota_properties_create_list WHERE destination_type IN (16,32);
 
 /* Buy item list for mansions */
-INSERT INTO ace_world.weenie_properties_create_list
+INSERT INTO ace_shard.biota_properties_create_list
 (object_Id, destination_type, weenie_class_Id, stack_Size, palette, shade, try_to_Bond)
-SELECT class_id, 16, 1000002, 500, 0, 0, 0
-FROM ace_world.weenie
-WHERE `type` = 55
+SELECT biota.id, 16, 1000002, 500, 0, 0, 0
+FROM ace_shard.biota biota
+JOIN ace_world.weenie weenie
+ON biota.weenie_class_id = weenie.class_id
+WHERE weenie.`type` = 55
 AND class_Name LIKE '%mansion%';
 
 /* Rent item list for mansions */
-INSERT INTO ace_world.weenie_properties_create_list
+INSERT INTO ace_shard.biota_properties_create_list
 (object_Id, destination_type, weenie_class_Id, stack_Size, palette, shade, try_to_Bond)
-SELECT class_id, 32, 1000002, 150, 0, 0, 0
-FROM ace_world.weenie
-WHERE `type` = 55
+SELECT biota.id, 32, 1000002, 150, 0, 0, 0
+FROM ace_shard.biota biota
+JOIN ace_world.weenie weenie
+ON biota.weenie_class_id = weenie.class_id
+WHERE weenie.`type` = 55
 AND class_Name LIKE '%mansion%';
