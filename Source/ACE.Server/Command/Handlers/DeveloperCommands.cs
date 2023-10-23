b482936d9cf11229544a2f332c0f3300f20b8435
Ftuoil Xelrash <37750686+FtuoilXelrash@users.noreply.github.com>
2023-10-22 19:16:24 -0500

    Update DeveloperCommands.cs (#4051)
    fix string

--- Source/ACE.Server/Command/Handlers/DeveloperCommands.cs
+++ Source/ACE.Server/Command/Handlers/DeveloperCommands.cs
@@ -2908,7 +2908,7 @@ namespace ACE.Server.Command.Handlers
             player.EnchantmentManager.RemoveVitae();
 
             if (player != session.Player)
-                session.Network.EnqueueSend(new GameMessageSystemChat("Removed vitae for {player.Name}", ChatMessageType.Broadcast));
+                session.Network.EnqueueSend(new GameMessageSystemChat($"Removed vitae for {player.Name}", ChatMessageType.Broadcast));
         }
 
         [CommandHandler("fast", AccessLevel.Developer, CommandHandlerFlag.RequiresWorld)]
