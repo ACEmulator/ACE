f048b3bfd28712f08172d57fc07bf4b3ffbddb4d
AcPvP <89650671+AcPvP@users.noreply.github.com>
2023-11-17 21:31:53 -0500

    Fix world boss loot profile

--- Content/sql/patches/2888.sql
+++ Content/sql/patches/2888.sql
@@ -1,7 +1,7 @@
 DELETE FROM `treasure_death` WHERE `id` = 2888;
 
 INSERT INTO `treasure_death` (`id`, `treasure_Type`, `tier`, `loot_Quality_Mod`, `unknown_Chances`, `item_Chance`, `item_Min_Amount`, `item_Max_Amount`, `item_Treasure_Type_Selection_Chances`, `magic_Item_Chance`, `magic_Item_Min_Amount`, `magic_Item_Max_Amount`, `magic_Item_Treasure_Type_Selection_Chances`, `mundane_Item_Chance`, `mundane_Item_Min_Amount`, `mundane_Item_Max_Amount`, `mundane_Item_Type_Selection_Chances`, `last_Modified`) 
-VALUES (1 	/* id */
+VALUES (2888 	/* id */
 	 , 2888		/* treasure_Type */
 	 , 8		/* tier */
 	 , 0.9		/* loot_Quality_Mod */
