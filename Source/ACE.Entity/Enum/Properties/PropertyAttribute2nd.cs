b6d0d47ada5c8652967c934bf71fab03a089921a
Mag-nus <Mag-nus@users.noreply.github.com>
2024-02-11 16:51:38 -0600

    Enum ToSentence improvements

--- Source/ACE.Entity/Enum/Properties/PropertyAttribute2nd.cs
+++ Source/ACE.Entity/Enum/Properties/PropertyAttribute2nd.cs
@@ -29,6 +29,25 @@ namespace ACE.Entity.Enum.Properties
         /// <returns>string with spaces infront of capital letters</returns>
         public static string ToSentence(this PropertyAttribute2nd attribute2nd)
         {
+            switch (attribute2nd)
+            {
+                case PropertyAttribute2nd.Undef:
+                    return "Undef";
+                case PropertyAttribute2nd.MaxHealth:
+                    return "Maximum Health";
+                case PropertyAttribute2nd.Health:
+                    return "Health";
+                case PropertyAttribute2nd.MaxStamina:
+                    return "Maximum Stamina";
+                case PropertyAttribute2nd.Stamina:
+                    return "Stamina";
+                case PropertyAttribute2nd.MaxMana:
+                    return "Maximum Mana";
+                case PropertyAttribute2nd.Mana:
+                    return "Mana";
+            }
+
+            // TODO we really should log this as a warning to indicate that we're missing a case up above, and that the inefficient (GC unfriendly) line below will be used
             return new string(attribute2nd.ToString().Replace("Max", "Maximum").ToCharArray().SelectMany((c, i) => i > 0 && char.IsUpper(c) ? new char[] { ' ', c } : new char[] { c }).ToArray());
         }
     }
