d4f62befb2f78b36f9ca483425daacabf4a065e7
Mag-nus <Mag-nus@users.noreply.github.com>
2024-02-13 16:20:07 -0600

    Switch PhysicsTimer
    PhysicsTimer.CurrentTime evalulated the stopwatch every single call.
    
    This simply redirects the timer to use the master Timers.PortalYearTicks timer

--- Source/ACE.Server/Physics/Common/PhysicsTimer.cs
+++ Source/ACE.Server/Physics/Common/PhysicsTimer.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
 
+using ACE.Server.Entity;
+
 namespace ACE.Server.Physics.Common
 {
     /// <summary>
@@ -10,11 +12,14 @@ namespace ACE.Server.Physics.Common
     {
         private static readonly Stopwatch _timer;
 
+        /*
         /// <summary>
         /// This should only be used by the ACE.Server.Physics namespace and physics related properties<para />
         /// For a equally precise timer outside of this namespace, you can use WorldManager.PortalYearTicks
         /// </summary>
         public static double CurrentTime => _timer.Elapsed.TotalSeconds;
+        */
+        public static double CurrentTime => Timers.PortalYearTicks;
 
         static PhysicsTimer()
         {
