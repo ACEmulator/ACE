a096f0c9b9308ed32e7841a693c2a49979492b30
Mag-nus <Mag-nus@users.noreply.github.com>
2024-04-28 14:30:09 -0500

    Switch LandblockManager from lock to ReaderWriterLockSlim
    This should help reduce the lock contention (usually by lscape) with minimal impact to performance.

--- Source/ACE.Server/Managers/LandblockManager.cs
+++ Source/ACE.Server/Managers/LandblockManager.cs
@@ -26,7 +26,7 @@ namespace ACE.Server.Managers
         /// <summary>
         /// Locking mechanism provides concurrent access to collections
         /// </summary>
-        private static readonly object landblockMutex = new object();
+        private static readonly ReaderWriterLockSlim landblockLock = new ReaderWriterLockSlim(LockRecursionPolicy.SupportsRecursion);
 
         /// <summary>
         /// A table of all the landblocks in the world map
@@ -46,8 +46,15 @@ namespace ACE.Server.Managers
         {
             get
             {
-                lock (landblockMutex)
+                landblockLock.EnterReadLock();
+                try
+                {
                     return landblockGroups.Count;
+                }
+                finally
+                {
+                    landblockLock.ExitReadLock();
+                }
             }
         }
 
@@ -173,7 +180,8 @@ namespace ACE.Server.Managers
             if (landblockGroupPendingAdditions.Count == 0)
                 return;
 
-            lock (landblockMutex)
+            landblockLock.EnterWriteLock();
+            try
             {
                 for (int i = landblockGroupPendingAdditions.Count - 1; i >= 0; i--)
                 {
@@ -234,6 +242,10 @@ namespace ACE.Server.Managers
                     count += group.Count;
                 if (count != loadedLandblocks.Count)
                     log.Error($"[LANDBLOCK GROUP] ProcessPendingAdditions count ({count}) != loadedLandblocks.Count ({loadedLandblocks.Count})");
+            }
+            finally
+            {
+                landblockLock.ExitWriteLock();
             }
         }
 
@@ -390,8 +402,15 @@ namespace ACE.Server.Managers
 
         public static bool IsLoaded(LandblockId landblockId)
         {
-            lock (landblockMutex)
+            landblockLock.EnterReadLock();
+            try
+            {
                 return landblocks[landblockId.LandblockX, landblockId.LandblockY] != null;
+            }
+            finally
+            {
+                landblockLock.ExitReadLock();
+            }
         }
 
         /// <summary>
@@ -401,7 +420,8 @@ namespace ACE.Server.Managers
         {
             Landblock landblock;
 
-            lock (landblockMutex)
+            landblockLock.EnterUpgradeableReadLock();
+            try
             {
                 bool setAdjacents = false;
 
@@ -409,16 +429,24 @@ namespace ACE.Server.Managers
 
                 if (landblock == null)
                 {
-                    // load up this landblock
-                    landblock = landblocks[landblockId.LandblockX, landblockId.LandblockY] = new Landblock(landblockId);
-
-                    if (!loadedLandblocks.Add(landblock))
+                    landblockLock.EnterWriteLock();
+                    try
                     {
-                        log.Error($"LandblockManager: failed to add {landblock.Id.Raw:X8} to active landblocks!");
-                        return landblock;
-                    }
+                        // load up this landblock
+                        landblock = landblocks[landblockId.LandblockX, landblockId.LandblockY] = new Landblock(landblockId);
 
-                    landblockGroupPendingAdditions.Add(landblock);
+                        if (!loadedLandblocks.Add(landblock))
+                        {
+                            log.Error($"LandblockManager: failed to add {landblock.Id.Raw:X8} to active landblocks!");
+                            return landblock;
+                        }
+
+                        landblockGroupPendingAdditions.Add(landblock);
+                    }
+                    finally
+                    {
+                        landblockLock.ExitWriteLock();
+                    }
 
                     landblock.Init();
 
@@ -442,6 +470,10 @@ namespace ACE.Server.Managers
                 if (setAdjacents)
                     SetAdjacents(landblock, true, true);
             }
+            finally
+            {
+                landblockLock.ExitUpgradeableReadLock();
+            }
 
             return landblock;
         }
@@ -451,8 +483,15 @@ namespace ACE.Server.Managers
         /// </summary>
         public static List<Landblock> GetLoadedLandblocks()
         {
-            lock (landblockMutex)
+            landblockLock.EnterReadLock();
+            try
+            {
                 return loadedLandblocks.ToList();
+            }
+            finally
+            {
+                landblockLock.ExitReadLock();
+            }
         }
 
         /// <summary>
@@ -600,7 +639,8 @@ namespace ACE.Server.Managers
 
                     bool unloadFailed = false;
 
-                    lock (landblockMutex)
+                    landblockLock.EnterWriteLock();
+                    try
                     {
                         // remove from list of managed landblocks
                         if (loadedLandblocks.Remove(landblock))
@@ -650,6 +690,10 @@ namespace ACE.Server.Managers
                         else
                             unloadFailed = true;
                     }
+                    finally
+                    {
+                        landblockLock.ExitWriteLock();
+                    }
 
                     if (unloadFailed)
                         log.Error($"LandblockManager: failed to unload {landblock.Id.Raw:X8}");
@@ -674,10 +718,15 @@ namespace ACE.Server.Managers
         /// </summary>
         public static void AddAllActiveLandblocksToDestructionQueue()
         {
-            lock (landblockMutex)
+            landblockLock.EnterWriteLock();
+            try
             {
                 foreach (var landblock in loadedLandblocks)
                     AddToDestructionQueue(landblock);
+            }
+            finally
+            {
+                landblockLock.ExitWriteLock();
             }
         }
 
@@ -708,12 +757,17 @@ namespace ACE.Server.Managers
 
         public static void DoEnvironChange(EnvironChangeType environChangeType)
         {
-            lock (landblockMutex)
+            landblockLock.EnterReadLock();
+            try
             {
                 if (environChangeType.IsFog())
                     SetGlobalFogColor(environChangeType);
                 else
                     SendGlobalEnvironSound(environChangeType);
+            }
+            finally
+            {
+                landblockLock.ExitReadLock();
             }
         }
     }
