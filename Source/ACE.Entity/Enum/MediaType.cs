ed7af65f9aa7ca38aeca46c4865388064e89c170
OptimShi <OptimShi@users.noreply.github.com>
2024-08-13 11:45:30 -0700

    Added Dat structures and tests for UILayout and DbProperties (#4208)

---
+++ Source/ACE.Entity/Enum/MediaType.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ACE.Entity.Enum
+{
+    public enum MediaType
+    {
+        Undef = 0,
+        Movie = 1,
+        Alpha = 2,
+        Animation = 3,
+        Cursor = 4,
+        Image = 5,
+        Jump = 6,
+        Message = 7,
+        Pause = 8,
+        Sound = 9,
+        State = 10,
+        Fade = 11,
+        Stretch = 12,
+    }
+}
