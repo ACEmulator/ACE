bc1fb9b71169018e7839b109c6f72af8c3443cd6
Ty Conner <tyconner@itcproductions.com>
2024-12-30 16:37:06 -0500

    Update Landblock.cs

--- Source/ACE.Server/Entity/Landblock.cs
+++ Source/ACE.Server/Entity/Landblock.cs
@@ -894,9 +894,11 @@ namespace ACE.Server.Entity
                             success = wo.AddPhysicsObj();
                             if (!success)
                                 log.Warn($"Landblock.AddWorldObjectInternal: Could not spawn dynamic object!\n 0x{wo.Guid} - {wo.NameWithMaterial} (WCID: {wo.WeenieClassId} | WeenieType: {wo.WeenieType})\n at {wo.Location.ToLOCString()}");
-                            else if (log.IsDebugEnabled)
-                                log.Debug($"Landblock.AddWorldObjectInternal: Successfully moved and spawned dynamic object!\n 0x{wo.Guid} - {wo.NameWithMaterial} (WCID: {wo.WeenieClassId} | WeenieType: {wo.WeenieType})\n from: {wo.Location.ToLOCString()}\n   to: {woFailedLocation.ToLOCString()}");
-
+                            else
+                            {
+                                if (log.IsDebugEnabled)
+                                    log.Debug($"Landblock.AddWorldObjectInternal: Successfully moved and spawned dynamic object!\n 0x{wo.Guid} - {wo.NameWithMaterial} (WCID: {wo.WeenieClassId} | WeenieType: {wo.WeenieType})\n from: {wo.Location.ToLOCString()}\n   to: {woFailedLocation.ToLOCString()}");
+                            }
                         }
                         else
                             log.Warn($"AddWorldObjectInternal: couldn't spawn 0x{wo.Guid}:{wo.Name} [{wo.WeenieClassId} - {wo.WeenieType}] at {wo.Location.ToLOCString()}");
