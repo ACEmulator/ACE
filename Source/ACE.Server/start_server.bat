1c597dc3774fb4258771ee25f3f69f3155d0e54f
Galli <99272482+KT053374@users.noreply.github.com>
2025-06-10 14:59:13 -0400

    Update start_server script for net8 and clarify usage

--- Source/ACE.Server/start_server.bat
+++ Source/ACE.Server/start_server.bat
@@ -1,4 +1,4 @@
 @cd %~dp0
-@if not exist "ACE.Server.dll" (echo please run the copy of this file residing in the output folder: .\bin\x64\XXXXX\netcoreapp2.2\
+@if not exist "ACE.Server.dll" (echo please run the copy of this file residing in the output folder: .\bin\x64\XXXXX\net8.0\
 pause)
 dotnet ACE.Server.dll
