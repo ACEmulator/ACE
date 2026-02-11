b74cf7f3c5d08602feb750302369f1f6b60a4ab0
Ty Conner <tyconner@itcproductions.com>
2026-02-11 02:22:02 -0500

    Update AppVeyorBuild.bat

--- AppVeyor/AppVeyorBuild.bat
+++ AppVeyor/AppVeyorBuild.bat
@@ -1,2 +1,3 @@
 @echo on
 msbuild "C:\projects\ace\Source\ACE.sln" /verbosity:minimal /logger:"C:\Program Files\AppVeyor\BuildAgent\Appveyor.MSBuildLogger.dll" /p:NoWarn="NETSDK1233"
+AppVeyor\AppVeyorBeforePackage.bat
