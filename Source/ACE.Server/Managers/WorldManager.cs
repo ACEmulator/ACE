7945a493050792254ce3fc8ce70d3f554a5a5395
Ty Conner <tyconner@itcproductions.com>
2026-07-24 13:49:57 -0400

    Update WorldManager.cs

--- Source/ACE.Server/Managers/WorldManager.cs
+++ Source/ACE.Server/Managers/WorldManager.cs
@@ -241,7 +241,7 @@ namespace ACE.Server.Managers
             }
 
             // These warnings are set by DDD_InterrogationResponse
-            if ((session.DatWarnCell || session.DatWarnLanguage || session.DatWarnPortal) && PropertyManager.GetBool("show_dat_warning").Item)
+            if ((session.DatWarnCell || session.DatWarnLanguage || session.DatWarnPortal || session.DatWarnHighRes) && PropertyManager.GetBool("show_dat_warning").Item)
             {
                 var msg = PropertyManager.GetString("dat_older_warning_msg").Item;
                 var chatMsg = new GameMessageSystemChat(msg, ChatMessageType.System);
